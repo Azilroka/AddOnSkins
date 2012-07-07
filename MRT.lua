a87ce371cc50a924dae3fb2830e9e6da37af9e88
Azilroka <pheonix@pheonixro.com>
2012-07-07 00:40:31 -0500

    mizus raid tracker

---
+++ MRT.lua
@@ -0,0 +1,38 @@
+	if not IsAddOnLoaded("MizusRaidTracker") then return end
+
+	MRT_GUIFrame:StripTextures()
+	MRT_GUIFrame:SetTemplate("Transparent")
+	MRT_GUI_FourRowDialog:StripTextures()
+	MRT_GUI_FourRowDialog:SetTemplate("Transparent")
+	cSkinCloseButton(MRT_GUIFrame_CloseButton)
+	cSkinButton(MRT_GUIFrame_RaidLog_Export_Button)
+	cSkinButton(MRT_GUIFrame_RaidLog_Delete_Button)
+	cSkinButton(MRT_GUIFrame_RaidLog_ExportNormal_Button)
+	cSkinButton(MRT_GUIFrame_RaidLog_ExportHeroic_Button)
+	cSkinButton(MRT_GUIFrame_RaidBosskills_Add_Button)
+	cSkinButton(MRT_GUIFrame_RaidBosskills_Delete_Button)
+	cSkinButton(MRT_GUIFrame_RaidBosskills_Export_Button)
+	cSkinButton(MRT_GUIFrame_RaidAttendees_Add_Button)
+	cSkinButton(MRT_GUIFrame_RaidAttendees_Delete_Button)
+	cSkinButton(MRT_GUIFrame_TakeSnapshot_Button)
+	cSkinButton(MRT_GUIFrame_StartNewRaid_Button)
+	cSkinButton(MRT_GUIFrame_MakeAttendanceCheck_Button)
+	cSkinButton(MRT_GUIFrame_EndCurrentRaid_Button)
+	cSkinButton(MRT_GUIFrame_ResumeLastRaid_Button)
+	cSkinButton(MRT_GUIFrame_BossLoot_Add_Button)
+	cSkinButton(MRT_GUIFrame_BossLoot_Modify_Button)
+	cSkinButton(MRT_GUIFrame_BossLoot_Delete_Button)
+	cSkinButton(MRT_GUIFrame_BossAttendees_Add_Button)
+	cSkinButton(MRT_GUIFrame_BossAttendees_Delete_Button)
+	cSkinButton(MRT_GUI_FourRowDialog_OKButton)
+	cSkinButton(MRT_GUI_FourRowDialog_CancelButton)
+
+for i = 1, 6 do
+	_G["ScrollTable"..i]:StripTextures(true)
+	_G["ScrollTable"..i]:SetTemplate("Transparent")
+	_G["ScrollTable"..i.."ScrollFrameScrollBar"]:StripTextures(true)
+	cSkinScrollBar(_G["ScrollTable"..i.."ScrollFrameScrollBar"])
+end
+
+MRT_GUI_ItemTT:HookScript( "OnShow", function( self ) self:SetTemplate( "Transparent" ) end )
+MRT_GUI_TT:HookScript( "OnShow", function( self ) self:SetTemplate( "Transparent" ) end )
