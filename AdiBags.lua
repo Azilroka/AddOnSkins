30ef9cbc2ddbe9e969f7a1de449e23348ec16a2c
Azilroka <pheonix@pheonixro.com>
2012-07-30 04:24:20 -0500

    not skinned completely

--- AdiBags.lua
+++
@@ -1,5 +0,0 @@
-AdiBagsContainer1:SetTemplate("Transparent")
-cSkinCloseButton(AdiBagsContainer1.CloseButton)
-
-AdiBagsContainer2:SetTemplate("Transparent")
-cSkinCloseButton(AdiBagsContainer2.CloseButton)
