908036934cbc8786436c1a781a0bd8752210dfac
Azilroka <pheonix@pheonixro.com>
2012-08-17 12:21:24 -0500

    stAddonManager

---
+++ stAddonManager.lua
@@ -0,0 +1,18 @@
+LoadAddOn("stAddonmanager")
+if not (IsAddOnLoaded("ElvUI") or IsAddOnLoaded("Tukui")) or not IsAddOnLoaded("stAddonmanager") then return end
+print("stAddonManager Skinning")
+local SkinstAddonmanager = CreateFrame("Frame")
+	SkinstAddonmanager:RegisterEvent("PLAYER_ENTERING_WORLD")
+	SkinstAddonmanager:SetScript("OnEvent", function(self)
+
+	stAddonManager:LoadWindow()
+	stAddonManager:CreateBackdrop()
+	stAddonManager_ScrollBackground:CreateBackdrop()
+	stAddonManager_ScrollBackground.backdrop:SetBackdropColor(0,0,0,0)
+	cSkinEditBox(stAddonManager_SearchBar)
+	stAddonManager.reloadButton:CreateBackdrop()
+	stAddonManager.profileButton:CreateBackdrop()
+	stAddonManager_ScrollFrameScrollBar.thumbbg:CreateBackdrop()
+	stAddonManager:Hide()
+
+end)
