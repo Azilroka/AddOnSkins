86f38cbd08607e7282365d2418eb0c71cdd58394
Azilroka <pheonix@pheonixro.com>
2012-09-02 13:53:30 -0500

    EasyMail fix

--- EasyMail.lua
+++ EasyMail.lua
@@ -3,19 +3,19 @@ local SkinEasyMail = CreateFrame("Frame")
 	SkinEasyMail:RegisterEvent("MAIL_SHOW")
 	SkinEasyMail:SetScript("OnEvent", function(self, event, addon)
 	if (UISkinOptions.EasyMailSkin ~= "Enabled") then return end
+	MailFrame:Size(360,440)
 		cSkinButton(EasyMail_CheckAllButton)
 		cSkinButton(EasyMail_GetAllButton)
 		cSkinButton(EasyMail_AttButton)
 		cSkinButton(EasyMail_ForwardButton)
 		cSkinNextPrevButton(EasyMail_MailButton)
-
+	
 		for i = 1, 7 do
 			cSkinCheckBox(_G["EasyMail_CheckButton"..i])
 		end
 
 		EasyMail_ForwardButton:Point("RIGHT", OpenMailReplyButton, "LEFT", -2, 0)
 		InboxTooMuchMail:Point("TOPLEFT", MailFrame, "TOPLEFT", 4, -10)
-		OpenMailCancelButton:Point("BOTTOMRIGHT", OpenMailFrame, "BOTTOMRIGHT", -12, 80)
 		InboxPrevPageButton:Point("CENTER", InboxFrame, "BOTTOMLEFT", 63, 104)
 		InboxNextPageButton:Point("CENTER", InboxFrame, "BOTTOMLEFT", 314, 104)
 		EasyMail_CheckAllButton:Point("TOPLEFT", InboxFrame, "TOPLEFT", 50, -45)
@@ -23,12 +23,12 @@ local SkinEasyMail = CreateFrame("Frame")
 		EasyMail_MailButton:Point("TOPLEFT", SendMailNameEditBox, "TOPRIGHT", 4, 2)
 		EasyMail_MailButton:Size(22)
 		EasyMail_MailDropdownBackdrop:SetTemplate("Transparent")
-
+		OpenMailCancelButton:Point("BOTTOMRIGHT", OpenMailFrame, "BOTTOMRIGHT", -3, 3)
+		EasyMail_AttButton:Point("TOPRIGHT", OpenMailFrame, "TOPRIGHT", -120, -328)
 		for i = 1, 7 do	
 			local t = _G["MailItem"..i.."ExpireTime"]
 				if t then
 					t:ClearAllPoints()
-					t:Point("TOPRIGHT", -10, -4)
 					t:Point("TOPRIGHT", -10, -4)
 				end				
 		end
