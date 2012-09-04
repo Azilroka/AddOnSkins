70a0030909f55cfca5664cf1f09f7d5cb283ecff
Azilroka <pheonix@pheonixro.com>
2012-09-04 02:57:06 -0500

    Quartz Fix

--- Quartz.lua
+++ Quartz.lua
@@ -7,14 +7,28 @@ local SkinQuartz = CreateFrame("Frame")
 	SkinQuartz:SetScript("OnEvent", function(self)
 	if (UISkinOptions.QuartzSkin ~= "Enabled") then return end
 	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+	local s = UIPackageSkinFuncs.s
+	local c = UIPackageSkinFuncs.c
+	if IsAddOnLoaded("Tukui") then UIFont = [[Interface\AddOns\Tukui\medias\fonts\normal_font.ttf]] UIFontSize = 12 end
+	if IsAddOnLoaded("ElvUI") then UIFont = [[Interface\AddOns\ElvUI\media\fonts\PT_Sans_Narrow.ttf]] UIFontSize = 12 end
 
 	SkinQuartzBar = function(self, bar)
 		self:SetTemplate("Transparent")
-		self.IconBorder = CreateFrame("Frame", nil, self)
-		self.IconBorder:SetTemplate("Transparent")
-		self.IconBorder:SetPoint("TOPLEFT",self.Icon, "TOPLEFT", -2, 2)
-		self.IconBorder:SetPoint("BOTTOMRIGHT", self.Icon, "BOTTOMRIGHT", 2, -2)
-		self.IconBorder:SetFrameStrata("LOW")
+		if not self.IconBorder then
+			self.IconBorder = CreateFrame("Frame", nil, self)
+			self.IconBorder:SetTemplate("Transparent")
+			self.IconBorder:SetPoint("TOPLEFT",self.Icon, "TOPLEFT", -2, 2)
+			self.IconBorder:SetPoint("BOTTOMRIGHT", self.Icon, "BOTTOMRIGHT", 2, -2)
+			self.IconBorder:SetFrameStrata("LOW")
+		end
+		if self.config.hideicon then
+			self.IconBorder:Hide()
+		else
+			self.IconBorder:Show()
+		end
+		self.Text:SetFont(UIFont,UIFontSize)
+		self.TimeText:SetFont(UIFont,UIFontSize)
+		self.Bar:SetStatusBarTexture(c["media"].normTex)
 	end
 
 	local template = Q3.CastBarTemplate.template
