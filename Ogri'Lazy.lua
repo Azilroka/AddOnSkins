3975e4d04ac4cb2d9803e0ec6142dcc9693ac77c
Michael Tindal <babyplatypus@me.com>
2012-09-10 01:04:39 -0500

    Change Ogri'Lazy skin.

--- Ogri'Lazy.lua
+++ Ogri'Lazy.lua
@@ -1,16 +1,11 @@
 if not (IsAddOnLoaded("ElvUI") or IsAddOnLoaded("Tukui")) or not IsAddOnLoaded("Ogri'Lazy") then return end
-local SkinOgriLazy = CreateFrame("Frame")
-	SkinOgriLazy:RegisterEvent("PLAYER_ENTERING_WORLD")
-	SkinOgriLazy:SetScript("OnEvent", function(self)
-	if (UISkinOptions.OgriLazySkin == "Disabled") then return end
-	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
-
+local name = "OgriLazySkin"
+local function SkinOgriLazy(self)
 	local s = UIPackageSkinFuncs.s
 	local c = UIPackageSkinFuncs.c
 
 	cSkinFrame(Relic_View)
 	cSkinCloseButton(Relic_ViewCloseButton)
+end
 
-end)
-
-
+cRegisterSkin(name,SkinOgriLazy)
