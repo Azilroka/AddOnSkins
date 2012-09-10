3da34efad88158f4068f1d1c7e1dad28f2b426ca
Michael Tindal <babyplatypus@me.com>
2012-09-10 07:14:28 -0500

    Oops

--- EnergyWatch.lua
+++ EnergyWatch.lua
@@ -1,12 +1,12 @@
 if not (IsAddOnLoaded("Tukui") or IsAddOnLoaded("ElvUI")) or not IsAddOnLoaded("EnergyWatch") then return end
 local name = "EnergyWatchSkin"
 local function SkinEnergyWatch(self)
-local s = UIPackageSkinFuncs.s
-local c = UIPackageSkinFuncs.c
+	local s = UIPackageSkinFuncs.s
+	local c = UIPackageSkinFuncs.c
 	EnergyWatchBar:StripTextures(True)
 	EnergyWatchStatusBar:CreateBackdrop()
 	EnergyWatchStatusBar:SetStatusBarTexture(c["media"].normTex)
-end)
+end
 
 cRegisterSkin(name,SkinEnergyWatch)
 
