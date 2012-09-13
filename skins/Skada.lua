4c07b7b1ce7b57ada130696ab9bdbd482dbef0b2
Michael Tindal <babyplatypus@me.com>
2012-09-13 18:36:09 -0500

    Skada fix hopefully.

--- skins/Skada.lua
+++ skins/Skada.lua
@@ -8,96 +8,6 @@ local barSpacing = 1
 local borderWidth = 1
 local barmod = Skada.displays["bar"]
 
-local name = "SkadaSkin"
-local function SkinSkada(self)
-local function StripOptions(options)
-	options.baroptions.args.barspacing = nil
-	options.titleoptions.args.texture = nil
-	options.titleoptions.args.bordertexture = nil
-	options.titleoptions.args.thickness = nil
-	options.titleoptions.args.margin = nil
-	options.titleoptions.args.color = nil
-	options.windowoptions = nil
---	options.baroptions.args.barfont = nil
---	options.titleoptions.args.font = nil
-end
-
-local barmod = Skada.displays["bar"]
-barmod.AddDisplayOptions_ = barmod.AddDisplayOptions
-barmod.AddDisplayOptions = function(self, win, options)
-	self:AddDisplayOptions_(win, options)
-	StripOptions(options)
-end
-
-for k, options in pairs(Skada.options.args.windows.args) do
-	if(options.type == "group") then
-		StripOptions(options.args)
-	end
-end
-
-local titleBG = {
-	bgFile = c["media"].normTex,
-	tile = false,
-	tileSize = 0
-}
-
-barmod.ApplySettings_ = barmod.ApplySettings
-barmod.ApplySettings = function(self, win)
-	barmod.ApplySettings_(self, win)
-
-	local skada = win.bargroup
-
-	if(win.db.enabletitle) then
-		skada.button:SetBackdrop(titleBG)
-	end
-
-	skada:SetTexture(c["media"].normTex)
-	skada:SetSpacing(barSpacing)
-	skada:SetFrameLevel(5)
-	
-	if not skada.TitleBackGround then
-		skada.TitleBackGround = CreateFrame("Frame", nil, skada.button)
-		skada.TitleBackGround:SetPoint("TOP")
-		skada.TitleBackGround:SetPoint("LEFT")
-		skada.TitleBackGround:SetPoint("RIGHT")
-		skada.TitleBackGround:SetPoint("BOTTOM", 0, 1)
-		skada.TitleBackGround:SetTemplate("Default")
-		skada.TitleBackGround:SetFrameLevel(skada.button:GetFrameLevel() -1)
-	end
-	
-	local titlefont = CreateFont("TitleFont" .. win.db.name)
-	skada.button:SetNormalFontObject(titlefont)
-	if IsAddOnLoaded("Tukui") then 
-		local color = win.db.title.color
-		skada.button:SetBackdropColor(0, 0, 0, 0)
-	end
-	if IsAddOnLoaded("ElvUI") then 
-		win.bargroup.button:SetBackdropColor(unpack(c["media"].backdropcolor))
-	end
-	skada:SetBackdrop(nil)
-	if not skada.backdrop then
-		skada:CreateBackdrop("Default")
-	end
-	skada.backdrop:ClearAllPoints()
-	if(win.db.enabletitle) then
-		skada.backdrop:Point("TOPLEFT", skada.button, "TOPLEFT", -2, 2)
-	else
-		skada.backdrop:Point("TOPLEFT", skada, "TOPLEFT", -2, 2)
-	end
-	skada.backdrop:Point("BOTTOMRIGHT", skada, "BOTTOMRIGHT", 2, -2)
-	if (not cCheckOption("SkadaBackdrop")) then skada.backdrop:Hide() end
-	if (cCheckOption("EmbedSkada")) then
-		win.bargroup.button:SetFrameStrata("MEDIUM")
-		win.bargroup.button:SetFrameLevel(5)	
-		win.bargroup:SetFrameStrata("MEDIUM")
-	end
-end
-	if(cCheckOption("EmbedSkada")) then
-		EmbedSkada()
-	end
-end
-
-cRegisterSkin(name, SkinSkada)
 local function EmbedWindow(window, width, height, point, relativeFrame, relativePoint, ofsx, ofsy)
 	window.db.barwidth = width
 	window.db.background.height = height
@@ -110,10 +20,10 @@ local function EmbedWindow(window, width, height, point, relativeFrame, relative
 end
 
 local windows = {}
-	function EmbedSkada()
-		if(#windows == 1) then
-			EmbedWindow(windows[1], EmbeddingWindow:GetWidth() - 4, (EmbeddingWindow:GetHeight() - 24), "TOPRIGHT", EmbeddingWindow, "TOPRIGHT", -2, -18)
-		elseif(#windows == 2) then
+function EmbedSkada()
+	if(#windows == 1) then
+		EmbedWindow(windows[1], EmbeddingWindow:GetWidth() - 4, (EmbeddingWindow:GetHeight() - 24), "TOPRIGHT", EmbeddingWindow, "TOPRIGHT", -2, -18)
+	elseif(#windows == 2) then
 		if IsAddOnLoaded("Tukui") then
 			EmbedWindow(windows[1], ((EmbeddingWindow:GetWidth() - 4) / 2) - (borderWidth + s.mult), EmbeddingWindow:GetHeight() - 24, "TOPRIGHT", EmbeddingWindow, "TOPRIGHT", -2, -18)
 			EmbedWindow(windows[2], ((EmbeddingWindow:GetWidth() - 4) / 2) - (borderWidth + s.mult), EmbeddingWindow:GetHeight() - 24, "TOPLEFT", EmbeddingWindow, "TOPLEFT", 2, -18)
@@ -125,34 +35,125 @@ local windows = {}
 	end
 end
 
-for _, window in ipairs( Skada:GetWindows() ) do
-	window:UpdateDisplay()
-end
+local name = "SkadaSkin"
+local function SkinSkada(self)
+	local function StripOptions(options)
+		options.baroptions.args.barspacing = nil
+		options.titleoptions.args.texture = nil
+		options.titleoptions.args.bordertexture = nil
+		options.titleoptions.args.thickness = nil
+		options.titleoptions.args.margin = nil
+		options.titleoptions.args.color = nil
+		options.windowoptions = nil
+	--	options.baroptions.args.barfont = nil
+	--	options.titleoptions.args.font = nil
+	end
 
-Skada.CreateWindow_ = Skada.CreateWindow
-	function Skada:CreateWindow(name, db)
-		Skada:CreateWindow_(name, db)
+	local barmod = Skada.displays["bar"]
+	barmod.AddDisplayOptions_ = barmod.AddDisplayOptions
+	barmod.AddDisplayOptions = function(self, win, options)
+		self:AddDisplayOptions_(win, options)
+		StripOptions(options)
+	end
 
-		windows = {}
-		for _, window in ipairs(Skada:GetWindows()) do
-			tinsert(windows, window)
-		end	
+	for k, options in pairs(Skada.options.args.windows.args) do
+		if(options.type == "group") then
+			StripOptions(options.args)
+		end
+	end
+
+	local titleBG = {
+		bgFile = c["media"].normTex,
+		tile = false,
+		tileSize = 0
+	}
+
+	barmod.ApplySettings_ = barmod.ApplySettings
+	barmod.ApplySettings = function(self, win)
+		barmod.ApplySettings_(self, win)
+
+		local skada = win.bargroup
+
+		if(win.db.enabletitle) then
+			skada.button:SetBackdrop(titleBG)
+		end
+
+		skada:SetTexture(c["media"].normTex)
+		skada:SetSpacing(barSpacing)
+		skada:SetFrameLevel(5)
+		
+		if not skada.TitleBackGround then
+			skada.TitleBackGround = CreateFrame("Frame", nil, skada.button)
+			skada.TitleBackGround:SetPoint("TOP")
+			skada.TitleBackGround:SetPoint("LEFT")
+			skada.TitleBackGround:SetPoint("RIGHT")
+			skada.TitleBackGround:SetPoint("BOTTOM", 0, 1)
+			skada.TitleBackGround:SetTemplate("Default")
+			skada.TitleBackGround:SetFrameLevel(skada.button:GetFrameLevel() -1)
+		end
+	
+		local titlefont = CreateFont("TitleFont" .. win.db.name)
+		skada.button:SetNormalFontObject(titlefont)
+		if IsAddOnLoaded("Tukui") then 
+			local color = win.db.title.color
+			skada.button:SetBackdropColor(0, 0, 0, 0)
+		end
+		if IsAddOnLoaded("ElvUI") then 
+			win.bargroup.button:SetBackdropColor(unpack(c["media"].backdropcolor))
+		end
+		skada:SetBackdrop(nil)
+		if not skada.backdrop then
+			skada:CreateBackdrop("Default")
+		end
+		skada.backdrop:ClearAllPoints()
+		if(win.db.enabletitle) then
+			skada.backdrop:Point("TOPLEFT", skada.button, "TOPLEFT", -2, 2)
+		else
+			skada.backdrop:Point("TOPLEFT", skada, "TOPLEFT", -2, 2)
+		end
+		skada.backdrop:Point("BOTTOMRIGHT", skada, "BOTTOMRIGHT", 2, -2)
+		if (not cCheckOption("SkadaBackdrop")) then skada.backdrop:Hide() end
+		if (cCheckOption("EmbedSkada")) then
+			win.bargroup.button:SetFrameStrata("MEDIUM")
+			win.bargroup.button:SetFrameLevel(5)	
+			win.bargroup:SetFrameStrata("MEDIUM")
+		end
+	end
 	if(cCheckOption("EmbedSkada")) then
 		EmbedSkada()
 	end
-end
 
-Skada.DeleteWindow_ = Skada.DeleteWindow
-	function Skada:DeleteWindow( name )
-		Skada:DeleteWindow_( name )
-		windows = {}
-		for _, window in ipairs( Skada:GetWindows() ) do
-			tinsert( windows, window )
-		end	
-	if(cCheckOption("EmbedSkada")) then
-		EmbedSkada()
+	for _, window in ipairs( Skada:GetWindows() ) do
+		window:UpdateDisplay()
+	end
+
+	Skada.CreateWindow_ = Skada.CreateWindow
+		function Skada:CreateWindow(name, db)
+			Skada:CreateWindow_(name, db)
+
+			windows = {}
+			for _, window in ipairs(Skada:GetWindows()) do
+				tinsert(windows, window)
+			end	
+		if(cCheckOption("EmbedSkada")) then
+			EmbedSkada()
+		end
+	end
+
+	Skada.DeleteWindow_ = Skada.DeleteWindow
+		function Skada:DeleteWindow( name )
+			Skada:DeleteWindow_( name )
+			windows = {}
+			for _, window in ipairs( Skada:GetWindows() ) do
+				tinsert( windows, window )
+			end	
+		if(cCheckOption("EmbedSkada")) then
+			EmbedSkada()
+		end
 	end
 end
+
+cRegisterSkin(name, SkinSkada)
 
 StaticPopupDialogs["SKADA_RELOADUI"] = {
 	text = "Reload your User Interface?",
