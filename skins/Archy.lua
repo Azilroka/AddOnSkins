828176ca12aef0494be5ea70a6831ee4f79063a4
Azilroka <pheonix@pheonixro.com>
2012-09-15 19:08:03 -0500

    Archy InCombatLockdown() Safeguard

--- skins/Archy.lua
+++ skins/Archy.lua
@@ -6,31 +6,33 @@ local function SkinArchy(self)
 	local c = U.c
 
 	local function SkinArchyArtifactFrame()
-		U.SkinFrame(ArchyArtifactFrame)
-		ArchyArtifactFrame:SetParent(UIParent)
-		ArchyArtifactFrame:SetScale(1)
-		--ArchyArtifactFrame:CreateBackdrop()
-		--ArchyArtifactFrame.backdrop:SetParent(UIParent)
-		--ArchyArtifactFrame.backdrop:Point("TOPLEFT", ArchyArtifactFrame, 0, 0)
-		--ArchyArtifactFrame.backdrop:Point("BOTTOMRIGHT", ArchyArtifactFrame, 0, 0)
+		if not InCombatLockdown() then
+			U.SkinFrame(ArchyArtifactFrame)
+			ArchyArtifactFrame:SetScale(1)
+			--ArchyArtifactFrame:CreateBackdrop()
+			--ArchyArtifactFrame.backdrop:SetParent(UIParent)
+			--ArchyArtifactFrame.backdrop:Point("TOPLEFT", ArchyArtifactFrame, 0, 0)
+			--ArchyArtifactFrame.backdrop:Point("BOTTOMRIGHT", ArchyArtifactFrame, 0, 0)
+		end
 	end
 
 	hooksecurefunc(Archy, "UpdateRacesFrame", SkinArchyArtifactFrame)
 
 	local function SkinArchyDigSiteFrame()
-		U.SkinFrame(ArchyDigSiteFrame)
-		ArchyDigSiteFrame:SetParent(UIParent)
-		ArchyDigSiteFrame:SetScale(1)
-		--ArchyDigSiteFrame:CreateBackdrop()
-		--ArchyDigSiteFrame.backdrop:SetParent(UIParent)
-		--ArchyDigSiteFrame.backdrop:Point("TOPLEFT", ArchyArtifactFrame, 0, 0)
-		--ArchyDigSiteFrame.backdrop:Point("BOTTOMRIGHT", ArchyArtifactFrame, 0, 0)
+		if not InCombatLockdown() then
+			U.SkinFrame(ArchyDigSiteFrame)
+			ArchyDigSiteFrame:SetScale(1)
+			--ArchyDigSiteFrame:CreateBackdrop()
+			--ArchyDigSiteFrame.backdrop:SetParent(UIParent)
+			--ArchyDigSiteFrame.backdrop:Point("TOPLEFT", ArchyArtifactFrame, 0, 0)
+			--ArchyDigSiteFrame.backdrop:Point("BOTTOMRIGHT", ArchyArtifactFrame, 0, 0)
+		end
 	end
 
 	hooksecurefunc(Archy, "UpdateDigSiteFrame", SkinArchyDigSiteFrame)
 
 	if ArchyArtifactFrameSkillBar then
-		U.SkinStatusBar(ArchyArtifactFrameSkillBar)
+			U.SkinStatusBar(ArchyArtifactFrameSkillBar)
 	end
 	U.SkinButton(ArchyDistanceIndicatorFrameSurveyButton)
 end
