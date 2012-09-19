3f2d4845f5ea6018d6eabc797d0a6846055e0195
Azilroka <pheonix@pheonixro.com>
2012-09-19 18:35:22 -0500

    PoMTracker Updated by MaXiMiUS

--- skins/PoMTracker.lua
+++ skins/PoMTracker.lua
@@ -11,17 +11,22 @@ local function SkinPoMTracker(self)
 	pomtracker1:CreateBackdrop("Transparent")
 	pomtracker1:Size(85,15)
 
+	-- pomtracker2 point configuration added by MaXiMiUS
+	pomtracker2:ClearAllPoints()
+	pomtracker2:Point("TOP", pomtracker1, "BOTTOM", 0, -3)
+	pomtracker2:Size(154,20)
+
 	U.SkinFrame(pomtracker2)
 
 	pomtracker3:CreateBackdrop("Transparent")
 	pomtracker3:ClearAllPoints()
-	pomtracker3:Point("TOPLEFT", pomtracker2, "BOTTOMLEFT", 5, -10)
+	pomtracker3:Point("TOPLEFT", pomtracker2, "BOTTOMLEFT", 2, -12) -- Changed from 5, -10 by MaXiMiUS
 	pomtracker3:Size(150,15)
 
 	U.SkinButton(pomtracker3_Button1)
 
 	pomtrackerstatusBar:ClearAllPoints()
-	pomtrackerstatusBar:Point("TOPLEFT", pomtracker2, "BOTTOMLEFT", 5, -2)
+	pomtrackerstatusBar:Point("TOPLEFT", pomtracker2, "BOTTOMLEFT", 2, -3) -- Changed from 5, -2 by MaXiMiUS
 	pomtrackerstatusBar:CreateBackdrop("Transparent")
 
 	for i = 1,6 do
