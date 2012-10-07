0937e5521ab24f7580a1a2e32ce214ce2b3cab79
Azilroka <pheonix@pheonixro.com>
2012-10-07 14:35:14 -0500

    Minimap Holders Detection

--- skins/MiscFixes.lua
+++ skins/MiscFixes.lua
@@ -11,7 +11,7 @@ local MiscFixes = CreateFrame("Frame")
 			if tComboEnergyBar then tComboEnergyBar:SetTemplate("Transparent") end
 		end
 	else
-	Minimap:SetMaskTexture("Interface\\AddOns\\Tukui_ElvUI_Skins\\media\\MinimapMaskSquare")
+	if MMHolder or TukuiMinimap then Minimap:SetMaskTexture("Interface\\AddOns\\Tukui_ElvUI_Skins\\media\\MinimapMaskSquare") end
 	if U.elv then if IsAddOnLoaded("stAddonmanager") then GameMenuFrame:HookScript("OnShow", function() U.SkinButton(GameMenuButtonAddons) end) end end
 	if IsAddOnLoaded("TomTom") then if TomTomBlock then TomTomBlock:SetTemplate("Transparent") end end
 	if IsAddOnLoaded("SymbiosisTip") then SymbiosisTip:HookScript("OnShow", function(self) self:SetTemplate("Transparent") end) end
