aa51a833b29ef73d7d4814eba3d86f43e710a835
Azilroka <pheonix@pheonixro.com>
2012-10-18 19:07:19 -0500

    Bagnon Update Again

--- skins/Bagnon.lua
+++ skins/Bagnon.lua
@@ -3,22 +3,36 @@ local U = unpack(select(2,...))
 local name = 'BagnonSkin'
 local function SkinBagnon(self,event)
 	if event == "BANKFRAME_OPENED" then
-		U.SkinFrame(BagnonFramebank)
-		U.SkinCloseButton(BagnonFramebankCloseButton)
+		if BagnonFramebank then
+			if not bagnonbankonce then
+				bagnonbankonce = true
+				U.SkinFrame(BagnonFramebank)
+				U.SkinCloseButton(BagnonFramebankCloseButton)
+			end
+		end
 	elseif event == "GUILDBANKFRAME_OPENED" then
-		print("gbank")
-		U.SkinFrame(BagnonFrameguildbank)
-		U.SkinCloseButton(BagnonFrameguildbankCloseButton)
+		if BagnonFrameguildbank then
+			if not bagnonguildonce then
+				bagnonguildonce = true
+				U.SkinFrame(BagnonFrameguildbank)
+				U.SkinCloseButton(BagnonFrameguildbankCloseButton)
+			end
+		end
 	elseif event == "VOID_STORAGE_OPEN" then
-		U.SkinFrame(BagnonFramevoidstorage)
-		U.SkinCloseButton(BagnonFramevoidstorageCloseButton)
-	elseif event == "BAG_UPDATE" then
+		if BagnonFramevoidstorage then
+			if not bagnonvoidonce then
+				bagnonvoidonce = true
+				U.SkinFrame(BagnonFramevoidstorage)
+				U.SkinCloseButton(BagnonFramevoidstorageCloseButton)
+			end
+		end
+	end
+	if not bagnononce then
+		bagnononce = true
+		ToggleBackpack()
 		U.SkinFrame(BagnonFrameinventory)
 		U.SkinCloseButton(BagnonFrameinventoryCloseButton)
+		ToggleBackpack()
 	end
-		ToggleBackpack()
-		BagnonFrameinventory:HookScript("OnShow", function() U.SkinFrame(BagnonFrameinventory) end)
-		U.SkinCloseButton(BagnonFrameinventoryCloseButton)
-		ToggleBackpack()
 end
-U.RegisterSkin(name,SkinBagnon,"BAG_UPDATE","BANKFRAME_OPENED","GUILDBANKFRAME_OPENED","VOID_STORAGE_OPEN")
+U.RegisterSkin(name,SkinBagnon,"BANKFRAME_OPENED","GUILDBANKFRAME_OPENED","VOID_STORAGE_OPEN")
