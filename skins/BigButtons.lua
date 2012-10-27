84da7ec6f8477d8a4e317eb6d4d614444bdfcb17
Azilroka <pheonix@pheonixro.com>
2012-10-27 06:58:49 -0500

    BigButtons Update

--- skins/BigButtons.lua
+++ skins/BigButtons.lua
@@ -17,9 +17,11 @@ FishingButton:RegisterEvent("UNIT_INVENTORY_CHANGED")
 FishingButton:SetScript("OnEvent", function(self)
 	if not InCombatLockdown() then
 		if IsEquippedItemType("Fishing Poles") then
+			FishingButton:Show()
 			UIFrameFadeIn(FishingButton, 0.2, FishingButton:GetAlpha(), 1)
 		else
 			UIFrameFadeOut(FishingButton, 0.2, FishingButton:GetAlpha(), 0)
+			FishingButton:Hide()
 		end
 	end
 end)
@@ -39,18 +41,81 @@ local SurveyButton = CreateFrame("Button", "SurveyButton", UIParent, "SecureActi
 	SurveyButton.icon:Size(46)
 	SurveyButton:RegisterEvent("ARTIFACT_DIG_SITE_UPDATED");
 
-local WaterCanButton = CreateFrame("Button", "WaterCanButton", UIParent, "SecureActionButtonTemplate")
-	WaterCanButton:Size(50)
-	WaterCanButton:SetPoint("BOTTOM", 0, 350)
-	WaterCanButton:SetMovable(true)
-	WaterCanButton:SetTemplate("Default")
-	WaterCanButton:SetAttribute("type", "spell");
-	WaterCanButton:SetAttribute("spell", "Survey");
-	--WaterCanButton:Hide()
-	WaterCanButton.icon = SurveyButton:CreateTexture(nil, "OVERLAY")
-	WaterCanButton.icon:SetPoint("CENTER")
-	WaterCanButton.icon:SetTexture(select(3, GetSpellInfo(79104)))
-	WaterCanButton.icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-	WaterCanButton.icon:Size(46)
+-- Sunsong Ranch
+local RustyWateringCanButton = CreateFrame("Button", "RustyWateringCanButton", UIParent, "SecureActionButtonTemplate")
+	RustyWateringCanButton:Size(50)
+	RustyWateringCanButton:SetPoint("CENTER", UIParent, "BOTTOM", -53, 350)
+	RustyWateringCanButton:SetMovable(true)
+	RustyWateringCanButton:SetTemplate("Default")
+	RustyWateringCanButton:SetAttribute("type", "item");
+	RustyWateringCanButton:SetAttribute("item", "Rusty Watering Can");
+	RustyWateringCanButton:CreateShadow()
+	RustyWateringCanButton:Hide()
+	RustyWateringCanButton.icon = RustyWateringCanButton:CreateTexture(nil, "OVERLAY")
+	RustyWateringCanButton.icon:SetPoint("CENTER")
+	RustyWateringCanButton.icon:SetTexture(select(10, GetItemInfo(79104)))
+	RustyWateringCanButton.icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+	RustyWateringCanButton.icon:Size(46)
+	RustyWateringCanButton:RegisterEvent("PLAYER_ENTERING_WORLD")
+	RustyWateringCanButton:RegisterEvent("ZONE_CHANGED")
+	RustyWateringCanButton:RegisterEvent("UNIT_INVENTORY_CHANGED")
+	RustyWateringCanButton:HookScript("OnEvent", function(self) 
+		if (GetSubZoneText() == "Sunsong Ranch" and GetItemCount(79104) == 1) then
+			RustyWateringCanButton:Show()
+		else
+			RustyWateringCanButton:Hide()
+		end
+		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+	end)
 
-if GetZoneText() == "Sunsong Ranch" then WaterCanButton:Show() else WaterCanButton:Hide() end
+local VintageBugSprayerButton = CreateFrame("Button", "VintageBugSprayerButton", UIParent, "SecureActionButtonTemplate")
+	VintageBugSprayerButton:Size(50)
+	VintageBugSprayerButton:SetPoint("CENTER", UIParent, "BOTTOM", 0, 350)
+	VintageBugSprayerButton:SetMovable(true)
+	VintageBugSprayerButton:SetTemplate("Default")
+	VintageBugSprayerButton:SetAttribute("type", "item");
+	VintageBugSprayerButton:SetAttribute("item", "Vintage Bug Sprayer");
+	VintageBugSprayerButton:CreateShadow()
+	VintageBugSprayerButton:Hide()
+	VintageBugSprayerButton.icon = VintageBugSprayerButton:CreateTexture(nil, "OVERLAY")
+	VintageBugSprayerButton.icon:SetPoint("CENTER")
+	VintageBugSprayerButton.icon:SetTexture(select(10, GetItemInfo(80513)))
+	VintageBugSprayerButton.icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+	VintageBugSprayerButton.icon:Size(46)
+	VintageBugSprayerButton:RegisterEvent("PLAYER_ENTERING_WORLD")
+	VintageBugSprayerButton:RegisterEvent("ZONE_CHANGED")
+	VintageBugSprayerButton:RegisterEvent("UNIT_INVENTORY_CHANGED")
+	VintageBugSprayerButton:HookScript("OnEvent", function(self) 
+		if (GetSubZoneText() == "Sunsong Ranch" and GetItemCount(80513) == 1) then
+			VintageBugSprayerButton:Show()
+		else
+			VintageBugSprayerButton:Hide()
+		end
+		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+	end)
+
+local DentedShovelButton = CreateFrame("Button", "DentedShovelButton", UIParent, "SecureActionButtonTemplate")
+	DentedShovelButton:Size(50)
+	DentedShovelButton:SetPoint("CENTER", UIParent, "BOTTOM", 53, 350)
+	DentedShovelButton:SetMovable(true)
+	DentedShovelButton:SetTemplate("Default")
+	DentedShovelButton:SetAttribute("type", "item");
+	DentedShovelButton:SetAttribute("item", "Dented Shovel");
+	DentedShovelButton:CreateShadow()
+	DentedShovelButton:Hide()
+	DentedShovelButton.icon = DentedShovelButton:CreateTexture(nil, "OVERLAY")
+	DentedShovelButton.icon:SetPoint("CENTER")
+	DentedShovelButton.icon:SetTexture(select(10, GetItemInfo(89880)))
+	DentedShovelButton.icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+	DentedShovelButton.icon:Size(46)
+	DentedShovelButton:RegisterEvent("PLAYER_ENTERING_WORLD")
+	DentedShovelButton:RegisterEvent("ZONE_CHANGED")
+	DentedShovelButton:RegisterEvent("UNIT_INVENTORY_CHANGED")
+	DentedShovelButton:HookScript("OnEvent", function(self) 
+		if (GetSubZoneText() == "Sunsong Ranch" and GetItemCount(89880) == 1) then
+			DentedShovelButton:Show()
+		else
+			DentedShovelButton:Hide()
+		end
+		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+	end)
