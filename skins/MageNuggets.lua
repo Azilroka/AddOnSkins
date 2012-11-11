f1d31660609432759a87eb95d5df8ff207f13b5d
Pat <PMorgan133780@gmail.com>
2012-11-11 10:30:09 -0500

    Fully skinned options frame
    gotta love my OCD.
    
    Fix up Pat's magenuggets fixes.

--- skins/MageNuggets.lua
+++ skins/MageNuggets.lua
@@ -83,7 +83,147 @@ local function SkinMageNuggets(self)
 	MageNugClearcast_Frame_Bar:ClearAllPoints()
 	MageNugClearcast_Frame_Bar:Point("TOPLEFT", MageNugClearcast_Frame, "TOPLEFT", 1, -42)
 	MNabCast_Frame:Point("TOPLEFT", MageNugAB_Frame, "TOPLEFT", 0, 18)
-
+	
+	
+	--stupid ass amount of check boxes
+	local checkbuttons = {
+		MageNugOptionsFrame_CheckButtonCC,
+		MageNugOptionsFrame_CheckButton6,
+		MageNugOptionsFrame_CheckButtonMG,
+		MageNugOptionsFrame_CheckButton7,
+		MageNugOptionsFrame_CheckButton8,
+		MageNugOptionsFrame_CheckButtonMGcombat,
+		MageNugOptionsFrame_CheckButtonFB,
+		MageNugOptionsFrame_CheckButtonIE,
+		MageNugOptionsFrame_CheckButton14,
+		MageNugOptionsFrame_CheckButton9,
+		MageNugOptionsFrame_CheckButton13,
+		MageNugOptionsFrame_ABcastCheckButton,
+		MageNugOptionsFrame_CheckButton2,
+		MageNugOptionsFrame_CheckButton11,
+		MageNugOptionsFrame_CheckButton3,
+		MageNugOptionsFrame_IgniteCheckButton,
+		CauterizeCheckButton,
+		MageNugStatMonOptionFrame_CheckButton0,
+		MageNugStatMonOptionFrame_CheckButton1,
+		MageNugStatMonOptionFrame_CheckButton2,
+		MageNugMsgOptionFrame_CheckButton,
+		MageNugMsgOptionFrame_CheckButton2,
+		MageNugMsgOptionFrame_CheckButton3,
+		MageNugMsgOptionFrame_CheckButton4,
+		MageNugMsgOptionFrame_CheckButton5,
+		MageNugMsgOptionFrame_CheckButton6,
+		MageNugSoundOptionFrame_MICheckButton,
+		MageNugSoundOptionFrame_ProcCheckButton,
+		MageNugSoundOptionFrame_PolyCheckButton,
+		MageNugSoundOptionFrame_HotStreakCheckButton,
+		MageNugSoundOptionFrame_ImpactCheckButton,
+		MageNugSoundOptionFrame_FoFCheckButton,
+		MageNugSoundOptionFrame_BrainFreezeCheckButton,
+		MageNugSoundOptionFrame_CauterizeCheckButton,
+		MageNugSoundOptionFrame_TimeWarpCheckButton,
+		MageNugPriestOptionFrame_CheckButton3,
+		MageNugPriestOptionFrame_CheckButton2,
+		MageNugPriestOptionFrame_CheckButton0,
+		MageNugMoonkinOptionFrame_CheckButton,
+		MageNugMoonkinOptionFrame_CheckButton0,
+		MageNugMoonkinOptionFrame_CheckButton1,
+		MageNugMoonkinOptionFrame_CheckButtonMin,
+		MageNugMoonkinOptionFrame_CheckButton3,
+		MageNugMoonkinOptionFrame_CheckButtonAnchor,
+		MageNugMoonkinOptionFrame_CastCheckButton,
+		MageNugMoonkinOptionFrame_ProcCheckButton,
+		MageNugMoonkinOptionFrame_CheckButton2,
+		MageNugOption2Frame_MinimapCheckButton,
+		MageNugOption2Frame_CameraCheckButton,
+		MageNugOption2Frame_ConsoleTextCheckButton,
+		MageNugOption2Frame_LockFramesCheckButton,
+		MageNugOption2Frame_CheckButtonTT,
+		MageNugOption2Frame_ClickThruCheckButton,
+		MageNugCooldownFrame_apButton,
+		MageNugCooldownFrame_bwButton,
+		MageNugCooldownFrame_cbButton,
+		MageNugCooldownFrame_csButton,
+		MageNugCooldownFrame_dfButton,
+		MageNugCooldownFrame_dbButton,
+		MageNugCooldownFrame_mwButton,
+		MageNugCooldownFrame_frzButton,
+		MageNugCooldownFrame_msButton,
+		MageNugCooldownFrame_ibrButton,
+		MageNugCooldownFrame_evoButton,
+		MageNugCooldownFrame_ivButton,
+		MageNugCooldownFrame_cdButton,
+		MageNugCooldownFrame_starfallButton,
+		MageNugCooldownFrame_treantButton,
+		MageNugCooldownFrame_miButton,
+	}
+		
+	for _,b in pairs(checkbuttons) do
+		XS:SkinCheckBox(b)
+	end
+	
+	-- Slider frames
+	local sliders = {
+		MageNugOptionsFrame_Slider5,
+		MageNugOptionsFrame_InvokersSlider,
+		MageNugOptionsFrame_Slider1,
+		MageNugOptionsFrame_Slider3,
+		MageNugOptionsFrame_Slider2,
+		MageNugOptionsFrame_Slider4,
+		MageNugStatMonOptionFrame_SPSizeSlider,
+		MageNugStatMonOptionFrame_BorderSlider,
+		MageNugStatMonOptionFrame_TransparencySlider,
+		MageNugMoonkinOptionFrame_Slider,
+		MageNugMoonkinOptionFrame_Slider1,
+		MageNugCooldownFrame_Slider1,
+	}
+	
+	for _,s in pairs(sliders) do
+		XS:SkinSliderFrame(s)
+	end
+	
+	-- Edit Boxes
+	local editbox = {
+		SlowFallMsgEditBox,
+		SlowFallMsgEditBox2,
+		SlowFallMsgEditBox3,
+		FocMagNotifyEditBox,
+		FocMagNotifyEditBox2,
+		FocMagNotifyEditBox3,
+		FocMagThankEditBox,
+		FocMagThankEditBox2,
+		InnervThankEditBox,
+		InnervThankEditBox2,
+		PowerInfusionEditBox,
+		DarkIntentEditBox,
+		MageNugSoundOptionFrame_MISoundEditBox,
+		MageNugSoundOptionFrame_ProcSoundEditBox,
+		MageNugSoundOptionFrame_PolySoundEditBox,
+		MageNugSoundOptionFrame_HotStreakSoundEditBox,
+		MageNugSoundOptionFrame_ImpactSoundEditBox,
+		MageNugSoundOptionFrame_FoFSoundEditBox,
+		MageNugSoundOptionFrame_BrainFreezeSoundEditBox,
+		MageNugSoundOptionFrame_CauterizeSoundEditBox,
+		MageNugSoundOptionFrame_TimeWarpSoundEditBox,
+		MageNugPriestOptionFrame_EditBox3,
+		MageNugPriestOptionFrame_EditBox2,
+		MageNugPriestOptionFrame_EditBox1,
+		MageNugMoonkinOptionFrame_SoundEditBox,
+		MageNugMoonkinOptionFrame_InnervateEditBox,
+	}
+	
+	for _,f in pairs(editbox) do
+		XS:SkinEditBox(f)
+		f:SetHeight(15)
+	end
+	
+	-- Buttons
+	XS:SkinButton(MageNugStatMonOptionFrameBlackBackdropButton)
+	XS:SkinButton(MageNugMoonkinOptionFrame_Button)
+	XS:SkinButton(MageNugOption2FrameButton1)
+	XS:SkinButton(MageNugOption2FrameButton2)
+	XS:SkinButton(MageNugOption2FrameButton3)
+	
 end
 
 AS:RegisterSkin(name,SkinMageNuggets)
