8d2670abffad8b7a6388e5792989bb9c0bb1fc5b
Michael James <darktemplar9900@yahoo.com>
2013-01-28 20:41:24 -0600

    Ovale/CombustionHelper Fixed

--- skins/Ovale.lua
+++ skins/Ovale.lua
@@ -3,17 +3,16 @@ local AS = E:GetModule('AddOnSkins')
 local S = E:GetModule('Skins')
 
 local name = "OvaleSkin"
-local function SkinOvale(self)
-	E.Delay(15, function()
-		Icon1n1:GetParent():StripTextures()
-		for i = 1, 3 do
-			AS:SkinBackdropFrame(_G["Icon"..i.."n1"], true)
-			_G["Icon"..i.."n1"].backdrop:SetFrameLevel(0)
-			_G["Icon"..i.."n1"].backdrop:SetAllPoints()
-			--_G["Icon"..i.."n1"].backdrop:SetScale(c["general"].uiscale)
-			_G["Icon"..i.."n1"].icone:SetInside(_G["Icon"..i.."n1"].backdrop, 1, 1)
-		end
-	end)
+local function SkinOvale(self, event)
+	if event == "PLAYER_ENTERING_WORLD" then return end
+	if not IsAddOnLoaded("Ovale") then return end
+	Icon1n1:HookScript("OnUpdate", function(s) s:GetParent():StripTextures() end)
+	for i = 1, 3 do
+		AS:SkinBackdropFrame(_G["Icon"..i.."n1"], true)
+		_G["Icon"..i.."n1"].backdrop:SetFrameLevel(0)
+		_G["Icon"..i.."n1"].backdrop:SetAllPoints()
+		_G["Icon"..i.."n1"].icone:SetInside(_G["Icon"..i.."n1"].backdrop, 1, 1)
+	end
 end
 
-AS:RegisterSkin(name,SkinOvale)
+AS:RegisterSkin(name,SkinOvale, "ADDON_LOADED")
