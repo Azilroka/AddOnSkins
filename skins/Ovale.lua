1b3bcbe5db4d7e8fd7445ddea31a176d5599500b
Michael James <darktemplar9900@yahoo.com>
2013-01-29 03:21:25 -0600

    Ovale

--- skins/Ovale.lua
+++ skins/Ovale.lua
@@ -6,13 +6,15 @@ local name = "OvaleSkin"
 local function SkinOvale(self, event)
 	if event == "PLAYER_ENTERING_WORLD" then return end
 	if not IsAddOnLoaded("Ovale") then return end
-	Icon1n1:HookScript("OnUpdate", function(s) s:GetParent():StripTextures() end)
-	for i = 1, 3 do
-		AS:SkinBackdropFrame(_G["Icon"..i.."n1"], true)
-		_G["Icon"..i.."n1"].backdrop:SetFrameLevel(0)
-		_G["Icon"..i.."n1"].backdrop:SetAllPoints()
-		_G["Icon"..i.."n1"].icone:SetInside(_G["Icon"..i.."n1"].backdrop, 1, 1)
-	end
+	E:Delay(15, function()
+		Icon1n1:HookScript("OnUpdate", function(s) s:GetParent():StripTextures() end)
+		for i = 1, 3 do
+			AS:SkinBackdropFrame(_G["Icon"..i.."n1"], true)
+			_G["Icon"..i.."n1"].backdrop:SetFrameLevel(0)
+			_G["Icon"..i.."n1"].backdrop:SetAllPoints()
+			_G["Icon"..i.."n1"].icone:SetInside(_G["Icon"..i.."n1"].backdrop, 1, 1)
+		end
+	end)
 end
 
 AS:RegisterSkin(name,SkinOvale, "ADDON_LOADED")
