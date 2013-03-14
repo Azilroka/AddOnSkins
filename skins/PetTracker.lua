ea3d5fa0f674e99801ac5eca0f1c368180527ba9
Michael Tindal <mj.tindal@icloud.com>
2013-03-13 22:10:41 -0400

    Fix FlightMapEnhanced, EasyMail, and PetTracker skins.

--- skins/PetTracker.lua
+++ skins/PetTracker.lua
@@ -4,7 +4,7 @@ local S = E:GetModule('Skins')
 
 local name = "PetTrackerSkin"
 local function SkinPetTracker(self)
-	local frame = PetTracker.ZoneTracker.usedFrames[1].Anchor -- Used to just be Anchor, but he overwrote __index and now we have to do this
+	local frame = PetTracker.Tracker.usedFrames[1].Anchor -- Used to just be Anchor, but he overwrote __index and now we have to do this
 	AS:SkinFrame(frame.Overlay)
 	for i = 1, PetTracker.MaxQuality do
 		AS:SkinStatusBar(frame[i])
