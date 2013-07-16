49c5144e4a12829ca376b65d433bdd6d68b15ac8
Michael James <darktemplar9900@yahoo.com>
2013-07-16 15:19:33 -0500

    RCLootCouncil

--- skins/RCLootCouncil.lua
+++ skins/RCLootCouncil.lua
@@ -5,6 +5,7 @@ local S = E:GetModule('Skins')
 local name = "RCLootCouncilSkin"
 function AS:SkinRCLootCouncil()
 
+	--MainFrame:SetScale(1)
 	AS:SkinFrame(MainFrame, "Transparent", true)
 	AS:SkinFrame(ContentFrame, "Default")
 	S:HandleButton(BtClose)
@@ -15,32 +16,36 @@ function AS:SkinRCLootCouncil()
 	S:HandleScrollBar(ContentFrameScrollBar)
 
 	for i = 1,10 do
-		local v = _G["ContentFrameEntry"..i.."BtVote"]
-		if v then S:HandleButton(_G["ContentFrameEntry"..i.."BtVote"])
+		if _G["ContentFrameEntry"..i.."BtVote"] then
+			S:HandleButton(_G["ContentFrameEntry"..i.."BtVote"])
 		end
 	end	
 
-	--LootFrame
-	for j = 1,10 do
-		for i = 1,8 do
-			if _G["RCLootFrameEntry"..j.."Button"..i] then S:HandleButton(_G["RCLootFrameEntry"..j.."Button"..i]) end
-		end
-	end	
+	RCLootFrame:StripTextures()
+	local function Loot_OnShow(self, event, ...)
+		for i = 1,5 do
+			if _G["RCLootFrameEntry"..i] then
+				--_G["RCLootFrameEntry"..i]:SetScale(1)
+				AS:SkinFrame(_G["RCLootFrameEntry"..i], "Transparent", true)
+				
+				for j = 1,8 do
+					if _G["RCLootFrameEntry"..i.."Button"..j] then
+						S:HandleButton(_G["RCLootFrameEntry"..i.."Button"..j])
+					end
+				end
+			end
+		end	
+	end
+	RCLootFrame:HookScript("OnShow", Loot_OnShow)
 
-	for i = 1,10 do
-		if _G["RCLootFrameEntry"..i] then AS:SkinFrame(_G["RCLootFrameEntry"..i], "Transparent", true) end
-	end	
-
-	--LootHistoryFrame
 	AS:SkinFrame(RCLootHistoryFrame)
 	AS:SkinFrame(RCLootHistoryFrameScrollFrame, "Default")
+	S:HandleCheckBox(RCLootHistoryFrameFilterPasses)
 	S:HandleButton(RCLootHistoryFrameButtonClose)
 	S:HandleScrollBar(RCLootHistoryFrameScrollFrameScrollBar)
 
-	--CheckVersionFrame
 	AS:SkinFrame(RCVersionFrame)
 	AS:SkinFrame(RCVersionFrameContentFrame, "Default")
-
 	S:HandleButton(ButtonGuild)
 	S:HandleButton(RaidButton)
 	S:HandleButton(CloseButton)
