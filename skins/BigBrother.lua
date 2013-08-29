4b74d14a8924fa38f9233cb06b192b03c326720b
Michael James <darktemplar9900@yahoo.com>
2013-08-29 10:40:59 -0500

    BigBrother

--- skins/BigBrother.lua
+++ skins/BigBrother.lua
@@ -2,18 +2,14 @@ local AS = ElvUI[1]:GetModule('AddOnSkins')
 
 local name = "BigBrotherSkin"
 function AS:SkinBigBrother()
-	BigBrother:ToggleBuffWindow()
-	AS:SkinFrame(BigBrother_BuffWindow)
-	AS:SkinCloseButton(BigBrother_BuffWindow.CloseButton)
-	AS:SkinButton(BigBrother_BuffWindow.LeftButton)
-	BigBrother_BuffWindow.LeftButton:FontString("text", AS.PixelFont, AS.DataTextFontSize) --"MONOCHROMEOUTLINE"
-	BigBrother_BuffWindow.LeftButton.text:SetText("<")
-	BigBrother_BuffWindow.LeftButton.text:SetPoint("CENTER", 1, 1)
-	AS:SkinButton(BigBrother_BuffWindow.RightButton)
-	BigBrother_BuffWindow.RightButton:FontString("text", AS.PixelFont, AS.DataTextFontSize) --"MONOCHROMEOUTLINE"
-	BigBrother_BuffWindow.RightButton.text:SetText(">")
-	BigBrother_BuffWindow.RightButton.text:SetPoint("CENTER", 1, 1)
-	BigBrother:ToggleBuffWindow()
+	hooksecurefunc(BigBrother, 'ToggleBuffWindow', function()
+		if BigBrother_BuffWindow.isSkinned then return end
+		AS:SkinFrame(BigBrother_BuffWindow)
+		AS:SkinCloseButton(BigBrother_BuffWindow.CloseButton)
+		AS:Desaturate(BigBrother_BuffWindow.LeftButton)
+		AS:Desaturate(BigBrother_BuffWindow.RightButton)
+		BigBrother_BuffWindow.isSkinned = true
+	end)
 end
 
 AS:RegisterSkin(name, AS.SkinBigBrother)
