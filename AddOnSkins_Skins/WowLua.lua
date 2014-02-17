e6eb64d411447fef48199491e6c21af25fe48fad
Michael James <darktemplar9900@yahoo.com>
2014-02-17 15:47:36 -0600

    License's / WowLua

--- AddOnSkins_Skins/WowLua.lua
+++ AddOnSkins_Skins/WowLua.lua
@@ -5,8 +5,8 @@ if not AS:CheckAddOn('Wowlua') then return end
 local name = 'WowLuaSkin'
 function AS:SkinWowLua()
 	AS:SkinFrame(WowLuaFrame)
-	WowLuaFrameLineNumScrollFrame:StripTextures(True)
-	WowLuaFrameResizeBar:StripTextures(True)
+	WowLuaFrameLineNumScrollFrame:StripTextures(true)
+	WowLuaFrameResizeBar:StripTextures(true)
 	WowLuaFrameResizeBar:Height(10)
 	AS:SkinCloseButton(WowLuaButton_Close)
 	WowLuaButton_Close:Point('TOPRIGHT', WowLuaFrame, 'TOPRIGHT', 0 , 0)
@@ -28,6 +28,30 @@ function AS:SkinWowLua()
 	AS:SkinBackdropFrame(WowLuaFrameCommand.bg1)
 	WowLuaFrameCommand.bg1:Point('TOPLEFT', 0, -4)
 	WowLuaFrameCommand.bg1:Point('BOTTOMRIGHT', -12, 2)
+
+	local Buttons = {
+		WowLuaButton_New,
+		WowLuaButton_Open,
+		WowLuaButton_Save,
+		WowLuaButton_Undo,
+		WowLuaButton_Redo,
+		WowLuaButton_Delete,
+		WowLuaButton_Lock,
+		WowLuaButton_Unlock,
+		WowLuaButton_Config,
+		WowLuaButton_Previous,
+		WowLuaButton_Next,
+		WowLuaButton_Run,
+	}
+	
+	for _, object in pairs(Buttons) do
+		object:CreateBackdrop()
+		object:GetNormalTexture():SetTexCoord(.1,.9,.1,.9)
+		if object:GetDisabledTexture() then
+			object:GetDisabledTexture():SetTexCoord(.1,.9,.1,.9)
+		end
+		object:StyleButton()
+	end
 end
 
 AS:RegisterSkin(name, AS.SkinWowLua)
