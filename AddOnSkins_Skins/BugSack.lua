7c64bdd870f2f52554e65379ad48f4677485da11
Azilroka <darktemplar9900@yahoo.com>
2014-02-23 23:35:54 -0600

    BugSack

---
+++ AddOnSkins_Skins/BugSack.lua
@@ -0,0 +1,27 @@
+local AS = unpack(AddOnSkins)
+
+if not AS:CheckAddOn('BugSack') then return end
+
+local name = 'BugSackSkin'
+function AS:SkinBugSack(event, addon)
+
+	hooksecurefunc(BugSack, "OpenSack", function()
+		if BugSackFrame.IsSkinned then return end
+		AS:SkinFrame(BugSackFrame)
+		AS:SkinTab(BugSackTabAll)
+		AS:SkinTab(BugSackTabSession)
+		AS:SkinTab(BugSackTabLast)
+		AS:SkinButton(BugSackNextButton)
+		AS:SkinButton(BugSackSendButton)
+		AS:SkinButton(BugSackPrevButton)
+		AS:SkinScrollBar(BugSackScrollScrollBar)
+		for _, child in pairs({frame:GetChildren()}) do
+			if (child:GetObjectType() == "Button" and child:GetScript("OnClick") == BugSack.CloseSack) then
+				AS:SkinCloseButton(child)
+			end
+		end
+		BugSackFrame.IsSkinned = true
+	end)
+end
+
+AS:RegisterSkin(name, AS.SkinBugSack)
