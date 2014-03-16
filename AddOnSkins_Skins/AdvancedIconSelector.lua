6d7d0b529a978140357d9363905611012b4a5274
Azilroka <darktemplar9900@yahoo.com>
2014-03-16 01:21:05 -0500

    AdvancedIconSelector

---
+++ AddOnSkins_Skins/AdvancedIconSelector.lua
@@ -0,0 +1,46 @@
+local AS = unpack(AddOnSkins)
+
+if not AS:CheckAddOn('AdvancedIconSelector') then return end
+
+local name = "AdvancedIconSelectorSkin"
+function AS:SkinAdvancedIconSelector()
+	local function SkinPopup(Name)
+		local Popup = Name
+		if Popup.IsSkinned then return end
+		AS:SkinFrame(Popup)
+		AS:SkinEditBox(Popup.editBox)
+		Popup.editBox:DisableDrawLayer('BACKGROUND')
+		AS:SkinScrollBar(Popup.iconsFrame.scrollFrame.ScrollBar)
+		Popup.iconsFrame:HookScript('OnUpdate', function(self)
+			for i = 1, 1024 do
+				if self.icons[i] and self.icons[i]:GetNormalTexture() then
+					AS:SkinTexture(self.icons[i]:GetNormalTexture())
+				end
+				if self.icons[i] and not self.icons[i].IsStyled then
+					self.icons[i]:StyleButton()
+					self.icons[i].IsStyled = true
+				end
+			end
+		end)
+		AS:SkinCloseButton(Popup.closeButton)
+		AS:SkinEditBox(Popup.searchBox)
+		Popup.searchBox:DisableDrawLayer('BACKGROUND')
+		AS:SkinButton(Popup.cancelButton)
+		if Popup.okButton then
+			AS:SkinButton(Popup.okButton)
+		end
+		Popup.IsSkinned = true
+	end
+
+	hooksecurefunc("CharacterFrame_Expand", function()
+		SkinPopup(_G["GearManagerDialogPopup"])
+	end)
+	hooksecurefunc("MacroFrame_LoadUI", function()
+		SkinPopup(_G["MacroPopupFrame"])
+	end)
+	hooksecurefunc("GuildBankFrame_LoadUI", function()
+		SkinPopup(_G["GuildBankPopupFrame"])
+	end)
+end
+
+AS:RegisterSkin(name, AS.SkinAdvancedIconSelector)
