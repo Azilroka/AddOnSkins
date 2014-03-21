099ca4ca9301286c87776352bd864998b47c2973
Azilroka <darktemplar9900@yahoo.com>
2014-03-21 00:22:09 -0500

    Picky Simpy

--- AddOnSkins_Skins/BetterQuest.lua
+++ AddOnSkins_Skins/BetterQuest.lua
@@ -8,22 +8,21 @@ function AS:SkinBetterQuest()
 	hooksecurefunc(BetterQuest, 'UpdateTrackers', function(self)
 		for id, _ in pairs(self.db.profile.trackerNames) do
 			local settings = self.db.profile.trackers[id]
-			if settings then
-				if settings.enabled then
-					self:UpdateTracker(id)
-					if not _G["BetterQuest_"..id].IsSkinned then
-						AS:SkinBackdropFrame(_G["BetterQuest_"..id])
-						_G["BetterQuest_"..id]:SetBackdrop(nil)
-						_G["BetterQuest_"..id].SetBackdrop = AS.Noop
-						_G["BetterQuest_"..id].IsSkinned = true
-					end
-					for i = 1, 100 do
-						local Line = i == 1 and _G["BetterQuest_"..id..'_Line'..i..'_Right'] or _G["BetterQuest_"..id..'_Line'..i..'_Left']
-						local LineText = Line and Line:GetText() or nil
-						if LineText then
-							LineText = gsub(LineText, "|TInterface\\Buttons\\UI%-(%a-)Button%-Up:%d-|t", function(x) return x == 'Minus' and '|cFFFFFFFF-|r ' or x == 'Plus' and '|cFFFFFFFF+|r ' or '' end)
-							Line:SetText(LineText)
-						end
+			if settings and settings.enabled then
+				self:UpdateTracker(id)
+				local Tracker = _G["BetterQuest_"..id]
+				if not Tracker.IsSkinned then
+					AS:SkinBackdropFrame(Tracker)
+					Tracker:SetBackdrop(nil)
+					Tracker.SetBackdrop = AS.Noop
+					Tracker.IsSkinned = true
+				end
+				for i = 1, 100 do
+					local Line = i == 1 and _G["BetterQuest_"..id..'_Line'..i..'_Right'] or _G["BetterQuest_"..id..'_Line'..i..'_Left']
+					local LineText = Line and Line:GetText() or nil
+					if LineText then
+						LineText = gsub(LineText, "|TInterface\\Buttons\\UI%-(%a-)Button%-Up:%d-|t", function(x) local z = (x == 'Minus' and '-' or x == 'Plus' and '+'); return z and '|cFFFFFFFF'..z..' |r' or '' end)
+						Line:SetText(LineText)
 					end
 				end
 			end
