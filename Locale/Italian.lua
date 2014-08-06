5413c00a24b1880858938e2e9bf8cf91068dfd66
Azilroka <darktemplar9900@yahoo.com>
2014-08-06 15:55:40 -0500

    Italian Locale

---
+++ Locale/Italian.lua
@@ -0,0 +1,17 @@
+local AS = unpack(AddOnSkins)
+
+function AS:UpdateLocale_itIT()
+    local L = AS.Locale
+
+    L.DataTextToggle = "Alterna"
+    L.DataTextEmbed = "Integra"
+
+    L.ToggleSkinDesc = "Abilita/Disabilita questa customizzazione."
+    L.ToggleEmbedDesc = "Abilita/Disabilita questa integrazione."
+    L.ToggleOptionDesc = "Abilita/Disabilita questa opzione."
+    L.MiscellaneousOptions = "Opzioni Generiche"
+    L.ToggleLeftChat = "Alterna Pannello Sinistro della Chat"
+    L.ToggleRightChat = "Alterna Pannello Destro della Chat"
+    L.ToggleEmbed = "Alterna Integrazione dell AddOn"
+    L.ToggleOptions = "Alterna Customizzazioni/Opzioni Aggiuntive"
+end
