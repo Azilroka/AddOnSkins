c9b7d13544e1b53f2c6eac002ea19bf5a785a294
Azilroka <darktemplar9900@yahoo.com>
2014-10-17 14:48:27 -0500

    TitanPanel #116

--- Skins/TitanPanel.lua
+++ Skins/TitanPanel.lua
@@ -4,13 +4,15 @@ if not AS:CheckAddOn('Titan') then return end
 
 local name = 'TitanPanelSkin'
 function AS:SkinTitanPanel()
-	AS:SkinFrame(Titan_Bar__Display_Bar, 'Default')
-	AS:SkinFrame(Titan_Bar__Display_Bar2, 'Default')
-	AS:SkinFrame(Titan_Bar__Display_AuxBar, 'Default')
-	AS:SkinFrame(Titan_Bar__Display_AuxBar2, 'Default')
-	if RaidUtility_ShowButton then
-		RaidUtility_ShowButton:SetFrameStrata('TOOLTIP')
-	end
+	AS:Delay(1, function()
+		AS:SkinFrame(Titan_Bar__Display_Bar, 'Default', nil, true)
+		AS:SkinFrame(Titan_Bar__Display_Bar2, 'Default', nil, true)
+		AS:SkinFrame(Titan_Bar__Display_AuxBar, 'Default', nil, true)
+		AS:SkinFrame(Titan_Bar__Display_AuxBar2, 'Default', nil, true)
+		if RaidUtility_ShowButton then
+			RaidUtility_ShowButton:SetFrameStrata('TOOLTIP')
+		end
+	end)
 end
 
 AS:RegisterSkin(name, AS.SkinTitanPanel)
