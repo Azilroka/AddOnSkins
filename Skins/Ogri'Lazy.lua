d146ddc5fcbdbfca2b3dc65019c48bd1e355ab99
Azilroka <darktemplar9900@yahoo.com>
2014-10-22 22:14:53 -0500

    Ogri'Lazy

--- Skins/Ogri'Lazy.lua
+++ Skins/Ogri'Lazy.lua
@@ -5,7 +5,13 @@ if not AS:CheckAddOn("Ogri'Lazy") then return end
 local name = 'OgriLazySkin'
 function AS:SkinOgriLazy()
 	AS:SkinFrame(Relic_View)
-	AS:SkinCloseButton(Relic_ViewCloseButton)
+	AS:SkinCloseButton(Relic_View.close)
+	for i = 1, 4 do
+		AS:SkinTexture(Relic_View.set[i].tex)
+	end
+	for i = 1, 10 do
+		AS:SkinTexture(Relic_View.replay[i].tex)
+	end
 end
 
 AS:RegisterSkin(name, AS.SkinOgriLazy)
