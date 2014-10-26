b4738370b5af7ed4f2d20c54a723f19f4e2b6da8
Azilroka <darktemplar9900@yahoo.com>
2014-10-26 14:57:37 -0500

    CritLine...

--- Skins/Critline.lua
+++ Skins/Critline.lua
@@ -4,9 +4,16 @@ if not AS:CheckAddOn('Critline') then return end
 
 local name = 'CritlineSkin'
 function AS:SkinCritline()
-	AS:SkinBackdropFrame(Critline.display)
-	local Backdrop = Critline.display.backdrop or Critline.display.Backdrop
-	Backdrop:SetFrameStrata('BACKGROUND')
+	local CritLineDisplay = Critline:GetModule('Display')
+	AS:SkinFrame(CritLineDisplay)
+	for i = 1, CritLineDisplay:GetNumChildren() do
+		local object = select(i, CritLineDisplay:GetChildren())
+		if object and object:IsObjectType('Frame') then
+			object:SetFrameStrata('MEDIUM')
+			object:SetBackdrop(nil)
+			AS:SkinTexture(object.icon)
+		end
+	end
 end
 
 AS:RegisterSkin(name, AS.SkinCritline)
