90c67c56cc9ccdb18960c982c941c8f4f2c567f4
Azilroka <darktemplar9900@yahoo.com>
2014-11-09 03:50:09 -0600

    Bags

--- Skins/Blizzard/Bags.lua
+++ Skins/Blizzard/Bags.lua
@@ -104,117 +104,13 @@ function AS:Blizzard_Bags()
 	end)
 
 	for i = 1, 3 do
-		AS:SkinTexture(_G["BackpackTokenFrameToken"..i].icon)
-		_G["BackpackTokenFrameToken"..i]:CreateBackdrop("Default")
-		_G["BackpackTokenFrameToken"..i].Backdrop:SetOutside(_G["BackpackTokenFrameToken"..i].icon)
-		_G["BackpackTokenFrameToken"..i].icon:Point("LEFT", _G["BackpackTokenFrameToken"..i].count, "RIGHT", 3, 0)
+		local Token = _G["BackpackTokenFrameToken"..i]
+		AS:SkinTexture(Token.icon)
+		Token:CreateBackdrop("Default")
+		local Backdrop = Token.Backdrop or Token.backdrop
+		Backdrop:SetOutside(Token.icon)
+		Token.icon:Point("LEFT", Token.count, "RIGHT", 3, 0)
 	end
-	-- BAGS
-	--[[
-	for i = 1, 5 do
-		local bag = _G["ContainerFrame"..i]
-		if bag then
-			bag:StripTextures(true)
-
-			_G["ContainerFrame"..i.."CloseButton"]:SkinCloseButton(_G["ContainerFrame"..i.."CloseButton"].backdrop)
-
-			--Slots
-			for j = 1, 36 do
-				local icon = _G["ContainerFrame"..i.."Item"..j.."IconTexture"]
-				local slots = _G["ContainerFrame"..i.."Item"..j]
-
-				slots:SkinButton()
-				icon:SetTexCoord(.08, .92, .08, .92)
-				icon:ClearAllPoints()
-				icon:SetAllPoints()
-				icon:Point("TOPLEFT", slots, 2, -2)
-				icon:Point("BOTTOMRIGHT", slots, -2, 2)
-			end
-		end
-		
-	end
-	
-	BagItemSearchBox:Size(159, 15)
-	BagItemSearchBox:ClearAllPoints()
-	BagItemSearchBox:Point("TOPLEFT", 19, -29)
-	BagItemSearchBox.ClearAllPoints = T.dummy
-	BagItemSearchBox.SetPoint = T.dummy
-	BagItemSearchBox:SkinEditBox()
-
-	-- BANK
-	local bank = BankFrame
-	if bank then
-		bank:StripTextures(true)
-		bank:CreateBackdrop("Default")
-		bank.backdrop:CreateShadow("Default")
-		BankFrameCloseButton:SkinCloseButton(BankFrameCloseButton.backdrop)
-		BankFrameMoneyFrameBorder:StripTextures()
-		BankFrameMoneyFrameInset:StripTextures()
-
-		BankFramePurchaseButton:SkinButton()
-		BankFramePurchaseButton:Height(22)
-		
-		BankItemSearchBox:Size(159, 15)
-		BankItemSearchBox:ClearAllPoints()
-		BankItemSearchBox:Point("BOTTOMRIGHT", -29, 70)
-		BankItemSearchBox.ClearAllPoints = T.dummy
-		BankItemSearchBox.SetPoint = T.dummy
-		BankItemSearchBox:SkinEditBox()
-
-		-- Bank Bags
-		for i = 1, 7 do
-			local bankbags = _G["BankFrameBag"..i]
-			local highlight = _G["BankFrameBag"..i.."HighlightFrameTexture"];
-			local icon = _G["BankFrameBag"..i.."IconTexture"]
-
-			bankbags:SkinButton()
-			bankbags:StyleButton()
-			highlight:Kill()
-			icon:SetTexCoord(.08, .92, .08, .92)
-			icon:ClearAllPoints()
-			icon:SetAllPoints()
-			icon:Point("TOPLEFT", bankbags, 2, -2)
-			icon:Point("BOTTOMRIGHT", bankbags, -2, 2)
-		end
-
-		-- Bank Slots
-		for i = 1, 28 do
-			local slots = _G["BankFrameItem"..i]
-			local icon = _G["BankFrameItem"..i.."IconTexture"]
-			slots:SkinButton()
-			icon:SetTexCoord(.08, .92, .08, .92)
-			icon:ClearAllPoints()
-			icon:SetAllPoints()
-			icon:Point("TOPLEFT", slots, 2, -2)
-			icon:Point("BOTTOMRIGHT", slots, -2, 2)
-		end
-
-		-- Bank Bags Frame
-		for i = 6, 12 do
-			local bag = _G["ContainerFrame"..i]
-			if bag then
-				bag:StripTextures(true)
-				bag:CreateBackdrop("Default")
-				bag.backdrop:CreateShadow("Default")
-				bag.backdrop:Point("TOPLEFT", 4, -2)
-				bag.backdrop:Point("BOTTOMRIGHT", 1, 1)
-				_G["ContainerFrame"..i.."CloseButton"]:SkinCloseButton(_G["ContainerFrame"..i.."CloseButton"].backdrop)
-
-				--Slots
-				for j = 1, 36 do
-					local icon = _G["ContainerFrame"..i.."Item"..j.."IconTexture"]
-					local slots = _G["ContainerFrame"..i.."Item"..j]
-
-					slots:SkinButton()
-					icon:SetTexCoord(.08, .92, .08, .92)
-					icon:ClearAllPoints()
-					icon:SetAllPoints()
-					icon:Point("TOPLEFT", slots, 2, -2)
-					icon:Point("BOTTOMRIGHT", slots, -2, 2)
-				end
-			end
-		end
-	end]]
 end
 
 AS:RegisterSkin(name, AS.Blizzard_Bags)
