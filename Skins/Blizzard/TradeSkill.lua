59dbbcb82db65351e6cf2efdb83c847dad287fce
Azilroka <darktemplar9900@yahoo.com>
2014-12-04 16:24:11 -0600

    TradeSkillDW

--- Skins/Blizzard/TradeSkill.lua
+++ Skins/Blizzard/TradeSkill.lua
@@ -1,144 +1,146 @@
 local AS = unpack(AddOnSkins)
 
 function AS:Blizzard_TradeSkill(event, addon)
-	if addon ~= 'Blizzard_TradeSkillUI' then return end
-	AS:SkinFrame(TradeSkillFrame, nil, nil, true)
-	AS:CreateShadow(TradeSkillFrame)
-	AS:SkinStatusBar(TradeSkillRankFrame)
-	TradeSkillFrame:Height(TradeSkillFrame:GetHeight() + 12)
+	if addon == 'Blizzard_TradeSkillUI' or IsAddOnLoaded('Blizzard_TradeSkillUI') then
+		AS:SkinFrame(TradeSkillFrame, nil, nil, true)
+		AS:CreateShadow(TradeSkillFrame)
+		AS:SkinStatusBar(TradeSkillRankFrame)
+		TradeSkillFrame:Height(TradeSkillFrame:GetHeight() + 12)
 
-	AS:SkinBackdropFrame(TradeSkillListScrollFrame)
-	AS:SkinBackdropFrame(TradeSkillDetailScrollFrame)
-	AS:StripTextures(TradeSkillFrameInset)
-	AS:StripTextures(TradeSkillExpandButtonFrame)
-	AS:StripTextures(TradeSkillDetailScrollChildFrame)
+		AS:SkinBackdropFrame(TradeSkillListScrollFrame)
+		AS:SkinBackdropFrame(TradeSkillDetailScrollFrame)
+		AS:StripTextures(TradeSkillFrameInset)
+		AS:StripTextures(TradeSkillExpandButtonFrame)
+		AS:StripTextures(TradeSkillDetailScrollChildFrame)
 
-	AS:SkinButton(TradeSkillCreateButton, true)
-	AS:SkinButton(TradeSkillCancelButton, true)
-	AS:StripTextures(TradeSkillFilterButton, true)
-	AS:SkinButton(TradeSkillFilterButton, true)
-	AS:SkinButton(TradeSkillCreateAllButton, true)
-	AS:SkinButton(TradeSkillViewGuildCraftersButton, true)
+		AS:SkinButton(TradeSkillCreateButton, true)
+		AS:SkinButton(TradeSkillCancelButton, true)
+		AS:StripTextures(TradeSkillFilterButton, true)
+		AS:SkinButton(TradeSkillFilterButton, true)
+		AS:SkinButton(TradeSkillCreateAllButton, true)
+		AS:SkinButton(TradeSkillViewGuildCraftersButton, true)
 
-	AS:SkinFrame(TradeSkillGuildFrame)
-	TradeSkillGuildFrame:Point("BOTTOMLEFT", TradeSkillFrame, "BOTTOMRIGHT", 3, 19)
-	AS:SkinFrame(TradeSkillGuildFrameContainer)
-	AS:SkinCloseButton(TradeSkillGuildFrameCloseButton)
-	
-	TradeSkillLinkButton:GetNormalTexture():SetTexCoord(0.25, 0.7, 0.37, 0.75)
-	TradeSkillLinkButton:GetPushedTexture():SetTexCoord(0.25, 0.7, 0.45, 0.8)
-	TradeSkillLinkButton:SetHighlightTexture('')
-	AS:CreateBackdrop(TradeSkillLinkButton, 'Default')
-	TradeSkillLinkButton:Size(17, 14)
-	TradeSkillLinkButton:Point("LEFT", TradeSkillLinkFrame, "LEFT", 5, -1)
-	AS:SkinEditBox(TradeSkillFrameSearchBox)
-	AS:SkinEditBox(TradeSkillInputBox)
-	AS:SkinNextPrevButton(TradeSkillDecrementButton)
-	AS:SkinNextPrevButton(TradeSkillIncrementButton)
-	TradeSkillIncrementButton:Point("RIGHT", TradeSkillCreateButton, "LEFT", -13, 0)
-	
-	AS:SkinCloseButton(TradeSkillFrameCloseButton)
-	AS:SkinScrollBar(TradeSkillDetailScrollFrameScrollBar)
-	AS:SkinScrollBar(TradeSkillListScrollFrameScrollBar)
-	
-	for i = 1, MAX_TRADE_SKILL_REAGENTS do
-		local Button = _G["TradeSkillReagent"..i]
-		AS:StripTextures(Button)
-		AS:SkinTexture(Button.Icon)
-		AS:CreateBackdrop(Button)
-		Button.Backdrop:SetOutside(Button.Icon)
-	end
-	TradeSkillReagent1:SetPoint("TOPLEFT", TradeSkillReagentLabel, "BOTTOMLEFT", 1, -3)
-
-	for i = 1, 8 do
-		local Button = _G["TradeSkillSkill"..i]
-		AS:SkinStatusBar(Button.SubSkillRankBar)
-	end
-
-	hooksecurefunc("TradeSkillFrame_SetSelection", function(id)
-		if not TradeSkillSkillIcon.isSkinned then
-			if TradeSkillSkillIcon:GetNormalTexture() then
-				AS:StyleButton(TradeSkillSkillIcon)
-				AS:SkinTexture(TradeSkillSkillIcon:GetNormalTexture())
-				TradeSkillSkillIcon:GetNormalTexture():SetInside()
-				AS:SetTemplate(TradeSkillSkillIcon)
-				TradeSkillSkillIcon.isSkinned = true
-			end
+		AS:SkinFrame(TradeSkillGuildFrame)
+		TradeSkillGuildFrame:Point("BOTTOMLEFT", TradeSkillFrame, "BOTTOMRIGHT", 3, 19)
+		AS:SkinFrame(TradeSkillGuildFrameContainer)
+		AS:SkinCloseButton(TradeSkillGuildFrameCloseButton)
+		
+		TradeSkillLinkButton:GetNormalTexture():SetTexCoord(0.25, 0.7, 0.37, 0.75)
+		TradeSkillLinkButton:GetPushedTexture():SetTexCoord(0.25, 0.7, 0.45, 0.8)
+		TradeSkillLinkButton:SetHighlightTexture('')
+		AS:CreateBackdrop(TradeSkillLinkButton, 'Default')
+		TradeSkillLinkButton:Size(17, 14)
+		TradeSkillLinkButton:Point("LEFT", TradeSkillLinkFrame, "LEFT", 5, -1)
+		AS:SkinEditBox(TradeSkillFrameSearchBox)
+		AS:SkinEditBox(TradeSkillInputBox)
+		AS:SkinNextPrevButton(TradeSkillDecrementButton)
+		AS:SkinNextPrevButton(TradeSkillIncrementButton)
+		TradeSkillIncrementButton:Point("RIGHT", TradeSkillCreateButton, "LEFT", -13, 0)
+		
+		AS:SkinCloseButton(TradeSkillFrameCloseButton)
+		AS:SkinScrollBar(TradeSkillDetailScrollFrameScrollBar)
+		AS:SkinScrollBar(TradeSkillListScrollFrameScrollBar)
+		
+		for i = 1, MAX_TRADE_SKILL_REAGENTS do
+			local Button = _G["TradeSkillReagent"..i]
+			AS:StripTextures(Button)
+			AS:SkinTexture(Button.Icon)
+			AS:CreateBackdrop(Button)
+			Button.Backdrop:SetOutside(Button.Icon)
 		end
-		local skillName, skillType, numAvailable, isExpanded, altVerb, numSkillUps, indentLevel, showProgressBar, currentRank, maxRank, startingRank, displayAsUnavailable, unavailableString = GetTradeSkillInfo(id);
-		local skillLink = GetTradeSkillItemLink(id)
-		if skillLink then
-			local Quality = select(3, GetItemInfo(skillLink))
-			if Quality and Quality > 1 and BAG_ITEM_QUALITY_COLORS[Quality] then
-				TradeSkillSkillIcon:SetBackdropBorderColor(BAG_ITEM_QUALITY_COLORS[Quality].r, BAG_ITEM_QUALITY_COLORS[Quality].g, BAG_ITEM_QUALITY_COLORS[Quality].b)
+		TradeSkillReagent1:SetPoint("TOPLEFT", TradeSkillReagentLabel, "BOTTOMLEFT", 1, -3)
+
+		for i = 1, 8 do
+			local Button = _G["TradeSkillSkill"..i]
+			AS:SkinStatusBar(Button.SubSkillRankBar)
+		end
+
+		hooksecurefunc("TradeSkillFrame_SetSelection", function(id)
+			if not TradeSkillSkillIcon.isSkinned then
+				if TradeSkillSkillIcon:GetNormalTexture() then
+					AS:StyleButton(TradeSkillSkillIcon)
+					AS:SkinTexture(TradeSkillSkillIcon:GetNormalTexture())
+					TradeSkillSkillIcon:GetNormalTexture():SetInside()
+					AS:SetTemplate(TradeSkillSkillIcon)
+					TradeSkillSkillIcon.isSkinned = true
+				end
+			end
+			local skillName, skillType, numAvailable, isExpanded, altVerb, numSkillUps, indentLevel, showProgressBar, currentRank, maxRank, startingRank, displayAsUnavailable, unavailableString = GetTradeSkillInfo(id);
+			local skillLink = GetTradeSkillItemLink(id)
+			if skillLink then
+				local Quality = select(3, GetItemInfo(skillLink))
+				if Quality and Quality > 1 and BAG_ITEM_QUALITY_COLORS[Quality] then
+					TradeSkillSkillIcon:SetBackdropBorderColor(BAG_ITEM_QUALITY_COLORS[Quality].r, BAG_ITEM_QUALITY_COLORS[Quality].g, BAG_ITEM_QUALITY_COLORS[Quality].b)
+				else
+					TradeSkillSkillIcon:SetBackdropBorderColor(unpack(AS.BorderColor))
+				end
+			end
+			local numReagents = GetTradeSkillNumReagents(id);
+			for i = 1, numReagents, 1 do
+				local reagentName, reagentTexture, reagentCount, playerReagentCount = GetTradeSkillReagentInfo(id, i);
+				local reagentLink = GetTradeSkillReagentItemLink(id, i)
+				local reagent = _G["TradeSkillReagent"..i]
+				if reagent:IsShown() then
+					if reagentLink then
+						local Quality = select(3, GetItemInfo(reagentLink))
+						if Quality and Quality > 1 and BAG_ITEM_QUALITY_COLORS[Quality] then
+							reagent.Backdrop:SetBackdropBorderColor(BAG_ITEM_QUALITY_COLORS[Quality].r, BAG_ITEM_QUALITY_COLORS[Quality].g, BAG_ITEM_QUALITY_COLORS[Quality].b)
+						else
+							reagent.Backdrop:SetBackdropBorderColor(unpack(AS.BorderColor))
+						end
+					end
+				end
+			end
+		end)
+
+		hooksecurefunc('TradeSkillFrame_Update', function()
+			local skillOffset = FauxScrollFrame_GetOffset(TradeSkillListScrollFrame);
+			local diplayedSkills = TRADE_SKILLS_DISPLAYED;
+			local numTradeSkills = GetNumTradeSkills();
+			local buttonIndex = 0
+			for i = 1, diplayedSkills, 1 do
+				local skillIndex = i + skillOffset;
+				local skillName, skillType, numAvailable, isExpanded, altVerb, numSkillUps, indentLevel, showProgressBar, currentRank, maxRank, startingRank, displayAsUnavailable = GetTradeSkillInfo(skillIndex);
+				if ( skillIndex <= numTradeSkills ) then
+					if ( skillType == "header" or skillType == "subheader" ) then
+						if hasFilterBar then
+							buttonIndex = i + 1;
+						else
+							buttonIndex = i;
+						end
+						local skillButton = _G["TradeSkillSkill"..buttonIndex];
+						skillButton:SetNormalTexture("Interface\\Buttons\\UI-PlusMinus-Buttons");
+						skillButton:GetNormalTexture():Size(12)
+						if(indentLevel ~= 0) then
+							skillButton:GetNormalTexture():SetPoint("LEFT", 23, 2);
+							skillButton:GetDisabledTexture():SetPoint("LEFT", 23, 2);
+						else
+							skillButton:GetNormalTexture():SetPoint("LEFT", 3, 2);
+							skillButton:GetDisabledTexture():SetPoint("LEFT", 3, 2);
+						end
+						skillButton:SetHighlightTexture('')
+						if ( isExpanded ) then
+							skillButton:GetNormalTexture():SetTexCoord(0.5625, 1, 0, 0.4375)
+						else
+							skillButton:GetNormalTexture():SetTexCoord(0, 0.4375, 0, 0.4375)
+						end
+					end
+				end
+			end
+		end)
+
+		TradeSkillCollapseAllButton:HookScript('OnUpdate', function(self)
+			self:SetNormalTexture("Interface\\Buttons\\UI-PlusMinus-Buttons")
+			self:SetHighlightTexture("")
+			self:GetNormalTexture():SetPoint("LEFT", 3, 2)
+			if (self.collapsed) then
+				self:GetNormalTexture():SetTexCoord(0, 0.4375, 0, 0.4375)
 			else
-				TradeSkillSkillIcon:SetBackdropBorderColor(unpack(AS.BorderColor))
+				self:GetNormalTexture():SetTexCoord(0.5625, 1, 0, 0.4375)
 			end
-		end
-		local numReagents = GetTradeSkillNumReagents(id);
-		for i = 1, numReagents, 1 do
-			local reagentName, reagentTexture, reagentCount, playerReagentCount = GetTradeSkillReagentInfo(id, i);
-			local reagentLink = GetTradeSkillReagentItemLink(id, i)
-			local reagent = _G["TradeSkillReagent"..i]
-			if reagent:IsShown() then
-				if reagentLink then
-					local Quality = select(3, GetItemInfo(reagentLink))
-					if Quality and Quality > 1 and BAG_ITEM_QUALITY_COLORS[Quality] then
-						reagent.Backdrop:SetBackdropBorderColor(BAG_ITEM_QUALITY_COLORS[Quality].r, BAG_ITEM_QUALITY_COLORS[Quality].g, BAG_ITEM_QUALITY_COLORS[Quality].b)
-					else
-						reagent.Backdrop:SetBackdropBorderColor(unpack(AS.BorderColor))
-					end
-				end
-			end
-		end
-	end)
-
-	hooksecurefunc('TradeSkillFrame_Update', function()
-		local skillOffset = FauxScrollFrame_GetOffset(TradeSkillListScrollFrame);
-		local diplayedSkills = TRADE_SKILLS_DISPLAYED;
-		local numTradeSkills = GetNumTradeSkills();
-		local buttonIndex = 0
-		for i = 1, diplayedSkills, 1 do
-			local skillIndex = i + skillOffset;
-			local skillName, skillType, numAvailable, isExpanded, altVerb, numSkillUps, indentLevel, showProgressBar, currentRank, maxRank, startingRank, displayAsUnavailable = GetTradeSkillInfo(skillIndex);
-			if ( skillIndex <= numTradeSkills ) then
-				if ( skillType == "header" or skillType == "subheader" ) then
-					if hasFilterBar then
-						buttonIndex = i + 1;
-					else
-						buttonIndex = i;
-					end
-					local skillButton = _G["TradeSkillSkill"..buttonIndex];
-					skillButton:SetNormalTexture("Interface\\Buttons\\UI-PlusMinus-Buttons");
-					skillButton:GetNormalTexture():Size(12)
-					if(indentLevel ~= 0) then
-						skillButton:GetNormalTexture():SetPoint("LEFT", 23, 2);
-						skillButton:GetDisabledTexture():SetPoint("LEFT", 23, 2);
-					else
-						skillButton:GetNormalTexture():SetPoint("LEFT", 3, 2);
-						skillButton:GetDisabledTexture():SetPoint("LEFT", 3, 2);
-					end
-					skillButton:SetHighlightTexture('')
-					if ( isExpanded ) then
-						skillButton:GetNormalTexture():SetTexCoord(0.5625, 1, 0, 0.4375)
-					else
-						skillButton:GetNormalTexture():SetTexCoord(0, 0.4375, 0, 0.4375)
-					end
-				end
-			end
-		end
-	end)
-
-	TradeSkillCollapseAllButton:HookScript('OnUpdate', function(self)
-		self:SetNormalTexture("Interface\\Buttons\\UI-PlusMinus-Buttons")
-		self:SetHighlightTexture("")
-		self:GetNormalTexture():SetPoint("LEFT", 3, 2)
-		if (self.collapsed) then
-			self:GetNormalTexture():SetTexCoord(0, 0.4375, 0, 0.4375)
-		else
-			self:GetNormalTexture():SetTexCoord(0.5625, 1, 0, 0.4375)
-		end
-	end)
+		end)
+		AS:UnregisterSkinEvent('Blizzard_TradeSkill', 'ADDON_LOADED')
+	end
 end
 
 AS:RegisterSkin('Blizzard_TradeSkill', AS.Blizzard_TradeSkill, 'ADDON_LOADED')
--- Skins/TradeSkillDW.lua
+++ Skins/TradeSkillDW.lua
@@ -3,52 +3,24 @@ local AS = unpack(AddOnSkins)
 if not AS:CheckAddOn('TradeSkillDW') then return end
 
 function AS:TradeSkillDW()
-	local HORZ_BAR_FNAME = [[Interface\ClassTrainerFrame\UI-ClassTrainer-HorizontalBar]]
-	local TradeSkillHorizontalBarRight
-	for i, region in ipairs({TradeSkillFrame:GetRegions()}) do
-		if region:IsObjectType("Texture") then
-			if (region:GetTexture() == HORZ_BAR_FNAME) then
-				if not(region:GetName()) then
-					TradeSkillHorizontalBarRight = region
-					break
-				end
-			end
-		end
-	end
-
-	AS:SkinFrame(TradeSkillFrame)
-
-	TradeSkillHorizontalBarRight:SetTexture(HORZ_BAR_FNAME)
-	TradeSkillHorizontalBarRight:Kill()
-
-	AS:SkinCloseButton(TradeSkillFrameCloseButton)
-
-	AS:SkinFrame(TradeSkillGuildFrame)
-	AS:SkinFrame(TradeSkillGuildFrameContainer)
-	AS:SkinCloseButton(TradeSkillGuildFrameCloseButton)
-
-	AS:SkinStatusBar(TradeSkillRankFrame)
-	AS:SkinButton(TradeSkillCreateButton, true)
-	AS:SkinNextPrevButton(TradeSkillDecrementButton)
-	AS:SkinNextPrevButton(TradeSkillIncrementButton)
-	AS:SkinButton(TradeSkillCancelButton, true)
-	AS:SkinButton(TradeSkillFilterButton, true)
-	AS:SkinButton(TradeSkillCreateAllButton, true)
-	AS:SkinButton(TradeSkillViewGuildCraftersButton, true)
-	AS:SkinScrollBar(TradeSkillDetailScrollFrameScrollBar)
-	AS:SkinScrollBar(TradeSkillListScrollFrameScrollBar)
-
 	local function SkinTabs(self)
 		for i = 1, self:GetNumChildren() do
 			local Child = select(i, self:GetChildren())
 			if Child:IsObjectType('CheckButton') and not Child.IsSkinned then
 				Child:DisableDrawLayer('BACKGROUND')
-				AS:CreateBackdrop(Child)
+				AS:SetTemplate(Child)
 				AS:SkinTexture(Child:GetNormalTexture())
 				Child:GetNormalTexture():SetInside()
 				Child:GetNormalTexture().SetPoint = AS.Noop
 				Child:GetNormalTexture().SetTexCoord = AS.Noop
 				AS:StyleButton(Child)
+				local a,b,c,d,e = Child:GetPoint()
+				if a == 'TOPLEFT' and b:GetName() == 'TradeSkillFrame' and c == 'TOPRIGHT' and d == 0 then
+					Child:SetPoint("TOPLEFT", TradeSkillFrame, "TOPRIGHT", 2, -30)
+				elseif a == "BOTTOMLEFT" and b:GetName() == 'TradeSkillFrame' and c == "BOTTOMRIGHT" and d == 0 then
+					Child:SetPoint("BOTTOMLEFT", TradeSkillFrame, "BOTTOMRIGHT", 2, 30)
+				end
+				Child.SetPoint = AS.Noop
 				Child.IsSkinned = true
 			end
 		end
@@ -71,85 +43,10 @@ function AS:TradeSkillDW()
 				self.Text:SetPoint('CENTER', -1, 0)
 			end
 		end)
-		AS:StripTextures(TradeSkillListScrollFrame, true)
 		SkinTabs(self)
+		AS:StripTextures(TradeSkillListScrollFrame, true)
 		self.isSkinned = true
 	end)
-
-	AS:SkinEditBox(TradeSkillFrameSearchBox)
-	AS:SkinEditBox(TradeSkillInputBox)
-
-	AS:StripTextures(TradeSkillDetailScrollFrame, true)
-	AS:StripTextures(TradeSkillFrameInset, true)
-	AS:StripTextures(TradeSkillExpandButtonFrame, true)
-	AS:StripTextures(TradeSkillDetailScrollChildFrame, true)
-
-	TradeSkillGuildFrame:Point('BOTTOMLEFT', TradeSkillFrame, 'BOTTOMRIGHT', 3, 19)
-
-	TradeSkillFrame:Height(TradeSkillFrame:GetHeight() + 12)
-	TradeSkillLinkButton:GetNormalTexture():SetTexCoord(0.25, 0.7, 0.37, 0.75)
-	TradeSkillLinkButton:GetPushedTexture():SetTexCoord(0.25, 0.7, 0.45, 0.8)
-	TradeSkillLinkButton:GetHighlightTexture():Kill()
-	TradeSkillLinkButton:Size(17, 14)
-	TradeSkillLinkButton:Point('LEFT', TradeSkillLinkFrame, 'LEFT', 5, -1)
-	TradeSkillIncrementButton:Point('RIGHT', TradeSkillCreateButton, 'LEFT', -13, 0)
-
-	local once = false
-	hooksecurefunc('TradeSkillFrame_SetSelection', function(id)
-		if not TradeSkillSkillIcon.isSkinned then
-			AS:StyleButton(TradeSkillSkillIcon)
-			AS:SetTemplate(TradeSkillSkillIcon, 'Default')
-			TradeSkillSkillIcon.isSkinned = true
-		end
-		if TradeSkillSkillIcon:GetNormalTexture() then
-			AS:SkinTexture(TradeSkillSkillIcon:GetNormalTexture())
-			TradeSkillSkillIcon:GetNormalTexture():ClearAllPoints()
-			TradeSkillSkillIcon:GetNormalTexture():SetInside()
-		end
-		for i = 1, MAX_TRADE_SKILL_REAGENTS do
-			local button = _G['TradeSkillReagent'..i]
-			local icon = _G['TradeSkillReagent'..i..'IconTexture']
-			local count = _G['TradeSkillReagent'..i..'Count']
-
-			if not button.isSkinned then
-				AS:SkinTexture(icon)
-				icon.SetTexCoord = AS.Noop
-				icon:SetDrawLayer('ARTWORK')
-				icon.backdrop = CreateFrame('Frame', nil, button)
-				icon.backdrop:SetFrameLevel(button:GetFrameLevel() - 1)
-				AS:SkinFrame(icon.backdrop)
-				icon.backdrop:SetOutside(icon)
-				icon:SetParent(icon.backdrop)
-				count:SetParent(icon.backdrop)
-				count:SetDrawLayer('OVERLAY')
-				_G['TradeSkillReagent'..i..'NameFrame']:Kill()
-				button.isSkinned = true
-			end
-			if i > 2 and once == false then
-				local point, anchoredto, point2, x, y = button:GetPoint()
-				button:ClearAllPoints()
-				button:Point(point, anchoredto, point2, x, y - 3)
-				once = true
-			end
-		end
-	end)
-
-	AS:SkinFrame(TradeSkillDW_QueueFrame, nil, nil, true)
-	AS:SkinCloseButton(TradeSkillDW_QueueFrameCloseButton)
-	AS:StripTextures(TradeSkillDW_QueueFrameInset, true)
-	AS:SkinButton(TradeSkillDW_QueueFrameClear, true)
-	AS:SkinButton(TradeSkillDW_QueueFrameDown, true)
-	AS:SkinButton(TradeSkillDW_QueueFrameUp, true)
-	AS:SkinButton(TradeSkillDW_QueueFrameDo, true)
-	AS:StripTextures(TradeSkillDW_QueueFrameDetailScrollFrame)
-	AS:StripTextures(TradeSkillDW_QueueFrameDetailScrollFrameChildFrame)
-
-	for i = 1, 8 do
-		AS:StripTextures(_G['TradeSkillDW_QueueFrameDetailScrollFrameChildFrameReagent'..i])
-		AS:SkinTexture(_G['TradeSkillDW_QueueFrameDetailScrollFrameChildFrameReagent'..i..'IconTexture'])
-	end
-
-	AS:SkinScrollBar(TradeSkillDW_QueueFrameDetailScrollFrameScrollBar)
 end
 
 AS:RegisterSkin('TradeSkillDW', AS.TradeSkillDW)
