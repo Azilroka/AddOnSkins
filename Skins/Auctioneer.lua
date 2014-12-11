5f5aa09309be11cc33f60c6a48655839c8f102a7
Azilroka <darktemplar9900@yahoo.com>
2014-12-10 22:35:53 -0600

    lalalala

--- Skins/Auctioneer.lua
+++ Skins/Auctioneer.lua
@@ -5,14 +5,15 @@ if not AS:CheckAddOn('Auc-Advanced') then return end
 function AS:Auctioneer(event)
 	if event == 'PLAYER_ENTERING_WORLD' then return end
 	if IsAddOnLoaded('Auc-Stat-Purchased') then
-		BrowsePrevPageButton:Point('BOTTOMRIGHT', BrowseScrollFrame, 'BOTTOMRIGHT', -160, 0)
-		local a, b, c, d, e = BrowseNextPageButton:GetPoint()
+		local a, b, c, d, e = BrowsePrevPageButton:GetPoint()
+		BrowsePrevPageButton:SetPoint(a, b, c, d + 10, 3)
+		a, b, c, d, e = BrowseNextPageButton:GetPoint()
 		BrowseNextPageButton:SetPoint(a, b, c, d, 3)
 	end
 	if AucAdvScanButton then AS:SkinButton(AucAdvScanButton) end
 	if AucAdvSimpFrameCreate then AS:SkinButton(AucAdvSimpFrameCreate) end
 	if AucAdvSimpFrameRemember then AS:SkinButton(AucAdvSimpFrameRemember) end
-	AS:UnregisterSkinEvent(name, event)
+	AS:UnregisterSkinEvent('Auctioneer', event)
 end
 
 AS:RegisterSkin('Auctioneer', AS.Auctioneer, 'AUCTION_HOUSE_SHOW')
