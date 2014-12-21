cd789afd6a22fc9286b4f22c90c41cccaffacbab
Azilroka <darktemplar9900@yahoo.com>
2014-12-21 15:21:50 -0600

    Merging Talent/Glyph

--- Skins/Blizzard/Talents.lua
+++ Skins/Blizzard/Talents.lua
@@ -1,30 +1,22 @@
 local AS = unpack(AddOnSkins)
 
+local AddOnSkinned = 0
 function AS:Blizzard_Talent(event, addon)
-	if addon == 'Blizzard_TalentUI' or IsAddOnLoaded('Blizzard_TalentUI') then
-		local buttons = {
-			PlayerTalentFrameTalentsLearnButton,
-			PlayerTalentFrameActivateButton,
-			PlayerTalentFramePetSpecializationLearnButton,
-			PlayerTalentFrameSpecializationLearnButton
-		}
-
-		for _, button in pairs(buttons) do
-			AS:StripTextures(button)
-			AS:SkinButton(button)
-		end
-
-		PlayerTalentFrameTalentsTutorialButton.Ring:Hide()
-		PlayerTalentFrameTalentsTutorialButton:Point("TOPLEFT", PlayerTalentFrame, "TOPLEFT", -12, 12)
-		PlayerTalentFrameSpecializationTutorialButton.Ring:Hide()
-		PlayerTalentFrameSpecializationTutorialButton:Point("TOPLEFT", PlayerTalentFrame, "TOPLEFT", -12, 12)
-		PlayerTalentFramePetSpecializationTutorialButton.Ring:Hide()
-		PlayerTalentFramePetSpecializationTutorialButton:Point("TOPLEFT", PlayerTalentFrame, "TOPLEFT", -12, 12)
-
+	if (addon == 'Blizzard_TalentUI' or IsAddOnLoaded('Blizzard_TalentUI')) and not PlayerTalentFrame.isSkinned then
 		AS:SkinFrame(PlayerTalentFrame)
 		AS:StripTextures(PlayerTalentFrameInset)
 		AS:StripTextures(PlayerTalentFrameTalents, true)
 		AS:SkinCloseButton(PlayerTalentFrameCloseButton)
+
+		for _, Button in pairs({ PlayerTalentFrameTalentsTutorialButton, PlayerTalentFrameSpecializationTutorialButton, PlayerTalentFramePetSpecializationTutorialButton }) do
+			Button.Ring:Hide()
+			Button:Point("TOPLEFT", PlayerTalentFrame, "TOPLEFT", -12, 12)
+		end
+
+		AS:SkinButton(PlayerTalentFrameTalentsLearnButton, true)
+		AS:SkinButton(PlayerTalentFrameActivateButton, true)
+		AS:SkinButton(PlayerTalentFramePetSpecializationLearnButton, true)
+		AS:SkinButton(PlayerTalentFrameSpecializationLearnButton, true)
 
 		AS:SkinFrame(PlayerTalentFrameTalentsClearInfoFrame)
 		AS:SkinTexture(PlayerTalentFrameTalentsClearInfoFrameIcon)
@@ -49,12 +41,10 @@ function AS:Blizzard_Talent(event, addon)
 		PlayerTalentFramePetSpecializationSpellScrollFrameScrollChild.Seperator:SetTexture(1, 1, 1)
 		PlayerTalentFramePetSpecializationSpellScrollFrameScrollChild.Seperator:SetAlpha(0.2)
 
-		local buttons = {"PlayerTalentFrameSpecializationSpecButton", "PlayerTalentFramePetSpecializationSpecButton"}
-
-		for _, name in pairs(buttons) do
+		for _, name in pairs({"PlayerTalentFrameSpecializationSpecButton", "PlayerTalentFramePetSpecializationSpecButton"}) do
 			for i = 1, 4 do
 				local bu = _G[name..i]
-				_G["PlayerTalentFrameSpecializationSpecButton"..i.."Glow"]:SetTexture(nil)
+				_G[name..i.."Glow"]:SetTexture(nil)
 
 				bu:SetHighlightTexture("")
 				bu.bg:SetAlpha(0)
@@ -244,6 +234,125 @@ function AS:Blizzard_Talent(event, addon)
 		TalentMicroButtonAlert.Text:SetTextColor(1, 1, 0)
 		TalentMicroButtonAlert:ClearAllPoints()
 		TalentMicroButtonAlert:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, -6)
+		PlayerTalentFrame.isSkinned = true
+		AddOnSkinned = AddOnSkinned + 1
+	end
+	if (addon == 'Blizzard_GlyphUI' or IsAddOnLoaded('Blizzard_GlyphUI')) and not GlyphFrame.isSkinned then
+		GlyphFrame.background:SetDrawLayer('BORDER')
+		AS:SkinBackdropFrame(GlyphFrame, nil, true)
+		GlyphFrame.background:SetInside(GlyphFrame.Backdrop)
+		AS:SkinEditBox(GlyphFrameSearchBox)
+		AS:SkinDropDownBox(GlyphFrameFilterDropDown, 212)
+
+		for i = 1, 2 do
+			_G["GlyphFrameHeader"..i].middle:SetTexture(nil)
+			_G["GlyphFrameHeader"..i].leftEdge:SetTexture(nil)
+			_G["GlyphFrameHeader"..i].rightEdge:SetTexture(nil)
+			AS:SetTemplate(_G["GlyphFrameHeader"..i])
+		end
+
+		for i = 1, 10 do
+			local button = _G["GlyphFrameScrollFrameButton"..i]
+			local icon = _G["GlyphFrameScrollFrameButton"..i.."Icon"]
+
+			AS:StripTextures(button)
+
+			if icon then
+				AS:SkinTexture(icon)
+				AS:SkinButton(button)
+			end
+		end
+
+		AS:SetTemplate(GlyphFrameClearInfoFrame, 'Default')
+		AS:SkinTexture(GlyphFrameClearInfoFrameIcon)
+		GlyphFrameClearInfoFrameIcon:SetInside()
+
+		AS:SkinScrollBar(GlyphFrameScrollFrameScrollBar)
+
+		local StripAllTextures = {
+			GlyphFrameScrollFrame,
+			GlyphFrameSideInset,
+			GlyphFrameScrollFrameScrollChild
+		}
+
+		for _, object in pairs(StripAllTextures) do
+			AS:StripTextures(object)
+		end
+
+		if not AS.ParchmentEnabled then
+			AS:StripTextures(GlyphFrame)
+			AS:SkinTexture(GlyphFrame.specIcon)
+			GlyphFrame:HookScript('OnUpdate', function(self)
+				self.specIcon:SetAlpha(1 - self.glow:GetAlpha())
+			end)
+
+			for i = 1, 6 do
+				local Glyph = _G["GlyphFrameGlyph"..i]
+				AS:SetTemplate(Glyph)
+				Glyph:SetBackdropColor(0,0,0,0)
+				Glyph:SetFrameLevel(Glyph:GetFrameLevel() + 5)
+				Glyph.ring:Hide()
+				Glyph.glyph:Hide()
+				Glyph.highlight:SetTexture(nil)
+
+				-- This will make the glyphs nice and square.
+				Glyph.glyph:Hide()
+				Glyph.icon = Glyph:CreateTexture(nil, 'OVERLAY')
+				Glyph.icon:SetInside()
+				AS:SkinTexture(Glyph.icon)
+
+				-- Real Highlight shit right here.
+				AS:CreateBackdrop(Glyph)
+				Glyph.Backdrop:SetAllPoints()
+				Glyph.Backdrop:SetFrameLevel(Glyph:GetFrameLevel() + 1)
+				Glyph.Backdrop:SetBackdropColor(0, 0, 0, 0)
+				Glyph.Backdrop:SetBackdropBorderColor(1, 1, 0)
+				Glyph.Backdrop:SetScript('OnUpdate', function(self)
+					local Alpha = Glyph.highlight:GetAlpha()
+					self:SetAlpha(Alpha)
+					if strfind(Glyph.icon:GetTexture(), "Interface\\Spellbook\\UI%-Glyph%-Rune") then
+						if Alpha == 0 then
+							Glyph.icon:SetVertexColor(1, 1, 1)
+							Glyph.icon:SetAlpha(1)
+						else
+							Glyph.icon:SetVertexColor(1, 1, 0)
+							Glyph.icon:SetAlpha(Alpha)
+						end
+					end
+				end)
+
+				hooksecurefunc(Glyph.highlight, 'Show', function()
+					Glyph.Backdrop:Show()
+				end)
+
+				Glyph.glyph:Hide()
+				hooksecurefunc(Glyph.glyph, 'Show', function(self) self:Hide() end)
+				
+				if i % 2 == 1 then
+					Glyph:Size(Glyph:GetWidth() * .8, Glyph:GetHeight() * .8)
+				end
+			end
+
+			hooksecurefunc('GlyphFrame_Update', function(self)
+				local isActiveTalentGroup = PlayerTalentFrame and PlayerTalentFrame.talentGroup == GetActiveSpecGroup();
+				for i = 1, NUM_GLYPH_SLOTS do
+					local GlyphSocket = _G["GlyphFrameGlyph"..i]
+					local _, _, _, _, iconFilename = GetGlyphSocketInfo(i, PlayerTalentFrame.talentGroup)
+					if iconFilename then
+						GlyphSocket.icon:SetTexture(iconFilename)
+					else
+						GlyphSocket.icon:SetTexture("Interface\\Spellbook\\UI-Glyph-Rune-"..i)
+					end
+					GlyphFrameGlyph_UpdateSlot(GlyphSocket);
+					SetDesaturation(GlyphSocket.icon, not isActiveTalentGroup);
+				end
+				SetDesaturation(self.specIcon, not isActiveTalentGroup);
+			end)
+		end
+		GlyphFrame.isSkinned = true
+		AddOnSkinned = AddOnSkinned + 1
+	end
+	if AddOnSkinned == 2 then
 		AS:UnregisterSkinEvent('Blizzard_Talent', 'ADDON_LOADED')
 	end
 end
