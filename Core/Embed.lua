031002d081f073e809681d3417365644e1886286
Azilroka <darktemplar9900@yahoo.com>
2015-01-28 20:57:21 -0600

    Details! HotFix 3

--- Core/Embed.lua
+++ Core/Embed.lua
@@ -337,7 +337,6 @@ if AS:CheckAddOn('Skada') then
 	end
 end
 
-
 if AS:CheckAddOn('Details') then
 	AS['DetailsInstances'] = {}
 	function AS:Embed_Details()
@@ -379,12 +378,9 @@ if AS:CheckAddOn('Details') then
 
 		--> remove tooltip borders
 		Details:SetTooltipBackdrop("Blizzard Tooltip", 16, {1, 1, 1, 0})
-		--> use white square borders
-		--Details:SetTooltipBackdrop ("Details BarBorder 2", 24, {1, 1, 1, 1})
 
 		--> enable bar animations and make the update speed faster
 		Details:SetUseAnimations (true)
-		--Details:SetWindowUpdateSpeed (0.3) -- 0.05 to 3.00
 
 		local function EmbedWindow (window, width, height, point, relativeFrame, relativePoint, ofsx, ofsy)
 			if not window then return end
@@ -406,26 +402,13 @@ if AS:CheckAddOn('Details') then
 			--> break the group or window 2 will mess with the setpoint
 			window:UngroupInstance()
 
-			--> set the skin to ElvUI
-			if (not window.skin:find("ElvUI")) then
-				window:ChangeSkin("ElvUI Frame Style")
+			--> set the skin to ElvUI if not using already
+			if (not window.skin:find("ElvUI") and not window.skin:find("Forced")) then
+				window:ChangeSkin("ElvUI Style II")
+				window:AttributeMenu(true, -19, 5, "ElvUI Font", 11)
+				window:SetBarTextSettings(10, "ElvUI Font")
+				window:ToolbarMenuSetButtons(true, true, true, true, true, false)
 			end
-
-			--> always show the player's bar even if he/she isn't in the top ranks (for example, the window is showing 1 to 7 players, the player is 12 place, so the window shows 1 to 6 and 12).
-			window:SetBarFollowPlayer(true)
-			--> set the font on the title bar to ElvUI font
-			if (window.skin == "ElvUI Style II") then 
-				window:AttributeMenu(true, -19, 5, "ElvUI Font", 11) 
-			else 
-				window:AttributeMenu(true, -20, 5, "ElvUI Font", 11) 
-			end
-			--> set the font on bars to elvui font
-			window:SetBarTextSettings(10, "ElvUI Font")
-			--> remove the close button
-			window:ToolbarMenuSetButtons(true, true, true, true, true, false)
-
-			--> resize the baseframe // -20 because this size doesn't count the title bar
-			window:SetSize(width, height - 20)
 
 			--> set the point and save the position
 			window:SetFrameStrata('LOW')
@@ -433,6 +416,39 @@ if AS:CheckAddOn('Details') then
 			window.baseframe:SetParent(relativeFrame)
 
 			ofsx = ofsx - 1 --> wasn't fitting correctly, with -1 it get aligned.
+			if (window.skin == "Forced Square") then
+				ofsx = ofsx - 1
+				if (window:GetId() == 2) then
+					window:SetSize(width+1, height - 20)
+				else
+					window:SetSize(width, height - 20)
+				end
+				
+			elseif (window.skin == "ElvUI Frame Style") then
+				if (window:GetId() == 2) then
+					window:SetSize(width-1, height - 20)
+				else
+					if NumberToEmbed == 1 then
+						window:SetSize(width-2, height - 20)
+					else
+						window:SetSize(width, height - 20)
+					end
+				end
+			
+			elseif (window.skin == "ElvUI Style II") then
+				if (window:GetId() == 2) then
+					window:SetSize(width-3, height - 20)
+				else
+					if NumberToEmbed == 1 then
+						window:SetSize(width-6, height - 20)
+					else
+						window:SetSize(width-2, height - 20)
+					end
+				end
+			else
+				window:SetSize(width, height - 20)
+			end
+			
 			window.baseframe:SetPoint(point, relativeFrame, relativePoint, ofsx, -offsety)
 			window:SaveMainWindowPosition()
 			window:RestoreMainWindowPosition()
@@ -477,10 +493,7 @@ if AS:CheckAddOn('Details') then
 		elseif NumberToEmbed == 2 then
 			EmbedWindow(AS.DetailsInstances[1], EmbedSystem_LeftWindow:GetWidth(), EmbedSystem_LeftWindow:GetHeight(), 'TOPLEFT', EmbedSystem_LeftWindow, 'TOPLEFT', 2, 0)
 			EmbedWindow(AS.DetailsInstances[2], EmbedSystem_RightWindow:GetWidth(), EmbedSystem_RightWindow:GetHeight(), 'TOPRIGHT', EmbedSystem_RightWindow, 'TOPRIGHT', -2, 0)
-			if (AS.DetailsInstances[1] and AS.DetailsInstances[2] and AS.DetailsInstances[1].skin == "ElvUI Style II") then 
-				local width = AS.DetailsInstances[1]:GetSize() 
-				AS.DetailsInstances[2]:SetSize(width)
-			end 
+			
 		end
 		
 		--> internal events
@@ -492,14 +505,23 @@ if AS:CheckAddOn('Details') then
 			if (event == "DETAILS_INSTANCE_CLOSE") then
 				local instance = select (1, ...)
 				--> alert the used about closing an window embed: if the window is hidden from the UI because out of combat on ElvUI, it shows as opened under Details! Options panel.
-				if (instance._ElvUIEmbed) then
+				if (instance._ElvUIEmbed and _G.DetailsOptionsWindow and _G.DetailsOptionsWindow:IsShown()) then
 					Details:Msg("You just closed a window Embed on ElvUI, if wasn't intended click on Reopen.") --> need localization
 				end
 			elseif (event == "DETAILS_INSTANCE_OPEN") then
 				local instance = select(1, ...)
-				if (instance._ElvUIEmbed and _G.DetailsOptionsWindow and _G.DetailsOptionsWindow:IsShown()) then
+				if (instance._ElvUIEmbed) then
 					--> when a window is closed, it is removed from the window group, we need to add the window back to the group.
 					if (NumberToEmbed == 2) then
+						AS.DetailsInstances[1]:UngroupInstance()
+						AS.DetailsInstances[2]:UngroupInstance()
+						
+						AS.DetailsInstances[1].baseframe:ClearAllPoints()
+						AS.DetailsInstances[2].baseframe:ClearAllPoints()
+						
+						AS.DetailsInstances[1]:RestoreMainWindowPosition()
+						AS.DetailsInstances[2]:RestoreMainWindowPosition()
+
 						AS.DetailsInstances[2]:MakeInstanceGroup({1})
 					end
 				end
