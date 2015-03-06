ac9ac41fc2bd958db8137eedda2b76f95b50a253
Azilroka <darktemplar9900@yahoo.com>
2015-03-05 21:13:28 -0600

    Bagnon

--- Skins/Bagnon.lua
+++ Skins/Bagnon.lua
@@ -3,38 +3,18 @@ local AS = unpack(AddOnSkins)
 if not AS:CheckAddOn('Bagnon') then return end
 
 function AS:Bagnon(event, addon)
-	if event == 'PLAYER_ENTERING_WORLD' then
-		ToggleBackpack()
-		if BagnonFrameinventory then
-			AS:SkinFrame(BagnonFrameinventory)
-			AS:SkinCloseButton(BagnonFrameinventoryCloseButton)
+	for k, frame in Bagnon:IterateFrames() do
+		if frame and not frame.isSkinned then
+			AS:SkinFrame(frame)
+			frame:HookScript('OnUpdate', function(self)
+				if self and self.closeButton and not self.closeButton.isSkinned then
+					AS:SkinCloseButton(self.closeButton)
+					self.closeButton.isSkinned = true
+				end
+			end)
+			frame.isSkinned = true
 		end
-		ToggleBackpack()
-	elseif event == 'BANKFRAME_OPENED' then
-		AS:Delay(0, function()
-			if BagnonFramebank then
-				AS:SkinFrame(BagnonFramebank)
-				AS:SkinCloseButton(BagnonFramebankCloseButton)
-			end
-		end)
-		AS:UnregisterSkinEvent('Bagnon', event)
-	elseif event == 'GUILDBANKFRAME_OPENED' and addon == 'Bagnon_GuildBank' then
-		AS:Delay(0, function()
-			if BagnonFrameguildbank then
-				AS:SkinFrame(BagnonFrameguildbank)
-				AS:SkinCloseButton(BagnonFrameguildbankCloseButton)
-			end
-		end)
-		AS:UnregisterSkinEvent('Bagnon', event)
-	elseif event == 'VOID_STORAGE_OPEN' and addon == 'Bagnon_VoidStorage' then
-		AS:Delay(0, function()
-			if BagnonFramevoidstorage then
-				AS:SkinFrame(BagnonFramevoidstorage)
-				AS:SkinCloseButton(BagnonFramevoidstorageCloseButton)
-			end
-		end)
-		AS:UnregisterSkinEvent('Bagnon', event)
 	end
 end
 
-AS:RegisterSkin('Bagnon', AS.Bagnon, 'ADDON_LOADED', 'BANKFRAME_OPENED', 'GUILDBANKFRAME_OPENED', 'VOID_STORAGE_OPEN')
+AS:RegisterSkin('Bagnon', AS.Bagnon, 'BANKFRAME_OPENED', 'GUILDBANKFRAME_OPENED', 'VOID_STORAGE_OPEN')
