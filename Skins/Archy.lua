e0547e537d130dfcaeeaa571705089378576c3b3
Azilroka <darktemplar9900@yahoo.com>
2015-03-05 20:50:14 -0600

    Archy Issue #214

--- Skins/Archy.lua
+++ Skins/Archy.lua
@@ -15,53 +15,54 @@ function AS:Archy()
 		for _, object in pairs({"surveyButton", "crateButton", "loritemButton"}) do
 			AS:SkinButton(ArchyDistanceIndicatorFrame[object])
 			ArchyDistanceIndicatorFrame[object]:SetFrameLevel(ArchyDistanceIndicatorFrame:GetFrameLevel() + 3)
-			AS:SkinTexture(ArchyDistanceIndicatorFrame[object].icon)
 			ArchyDistanceIndicatorFrame[object].icon:SetInside()
 			ArchyDistanceIndicatorFrame[object].icon:SetDrawLayer('OVERLAY')
+			ArchyDistanceIndicatorFrame[object].icon:SetTexCoord(unpack(AS.TexCoords))
 		end
 	end
 
 	if Archy.db.profile.general.theme ~= 'Minimal' then
 		local function SkinArchyArtifactFrame()
-			for i, child in pairs(ArchyArtifactFrame.children) do
-				local containerFrame = _G['ArchyArtifactChildFrame'..i]
-				local crest = _G['ArchyArtifactChildFrame'..i..'Crest']
-				local icon = _G['ArchyArtifactChildFrame'..i..'Icon']
-				local fragmentBar = _G['ArchyArtifactChildFrame'..i..'FragmentBar']
-				local solveButton = _G['ArchyArtifactChildFrame'..i..'SolveButton']
+			for _, child in pairs(ArchyArtifactFrame.children) do
+				if child then
+					local icon = child.icon
+					local fragmentBar = child.fragmentBar
+					local solveButton = child.solveButton
 
-				if not solveButton.isSkinned then
-					AS:SkinFrame(solveButton, false, true)
-					AS:SkinTexture(solveButton:GetNormalTexture())
-					solveButton:GetNormalTexture():SetInside()	
-					AS:SkinTexture(solveButton:GetDisabledTexture())
-					solveButton:GetDisabledTexture():SetInside()		
-					AS:StyleButton(solveButton)
-					solveButton.isSkinned = true
+					if not solveButton.isSkinned then
+						AS:SkinFrame(solveButton, false, true)
+						AS:StyleButton(solveButton)
+						solveButton.isSkinned = true
+					end
+
+					if not icon.isSkinned then
+						AS:SkinFrame(icon, false, true)
+						AS:SkinTexture(icon.texture)
+						icon.texture:SetInside()
+						icon.isSkinned = true
+					end
+
+					if not fragmentBar.isSkinned then
+						AS:CreateBackdrop(fragmentBar)
+						fragmentBar.isSkinned = true
+					end
+
+					if solveButton:GetNormalTexture() then
+						solveButton:GetNormalTexture():SetTexCoord(unpack(AS.TexCoords))
+						solveButton:GetNormalTexture():SetInside()
+						solveButton:GetDisabledTexture():SetTexCoord(unpack(AS.TexCoords))
+						solveButton:GetDisabledTexture():SetInside()
+					end
+
+					icon:SetSize(solveButton:GetHeight(),solveButton:GetHeight())
+					fragmentBar:SetPoint('TOPLEFT', icon, 'TOPRIGHT', 7, -2)
 				end
-
-				if not icon.isSkinned then
-					AS:SkinFrame(icon, false, true)
-					AS:SkinTexture(icon.texture)
-					icon.texture:SetInside()
-					icon.isSkinned = true
-				end
-
-				if not fragmentBar.isSkinned then
-					AS:CreateBackdrop(fragmentBar)
-					fragmentBar.isSkinned = true
-				end
-
-				icon:SetSize(solveButton:GetHeight(),solveButton:GetHeight())
-				fragmentBar:SetPoint('TOPLEFT', icon, 'TOPRIGHT', 7, -2)
 			end
 		end
 
-		hooksecurefunc(Archy, 'RefreshRacesDisplay', SkinArchyArtifactFrame)
-		hooksecurefunc(Archy, 'UpdateRacesFrame', SkinArchyArtifactFrame)
+		hooksecurefunc(ArchyArtifactFrame, 'RefreshDisplay', SkinArchyArtifactFrame)
 
-		Archy:UpdateRacesFrame()
-		Archy:RefreshRacesDisplay()
+		ArchyArtifactFrame:RefreshDisplay()
 
 		if ArchyArtifactFrameSkillBar then
 			AS:SkinStatusBar(ArchyArtifactFrameSkillBar)
