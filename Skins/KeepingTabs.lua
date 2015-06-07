32a8f23179f5b5ed33617ff54103c4bf1b7a0c87
Azilroka <darktemplar9900@yahoo.com>
2015-06-07 16:25:43 -0500

    KeepingTabs / Updates

--- Skins/KeepingTabs.lua
+++ Skins/KeepingTabs.lua
@@ -6,14 +6,20 @@ function AS:ProfessionTabs(event, addon)
 	local function SkinTabs(self)
 		for i = 1, self:GetNumChildren() do
 			local Child = select(i, self:GetChildren())
-			if Child:IsObjectType('CheckButton') and Child:GetNormalTexture("Interface\\ICONS\\Achievement_Dungeon_Outland_Dungeon_Hero") and not Child.IsSkinned then
-				AS:SkinIconButton(Child)
-				Child:SetNormalTexture("Interface\\ICONS\\Achievement_Dungeon_Outland_Dungeon_Hero")
+			if Child:IsObjectType('CheckButton') and not Child.IsSkinned then
+				Child:DisableDrawLayer('BACKGROUND')
+				AS:SkinFrame(Child, nil, true)
+				AS:StyleButton(Child)
+				AS:SkinTexture(Child:GetNormalTexture())
+				Child:GetNormalTexture():SetInside()
 				for k = 1, Child:GetNumChildren() do
 					local SubChild = select(k, Child:GetChildren())
-					if SubChild:IsObjectType('CheckButton') and SubChild:GetNormalTexture("Interface\\PVPFrame\\RandomPVPIcon") then
-						AS:SkinIconButton(SubChild)
-						SubChild:SetNormalTexture("Interface\\PVPFrame\\RandomPVPIcon")
+					if SubChild:IsObjectType('CheckButton') then
+						SubChild:DisableDrawLayer('BACKGROUND')
+						AS:SkinFrame(SubChild, nil, true)
+						AS:StyleButton(SubChild)
+						AS:SkinTexture(SubChild:GetNormalTexture())
+						SubChild:GetNormalTexture():SetInside()
 					end
 				end
 				Child.IsSkinned = true
@@ -21,8 +27,12 @@ function AS:ProfessionTabs(event, addon)
 		end
 	end
 
-	PVEFrame:HookScript('OnShow', SkinTabs)
-	PVPUIFrame:HookScript('OnShow', SkinTabs)
+	if event == 'PLAYER_ENTERING_WORLD' then
+		FriendsFrame:HookScript('OnShow', SkinTabs)
+	elseif addon == 'Blizzard_GuildUI' then
+		GuildFrame:HookScript('OnShow', SkinTabs)
+		AS:UnregisterSkinEvent('KeepingTabs', 'ADDON_LOADED')
+	end
 end
 
-AS:RegisterSkin('KeepingTabs', AS.ProfessionTabs)
+AS:RegisterSkin('KeepingTabs', AS.ProfessionTabs, 'ADDON_LOADED')
