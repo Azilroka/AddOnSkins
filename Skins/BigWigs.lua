66e74ed4e00c152113b326ced7bb25e5cc1532b0
Azilroka <darktemplar9900@yahoo.com>
2015-08-25 21:39:48 -0500

    BigWigs

--- Skins/BigWigs.lua
+++ Skins/BigWigs.lua
@@ -2,170 +2,162 @@ local AS = unpack(AddOnSkins)
 
 if not AS:CheckAddOn('BigWigs') then return end
 
-local Loaded
 function AS:BigWigs(event, addon)
-	if event == 'LFG_PROPOSAL_SHOW' or event == 'PLAYER_ENTERING_WORLD' then
-		if LFGDungeonReadyPopup then
-			LFGDungeonReadyPopup:HookScript('OnUpdate', function(self)
-				for i = 1, self:GetNumChildren() do
-					local object = select(i, self:GetChildren())
-					if object:GetObjectType() == 'StatusBar' and not object.IsSkinned then
-						AS:SkinStatusBar(object)
-						object:ClearAllPoints()
-						object:SetPoint('TOP', self, 'BOTTOM', 0, -2)
-						object:SetHeight(16)
-						object.IsSkinned = true
-					end
+	if event == 'PLAYER_ENTERING_WORLD' then
+		if BigWigsLoader then
+			BigWigsLoader.RegisterMessage('AddOnSkins', "BigWigs_FrameCreated", function(event, frame, name)
+				if name == "QueueTimer" then
+					AS:SkinStatusBar(frame)
+					frame:ClearAllPoints()
+					frame:SetPoint('TOP', '$parent', 'BOTTOM', 0, -(AS.PixelPerfect and 2 or 4))
+					frame:SetHeight(16)
 				end
 			end)
-			AS:UnregisterSkinEvent('BigWigs', event)
 		end
 	end
 
-	if event == 'LFG_PROPOSAL_SHOW' then return end
+	if IsAddOnLoaded('BigWigs_Plugins') or (addon == 'BigWigs_Plugins') then
+		AS:UnregisterSkinEvent('BigWigs', "ADDON_LOADED")
 
-	local buttonsize = 19
-	local FreeBackgrounds = {}
+		local buttonsize = 19
+		local FreeBackgrounds = {}
 
-	local CreateBG = function()
-		local BG = CreateFrame('Frame')
-		BG:SetTemplate('Transparent')
-		return BG
-	end
+		local CreateBG = function()
+			local BG = CreateFrame('Frame')
+			BG:SetTemplate('Transparent')
+			return BG
+		end
 
-	local function FreeStyle(bar)
-		local bg = bar:Get('bigwigs:AddOnSkins:bg')
-		if bg then
+		local function FreeStyle(bar)
+			local bg = bar:Get('bigwigs:AddOnSkins:bg')
+			if bg then
+				bg:ClearAllPoints()
+				bg:SetParent(UIParent)
+				bg:Hide()
+				FreeBackgrounds[#FreeBackgrounds + 1] = bg
+			end
+
+			local ibg = bar:Get('bigwigs:AddOnSkins:ibg')
+			if ibg then
+				ibg:ClearAllPoints()
+				ibg:SetParent(UIParent)
+				ibg:Hide()
+				FreeBackgrounds[#FreeBackgrounds + 1] = ibg
+			end
+
+			bar.candyBarIconFrame:ClearAllPoints()
+			bar.candyBarIconFrame.SetWidth = nil
+			bar.candyBarIconFrame:SetPoint('TOPLEFT')
+			bar.candyBarIconFrame:SetPoint('BOTTOMLEFT')
+
+			bar.candyBarBar:ClearAllPoints()
+			bar.candyBarBar.SetPoint = nil
+			bar.candyBarBar:SetPoint('TOPRIGHT')
+			bar.candyBarBar:SetPoint('BOTTOMRIGHT')
+		end
+
+		local function ApplyStyle(bar)
+			bar:SetHeight(buttonsize)
+
+			local bg
+			if #FreeBackgrounds > 0 then
+				bg = tremove(FreeBackgrounds)
+			else
+				bg = CreateBG()
+			end
+
+			bg:SetParent(bar)
+			bg:SetFrameStrata(bar:GetFrameStrata())
+			bg:SetFrameLevel(bar:GetFrameLevel() - 1)
 			bg:ClearAllPoints()
-			bg:SetParent(UIParent)
-			bg:Hide()
-			FreeBackgrounds[#FreeBackgrounds + 1] = bg
-		end
+			bg:SetOutside(bar)
+			bg:SetTemplate('Transparent')
+			bg:Show()
+			bar:Set('bigwigs:AddOnSkins:bg', bg)
 
-		local ibg = bar:Get('bigwigs:AddOnSkins:ibg')
-		if ibg then
-			ibg:ClearAllPoints()
-			ibg:SetParent(UIParent)
-			ibg:Hide()
-			FreeBackgrounds[#FreeBackgrounds + 1] = ibg
-		end
-
-		bar.candyBarIconFrame:ClearAllPoints()
-		bar.candyBarIconFrame.SetWidth = nil
-		bar.candyBarIconFrame:SetPoint('TOPLEFT')
-		bar.candyBarIconFrame:SetPoint('BOTTOMLEFT')
-
-		bar.candyBarBar:ClearAllPoints()
-		bar.candyBarBar.SetPoint = nil
-		bar.candyBarBar:SetPoint('TOPRIGHT')
-		bar.candyBarBar:SetPoint('BOTTOMRIGHT')
-	end
-
-	local function ApplyStyle(bar)
-		bar:SetHeight(buttonsize)
-
-		local bg
-		if #FreeBackgrounds > 0 then
-			bg = tremove(FreeBackgrounds)
-		else
-			bg = CreateBG()
-		end
-
-		bg:SetParent(bar)
-		bg:SetFrameStrata(bar:GetFrameStrata())
-		bg:SetFrameLevel(bar:GetFrameLevel() - 1)
-		bg:ClearAllPoints()
-		bg:SetOutside(bar)
-		bg:SetTemplate('Transparent')
-		bg:Show()
-		bar:Set('bigwigs:AddOnSkins:bg', bg)
-
-		if bar.candyBarIconFrame:GetTexture() then
-			local ibg
-			if #FreeBackgrounds > 0 then
-				ibg = tremove(FreeBackgrounds)
-			else
-				ibg = CreateBG()
+			if bar.candyBarIconFrame:GetTexture() then
+				local ibg
+				if #FreeBackgrounds > 0 then
+					ibg = tremove(FreeBackgrounds)
+				else
+					ibg = CreateBG()
+				end
+				ibg:SetParent(bar)
+				ibg:SetFrameStrata(bar:GetFrameStrata())
+				ibg:SetFrameLevel(bar:GetFrameLevel() - 1)
+				ibg:ClearAllPoints()
+				ibg:SetOutside(bar.candyBarIconFrame)
+				ibg:SetBackdropColor(0, 0, 0, 0)
+				ibg:Show()
+				bar:Set('bigwigs:AddOnSkins:ibg', ibg)
 			end
-			ibg:SetParent(bar)
-			ibg:SetFrameStrata(bar:GetFrameStrata())
-			ibg:SetFrameLevel(bar:GetFrameLevel() - 1)
-			ibg:ClearAllPoints()
-			ibg:SetOutside(bar.candyBarIconFrame)
-			ibg:SetBackdropColor(0, 0, 0, 0)
-			ibg:Show()
-			bar:Set('bigwigs:AddOnSkins:ibg', ibg)
+
+			bar.candyBarBar:ClearAllPoints()
+			bar.candyBarBar:SetAllPoints(bar)
+			bar.candyBarBar.SetPoint = AS.Noop
+			bar.candyBarBar:SetStatusBarTexture(AS.NormTex)
+
+			bar.candyBarBackground:SetTexture(unpack(AS.BackdropColor))
+
+			bar.candyBarIconFrame:ClearAllPoints()
+			bar.candyBarIconFrame:Point('BOTTOMRIGHT', bar, 'BOTTOMLEFT', -7, 0)
+			bar.candyBarIconFrame:SetSize(buttonsize, buttonsize)
+			bar.candyBarIconFrame.SetWidth = AS.Noop
+
+			AS:SkinTexture(bar.candyBarIconFrame)
 		end
 
-		bar.candyBarBar:ClearAllPoints()
-		bar.candyBarBar:SetAllPoints(bar)
-		bar.candyBarBar.SetPoint = AS.Noop
-		bar.candyBarBar:SetStatusBarTexture(AS.NormTex)
-
-		bar.candyBarBackground:SetTexture(unpack(AS.BackdropColor))
-
-		bar.candyBarIconFrame:ClearAllPoints()
-		bar.candyBarIconFrame:Point('BOTTOMRIGHT', bar, 'BOTTOMLEFT', -7, 0)
-		bar.candyBarIconFrame:SetSize(buttonsize, buttonsize)
-		bar.candyBarIconFrame.SetWidth = AS.Noop
-
-		AS:SkinTexture(bar.candyBarIconFrame)
-	end
-
-	local function ApplyStyleHalfBar(bar)
-		local bg
-		if #FreeBackgrounds > 0 then
-			bg = tremove(FreeBackgrounds)
-		else
-			bg = CreateBG()
-		end
-
-		bg:SetParent(bar)
-		bg:SetFrameStrata(bar:GetFrameStrata())
-		bg:SetFrameLevel(bar:GetFrameLevel() - 1)
-		bg:ClearAllPoints()
-		bg:SetOutside(bar)
-		bg:SetTemplate('Transparent')
-		bg:Show()
-		bar:Set('bigwigs:AddOnSkins:bg', bg)
-
-		if bar.candyBarIconFrame:GetTexture() then
-			local ibg
+		local function ApplyStyleHalfBar(bar)
+			local bg
 			if #FreeBackgrounds > 0 then
-				ibg = tremove(FreeBackgrounds)
+				bg = tremove(FreeBackgrounds)
 			else
-				ibg = CreateBG()
+				bg = CreateBG()
 			end
-			ibg:SetParent(bar)
-			ibg:SetFrameStrata(bar:GetFrameStrata())
-			ibg:SetFrameLevel(bar:GetFrameLevel() - 1)
-			ibg:ClearAllPoints()
-			ibg:SetOutside(bar.candyBarIconFrame)
-			ibg:SetBackdropColor(0, 0, 0, 0)
-			ibg:Show()
-			bar:Set('bigwigs:AddOnSkins:ibg', ibg)
+
+			bg:SetParent(bar)
+			bg:SetFrameStrata(bar:GetFrameStrata())
+			bg:SetFrameLevel(bar:GetFrameLevel() - 1)
+			bg:ClearAllPoints()
+			bg:SetOutside(bar)
+			bg:SetTemplate('Transparent')
+			bg:Show()
+			bar:Set('bigwigs:AddOnSkins:bg', bg)
+
+			if bar.candyBarIconFrame:GetTexture() then
+				local ibg
+				if #FreeBackgrounds > 0 then
+					ibg = tremove(FreeBackgrounds)
+				else
+					ibg = CreateBG()
+				end
+				ibg:SetParent(bar)
+				ibg:SetFrameStrata(bar:GetFrameStrata())
+				ibg:SetFrameLevel(bar:GetFrameLevel() - 1)
+				ibg:ClearAllPoints()
+				ibg:SetOutside(bar.candyBarIconFrame)
+				ibg:SetBackdropColor(0, 0, 0, 0)
+				ibg:Show()
+				bar:Set('bigwigs:AddOnSkins:ibg', ibg)
+			end
+
+			bar:SetHeight(buttonsize / 2)
+
+			bar.candyBarBar:ClearAllPoints()
+			bar.candyBarBar:SetAllPoints(bar)
+			bar.candyBarBar.SetPoint = AS.Noop
+			bar.candyBarBar:SetStatusBarTexture(AS.NormTex)
+
+			bar.candyBarBackground:SetTexture(unpack(AS.BackdropColor))
+
+			bar.candyBarIconFrame:ClearAllPoints()
+			bar.candyBarIconFrame:Point('BOTTOMRIGHT', bar, 'BOTTOMLEFT', -7, 0)
+			bar.candyBarIconFrame:SetSize(buttonsize, buttonsize)
+			bar.candyBarIconFrame.SetWidth = AS.Noop
+
+			AS:SkinTexture(bar.candyBarIconFrame)
 		end
 
-		bar:SetHeight(buttonsize / 2)
-
-		bar.candyBarBar:ClearAllPoints()
-		bar.candyBarBar:SetAllPoints(bar)
-		bar.candyBarBar.SetPoint = AS.Noop
-		bar.candyBarBar:SetStatusBarTexture(AS.NormTex)
-
-		bar.candyBarBackground:SetTexture(unpack(AS.BackdropColor))
-
-		bar.candyBarIconFrame:ClearAllPoints()
-		bar.candyBarIconFrame:Point('BOTTOMRIGHT', bar, 'BOTTOMLEFT', -7, 0)
-		bar.candyBarIconFrame:SetSize(buttonsize, buttonsize)
-		bar.candyBarIconFrame.SetWidth = AS.Noop
-
-		AS:SkinTexture(bar.candyBarIconFrame)
-	end
-
-	if (IsAddOnLoaded('BigWigs_Plugins') or event == "ADDON_LOADED" and addon == 'BigWigs_Plugins') then
-		if Loaded then return end
-		Loaded = true
 		local BigWigsBars = BigWigs:GetPlugin('Bars')
 		BigWigsBars:RegisterBarStyle('AddOnSkins', {
 			apiVersion = 1,
@@ -183,8 +175,7 @@ function AS:BigWigs(event, addon)
 			BarStopped = FreeStyle,
 			GetStyleName = function() return 'AddOnSkins Half-Bar' end,
 		})
-		AS:UnregisterSkinEvent('BigWigs', "ADDON_LOADED")
 	end
 end
 
-AS:RegisterSkin('BigWigs', AS.BigWigs, 'ADDON_LOADED', 'LFG_PROPOSAL_SHOW')
+AS:RegisterSkin('BigWigs', AS.BigWigs, 'ADDON_LOADED')
