1b58784af6dd772636c117d5ab506c037139d520
Azilroka <darktemplar9900@yahoo.com>
2016-01-09 00:11:47 -0600

    InboxMailBag

--- Skins/InboxMailBag.lua
+++ Skins/InboxMailBag.lua
@@ -10,21 +10,21 @@ function AS:InboxMailBag(event)
 	AS:SkinTab(MailFrameTab3)
 	AS:SkinScrollBar(InboxMailbagFrameScrollFrameScrollBar)
 
-	for i = 1, 42 do
-		local button = _G['InboxMailbagFrameItem'..i]
-		if button then
-			AS:SkinFrame(button)
-			button.icon:SetInside()
-			AS:SkinTexture(button.icon)
-			button.qualityOverlay:SetTexture(nil)
-			button:SetNormalTexture(nil)
-			hooksecurefunc(button.qualityOverlay, 'SetVertexColor', function(self, r, g, b, a)
-				button:SetBackdropBorderColor(r, g, b)
-			end)
-			hooksecurefunc(button.qualityOverlay, 'Hide', function(self)
-				button:SetBackdropBorderColor(unpack(AS.BorderColor))
-			end)
-		end
+	local index = 1
+	while _G['InboxMailbagFrameItem'..index] do
+		local button = _G['InboxMailbagFrameItem'..index]
+		AS:SkinFrame(button)
+		button.icon:SetInside()
+		AS:SkinTexture(button.icon)
+		button.qualityOverlay:SetTexture(nil)
+		button:SetNormalTexture(nil)
+		hooksecurefunc(button.qualityOverlay, 'SetVertexColor', function(self, r, g, b, a)
+			button:SetBackdropBorderColor(r, g, b)
+		end)
+		hooksecurefunc(button.qualityOverlay, 'Hide', function(self)
+			button:SetBackdropBorderColor(unpack(AS.BorderColor))
+		end)
+		index = index + 1
 	end
 	AS:UnregisterSkinEvent('InboxMailBag', event)
 end
