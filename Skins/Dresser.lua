d2edf2ab4041ea41de23ed218eb98c8ba71bcb94
Azilroka <darktemplar9900@yahoo.com>
2016-07-23 20:40:51 -0500

    Dresser

--- Skins/Dresser.lua
+++ Skins/Dresser.lua
@@ -3,22 +3,22 @@
 if not AS:CheckAddOn('Dresser') then return end
 
 function AS:Dresser()
-	AS:SkinButton(DresserButton1)
-	AS:SkinButton(DresserButton2)
-	AS:SkinButton(DresserButton3)
-	AS:SkinButton(DresserButton4)
-	DresserButton1:ClearAllPoints()
-	DresserButton1:Point('BOTTOMLEFT', DressUpFrame, 'BOTTOMLEFT', 138, 79)
-	DresserButton2:ClearAllPoints()
-	DresserButton2:Point('BOTTOMLEFT', DressUpFrame, 'BOTTOMLEFT', 96, 79)
-	DresserButton3:ClearAllPoints()
-	DresserButton3:Point('BOTTOMLEFT', DressUpFrame, 'BOTTOMLEFT', 54, 79)
-	DresserButton4:ClearAllPoints()
-	DresserButton4:Point('BOTTOMLEFT', DressUpFrame, 'BOTTOMLEFT', 12, 79)
-	DresserButton1:Size(38, 22)
-	DresserButton2:Size(38, 22)
-	DresserButton3:Size(38, 22)
-	DresserButton4:Size(38, 22)
+
+	for i = 1, 4 do
+		local Button = _G['DressUpFrameLegacyDresser'..i]
+		AS:SkinButton(Button)
+		Button:Size(38, 22)
+		Button:ClearAllPoints()
+		if i == 1 then
+			Button:Point('BOTTOMLEFT', DressUpFrame, 'BOTTOMLEFT', 138, 79)
+		elseif i == 2 then
+			Button:Point('BOTTOMLEFT', DressUpFrame, 'BOTTOMLEFT', 96, 79)
+		elseif i == 3 then
+			Button:Point('BOTTOMLEFT', DressUpFrame, 'BOTTOMLEFT', 54, 79)
+		elseif i == 4 then
+			Button:Point('BOTTOMLEFT', DressUpFrame, 'BOTTOMLEFT', 12, 79)
+		end
+	end
 end
 
 AS:RegisterSkin('Dresser', AS.Dresser)
