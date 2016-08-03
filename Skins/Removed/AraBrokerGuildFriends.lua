2ef8a231c95fde3db3ba1347454c91d5ce29312a
Azilroka <darktemplar9900@yahoo.com>
2016-08-02 23:37:46 -0500

    Remove Garbage

--- Skins/Removed/AraBrokerGuildFriends.lua
+++
@@ -1,9 +0,0 @@
-local AS = unpack(AddOnSkins)
-
-if not AS:CheckAddOn('Ara_Broker_Guild_Friends') then return end
-
-function AS:AraBrokerGuildFriends()
-	AraBrokerGuildFriends:HookScript('OnUpdate', function(f) AS:SkinFrame(f) end)
-end
-
-AS:RegisterSkin('AraBrokerGuildFriends', AS.AraBrokerGuildFriends)
--- Skins/Removed/AraBrokerTradeskills.lua
+++
@@ -1,9 +0,0 @@
-local AS = unpack(AddOnSkins)
-
-if not AS:CheckAddOn('Ara_Broker_Tradeskills') then return end
-
-function AS:AraTradeskills()
-	AraSkills:HookScript('OnUpdate', function(f) AS:SkinFrame(f) end)
-end
-
-AS:RegisterSkin('AraBrokerTradeskills', AS.AraTradeskills)
