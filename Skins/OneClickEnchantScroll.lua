cd4b0f48730387f49f1f27e8abbc1fe113e50a58
Azilroka <darktemplar9900@yahoo.com>
2016-08-02 19:37:06 -0500

    OCES

--- Skins/OneClickEnchantScroll.lua
+++ Skins/OneClickEnchantScroll.lua
@@ -6,7 +6,7 @@ function AS:OneClickEnchantScroll(event, addon)
 	if addon == 'OneClickEnchantScroll' or event == 'TRADE_SKILL_SHOW' then
 		AS:SkinButton(TradeSkillCreateScrollButton, true)
 		TradeSkillCreateScrollButton:ClearAllPoints()
-		TradeSkillCreateScrollButton:Point('RIGHT', TradeSkillCreateButton, 'LEFT', -2, 0)
+		TradeSkillCreateScrollButton:Point('RIGHT', TradeSkillFrame.DetailsFrame.CreateButton, 'LEFT', -2, 0)
 		AS:UnregisterSkinEvent('OneClickEnchantScroll', 'ADDON_LOADED')
 		AS:UnregisterSkinEvent('OneClickEnchantScroll', 'TRADE_SKILL_SHOW')
 	end
