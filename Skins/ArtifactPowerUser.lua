b1196bf0e69bfad7ad3c074575324dcbcae4055b
Azilroka <darktemplar9900@yahoo.com>
2016-10-29 09:27:45 -0500

    Issue #413

--- Skins/ArtifactPowerUser.lua
+++ Skins/ArtifactPowerUser.lua
@@ -3,8 +3,12 @@ local AS = unpack(AddOnSkins)
 if not AS:CheckAddOn('ArtifactPowerUser') then return end
 
 function AS:ArtifactPowerUser(event, addon)
-	AS:SkinIconButton(ArtifactPowerUserButton)
-	ArtifactPowerUserButton.icon:SetDrawLayer('OVERLAY')
+	if ArtifactPowerUserButton and not ArtifactPowerUserButton.IsSkinned then
+		AS:SkinIconButton(ArtifactPowerUserButton)
+		ArtifactPowerUserButton.icon:SetDrawLayer('OVERLAY')
+		AS:UnregisterSkinEvent('ArtifactPowerUser', 'BAG_UPDATE_DELAYED')
+		ArtifactPowerUserButton.IsSkinned = true
+	end
 end
 
-AS:RegisterSkin('ArtifactPowerUser', AS.ArtifactPowerUser)
+AS:RegisterSkin('ArtifactPowerUser', AS.ArtifactPowerUser, 'BAG_UPDATE_DELAYED')
