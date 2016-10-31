49d95595b67540e6f55b296942b31d5fa8f20d7e
Azilroka <darktemplar9900@yahoo.com>
2016-10-31 18:57:29 -0500

    Hekili

--- Skins/Hekili.lua
+++ Skins/Hekili.lua
@@ -3,17 +3,11 @@ local AS = unpack(AddOnSkins)
 if not AS:CheckAddOn('Hekili') then return end
 
 function AS:Hekili()
-	local NumDisplays
-
-	for display, _ in ipairs( Hekili.DB.profile.displays ) do
-		NumDisplays = display
-	end
-
-	for i = 1, NumDisplays do
-		local NumButtons = Hekili.DB.profile.displays[i]['Icons Shown']
-		for n = 1, NumButtons do
-			AS:CreateBackdrop(_G['Hekili_D'..i..'_B'..n], 'Transparent')
-			AS:SkinTexture(_G['Hekili_D'..i..'_B'..n].Texture)
+	for Display, _ in ipairs( Hekili.DB.profile.displays ) do
+		for Buttons = 1, Hekili.DB.profile.displays[Display]['Icons Shown'] do
+			local Button = _G['Hekili_D'..Display..'_B'..Buttons]
+			AS:CreateBackdrop(Button, 'Transparent')
+			AS:SkinTexture(Button.Texture)
 		end
 	end
 end
