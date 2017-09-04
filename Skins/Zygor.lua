7022f312f6a89287f364ecd11d7d65249b3a7f92
Azilroka <darktemplar9900@yahoo.com>
2017-09-03 22:38:09 -0500

    Test

--- Skins/Zygor.lua
+++ Skins/Zygor.lua
@@ -3,12 +3,12 @@ local AS = unpack(AddOnSkins)
 if not AS:CheckAddOn('ZygorGuidesViewer') then return end
 
 function AS:Zygor()
-	AS:StripTextures(ZygorGuidesViewerFrame_Border, true)
+--[[	AS:StripTextures(ZygorGuidesViewerFrame_Border, true)
 	AS:SkinFrame(ZygorGuidesViewerFrame, nil, nil, true)
 
 	for i = 1, 6 do
 		AS:SkinBackdropFrame(_G['ZygorGuidesViewerFrame_Step'..i])
-	end
+	end]]
 end
 
 AS:RegisterSkin('Zygor', AS.Zygor)
