abaeae768d5b59852610a35e66be5048370a5acf
Merathilis <neo0608@gmail.com>
2017-09-25 22:01:13 +0200

    Modifed a bit the OrderHallCommander skin. We need moar transparency everywhere!!

--- Skins/OrderHallCommander.lua
+++ Skins/OrderHallCommander.lua
@@ -7,42 +7,44 @@ function AS:OrderHallCommander(event)
 		if not OrderHallMissionFrameMissions then return end
 		OrderHallMissionFrameMissions:HookScript('OnShow', function(self)
 			AS:Delay(0.2, function()
-			local frame = FollowerIcon:GetParent()
-			if frame.IsSkinned then return end
+				local frame = FollowerIcon:GetParent()
+				if frame.IsSkinned then return end
 
-			AS:StripTextures(frame)
-			AS:SetTemplate(frame)
-			AS:CreateShadow(frame)
-			frame.IsSkinned = true
+				AS:StripTextures(frame)
+				AS:SetTemplate(frame, "Transparent")
+				frame:ClearAllPoints()
+				frame:SetPoint("BOTTOM", OrderHallMissionFrame, "TOP", 0, 0)
+				frame:SetWidth(OrderHallMissionFrame:GetWidth()+2)
+				frame.IsSkinned = true
 
-			frame = LibInitCheckbox00001:GetParent():GetParent()
-			AS:StripTextures(frame)
-			AS:SetTemplate(frame, 'Transparent')
+				frame = LibInitCheckbox00001:GetParent():GetParent()
+				AS:StripTextures(frame)
+				AS:SetTemplate(frame, 'Transparent')
 
-			for i=1, 21 do
-				if i < 10 then
-				AS:SkinCheckBox(_G["LibInitCheckbox0000"..i])
-				elseif i > 9 and i < 14 then
-				AS:SkinSlideBar(_G["LibInitSlider000"..i])
-				elseif i ~= 17 and i < 20 then
-				AS:SkinCheckBox(_G["LibInitCheckbox000"..i])
-				elseif i > 19 then
-				AS:SkinButton(_G["LibInitButton000"..i])
-				else
-				AS:SkinDropDownBox(LibInitDropdown00017, 200)
+				for i=1, 21 do
+					if i < 10 then
+					AS:SkinCheckBox(_G["LibInitCheckbox0000"..i])
+					elseif i > 9 and i < 14 then
+					AS:SkinSlideBar(_G["LibInitSlider000"..i])
+					elseif i ~= 17 and i < 20 then
+					AS:SkinCheckBox(_G["LibInitCheckbox000"..i])
+					elseif i > 19 then
+					AS:SkinButton(_G["LibInitButton000"..i])
+					else
+					AS:SkinDropDownBox(LibInitDropdown00017, 200)
+					end
 				end
-			end
 
-			frame = {OrderHallMissionFrame.MissionTab:GetChildren()}
-			AS:SkinNextPrevButton(frame[19], true)
-			frame[19]:Size(12, 12)
+				frame = {OrderHallMissionFrame.MissionTab:GetChildren()}
+				AS:SkinNextPrevButton(frame[19], true)
+				frame[19]:Size(12, 12)
 
-			frame = {LibInitCheckbox00001:GetParent():GetParent():GetChildren()}
-			AS:SkinCloseButton(frame[1])
-			AS:StripTextures(frame[2])
+				frame = {LibInitCheckbox00001:GetParent():GetParent():GetChildren()}
+				AS:SkinCloseButton(frame[1])
+				AS:StripTextures(frame[2])
 
-			frame = {OrderHallMissionFrameMissions.CompleteDialog.BorderFrame.ViewButton:GetChildren()}
-			AS:SkinButton(frame[1])
+				frame = {OrderHallMissionFrameMissions.CompleteDialog.BorderFrame.ViewButton:GetChildren()}
+				AS:SkinButton(frame[1])
 			end)
 		end)
 		AS:UnregisterSkinEvent('OrderHallCommander', event)
