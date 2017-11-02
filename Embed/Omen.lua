9f62e92d815d64536ce3a1d8cbbec34e14aae3bc
Azilroka <darktemplar9900@yahoo.com>
2017-11-02 18:47:54 -0500

    Omen Fix

--- Embed/Omen.lua
+++ Embed/Omen.lua
@@ -2,8 +2,6 @@ local AS = unpack(AddOnSkins)
 
 if not AS:CheckAddOn('Omen') then return end
 
-local Omen = Omen
-
 local _G = _G
 
 function AS:Embed_Omen()
@@ -11,21 +9,21 @@ function AS:Embed_Omen()
 	if AS:CheckOption('EmbedSystemDual') then EmbedParent = AS:CheckOption('EmbedRight') == 'Omen' and _G.EmbedSystem_RightWindow or _G.EmbedSystem_LeftWindow end
 	EmbedParent.FrameName = "OmenAnchor"
 
-	Omen.BarList.SetBackdrop = nil
-	Omen.BarList.SetBackdropColor = nil
-	Omen.BarList.SetBackdropBorderColor = nil
+	_G.Omen.BarList.SetBackdrop = nil
+	_G.Omen.BarList.SetBackdropColor = nil
+	_G.Omen.BarList.SetBackdropBorderColor = nil
 
 	if AS:CheckOption('OmenBackdrop') then
-		AS:SkinFrame(Omen.BarList, AS:CheckOption('TransparentEmbed') and 'Transparent' or 'Default')
+		AS:SkinFrame(_G.Omen.BarList, AS:CheckOption('TransparentEmbed') and 'Transparent' or 'Default')
 	else
-		AS:StripTextures(Omen.BarList)
+		AS:StripTextures(_G.Omen.BarList)
 	end
 
-	Omen.BarList.SetBackdrop = AS.Noop
-	Omen.BarList.SetBackdropColor = AS.Noop
-	Omen.BarList.SetBackdropBorderColor = AS.Noop
+	_G.Omen.BarList.SetBackdrop = AS.Noop
+	_G.Omen.BarList.SetBackdropColor = AS.Noop
+	_G.Omen.BarList.SetBackdropBorderColor = AS.Noop
 
-	local db = Omen.db
+	local db = _G.Omen.db
 	db.profile.Scale = 1
 	db.profile.Bar.Spacing = 1
 	db.profile.Background.EdgeSize = 1
@@ -35,7 +33,7 @@ function AS:Embed_Omen()
 	db.profile.Locked = true
 	db.profile.TitleBar.ShowTitleBar = true
 	db.profile.FrameStrata = AS:CheckOption('EmbedFrameStrata')
-	Omen:OnProfileChanged(nil, db)
+	_G.Omen:OnProfileChanged(nil, db)
 
 	_G.OmenAnchor:SetParent(EmbedParent)
 	_G.OmenAnchor:SetTemplate()
