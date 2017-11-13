97d7654328543db96dd96ccc178afd9b9bd0a0e1
Azilroka <darktemplar9900@yahoo.com>
2017-11-12 18:12:10 -0600

    Move this outside the skin function to stop creating anon

--- Skins/AdiBags.lua
+++ Skins/AdiBags.lua
@@ -2,26 +2,26 @@ local AS = unpack(AddOnSkins)
 
 if not AS:CheckAddOn('AdiBags') then return end
 
-function AS:AdiBags(event)
-	local AdiBags = LibStub('AceAddon-3.0'):GetAddon('AdiBags')
-
-	local function SkinFrame(frame)
-		local region = frame.HeaderRightRegion
-		AS:SkinFrame(frame)
-		AS:SkinFrame(_G[frame:GetName()..'Bags'], 'Default', true)
-		AS:SkinCloseButton(frame.CloseButton)
-		for i = 1, 5 do
-			local widget = region.widgets[i] and region.widgets[i].widget
-			if widget then
-				if widget:IsObjectType('Button') then
-					AS:SkinButton(widget, true)
-				elseif widget:IsObjectType('EditBox') then
-					AS:SkinEditBox(widget)
-				end
+local function SkinFrame(frame)
+	local region = frame.HeaderRightRegion
+	AS:SkinFrame(frame)
+	AS:SkinFrame(_G[frame:GetName()..'Bags'], 'Default', true)
+	AS:SkinCloseButton(frame.CloseButton)
+	for i = 1, 5 do
+		local widget = region.widgets[i] and region.widgets[i].widget
+		if widget then
+			if widget:IsObjectType('Button') then
+				AS:SkinButton(widget, true)
+			elseif widget:IsObjectType('EditBox') then
+				AS:SkinEditBox(widget)
 			end
 		end
-		AS:SkinTexture(frame.BagSlotButton:GetNormalTexture())
 	end
+	AS:SkinTexture(frame.BagSlotButton:GetNormalTexture())
+end
+
+function AS:AdiBags(event)
+	local AdiBags = LibStub('AceAddon-3.0'):GetAddon('AdiBags')
 
 	if event == 'PLAYER_ENTERING_WORLD' then
 		AS:Delay(1, function()
