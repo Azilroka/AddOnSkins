6f958e7dbc4dd5732f243b909af25e2ddd0b1408
Azilroka <darktemplar9900@yahoo.com>
2017-12-10 14:06:30 -0600

    More ATT

--- Skins/AllTheThings.lua
+++ Skins/AllTheThings.lua
@@ -1,7 +1,11 @@
 local AS = unpack(AddOnSkins)
 
 function AS:AllTheThings()
-	AllTheThings:GetWindow('CurrentInstance'):SetTemplate('Transparent')
+	local CurrentInstance = AllTheThings:GetWindow('CurrentInstance')
+	AS:SetTemplate(CurrentInstance, 'Transparent')
+	AS:SkinCloseButton(CurrentInstance.CloseButton)
+	AS:SkinScrollBar(CurrentInstance.ScrollBar)
+	CurrentInstance.Container:SetPoint("RIGHT", CurrentInstance.ScrollBar, "LEFT", -3, 0)
 end
 
 AS:RegisterSkin('AllTheThings', AS.AllTheThings)
