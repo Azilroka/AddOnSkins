92dcfafc95ab3daa67566286f3899f0636fe8660
AcidWeb <pawelj@iosphe.re>
2018-02-24 04:10:44 -0500

    Updated REFlex skin

--- Skins/REFlex.lua
+++ Skins/REFlex.lua
@@ -3,22 +3,22 @@ local AS = unpack(AddOnSkins)
 if not AS:CheckAddOn('REFlex') then return end
 
 function AS:REFlex()
-	AS:SkinFrame(REFlex)
-	AS:SkinFrame(REFlexNamespace.TableBG.frame, nil, true)
-	AS:StripTextures(_G[REFlexNamespace.TableBG.frame:GetName()..'ScrollTrough'], true)
-	AS:SkinScrollBar(_G[REFlexNamespace.TableBG.frame:GetName()..'ScrollFrameScrollBar'])
-	AS:SkinFrame(REFlexNamespace.TableArena.frame, nil, true)
-	AS:StripTextures(_G[REFlexNamespace.TableArena.frame:GetName()..'ScrollTrough'], true)
-	AS:SkinScrollBar(_G[REFlexNamespace.TableArena.frame:GetName()..'ScrollFrameScrollBar'])
-	AS:SkinButton(REFlex_DumpButton)
-	AS:SkinCloseButton(REFlex_CloseButton)
-	AS:StripTextures(REFlex_HKBar)
-	AS:SkinStatusBar(REFlex_HKBar_I)
-	for i = 1, REFlex.numTabs do
-		AS:SkinTab(_G["REFlexTab"..i])
+	AS:SkinFrame(REFlexFrame)
+	AS:SkinFrame(REFlex.TableBG.frame, nil, true)
+	AS:StripTextures(_G[REFlex.TableBG.frame:GetName()..'ScrollTrough'], true)
+	AS:SkinScrollBar(_G[REFlex.TableBG.frame:GetName()..'ScrollFrameScrollBar'])
+	AS:SkinFrame(REFlex.TableArena.frame, nil, true)
+	AS:StripTextures(_G[REFlex.TableArena.frame:GetName()..'ScrollTrough'], true)
+	AS:SkinScrollBar(_G[REFlex.TableArena.frame:GetName()..'ScrollFrameScrollBar'])
+	AS:SkinButton(REFlexFrame_DumpButton)
+	AS:SkinCloseButton(REFlexFrame_CloseButton)
+	AS:StripTextures(REFlexFrame_HKBar)
+	AS:SkinStatusBar(REFlexFrame_HKBar_I)
+	for i = 1, REFlexFrame.numTabs do
+		AS:SkinTab(_G["REFlexFrameTab"..i])
 	end
-	REFlex_Title:SetPoint('TOP', 0, -10)
-	REFlex_HKBar:SetPoint('BOTTOM', 0, 19)
+	REFlexFrame_Title:SetPoint('TOP', 0, -10)
+	REFlexFrame_HKBar:SetPoint('BOTTOM', 0, 19)
 end
 
 AS:RegisterSkin('REFlex', AS.REFlex)
