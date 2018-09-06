a4e507009ed61b513e514593f644b8c684533df3
Azilroka <darktemplar9900@yahoo.com>
2018-09-06 18:37:20 -0500

    Libraries Update

--- Skins/Libraries.lua
+++ Skins/Libraries.lua
@@ -207,7 +207,7 @@ function AS:SkinLibraries()
 
 	local DBIcon = LibStub("LibDBIcon-1.0", true)
 	if DBIcon then
-		AS:SkinTooltip(LibDBIconTooltip)
+		DBIcon.tooltip:HookScript("OnShow", function(self) AS:SetTemplate(self, 'Transparent') end)
 	end
 
 	local LET = LibStub('LibExtraTip-1', true)
