5a2c5180f531e4b9ce5e8787556a6a557db61958
Azilroka <darktemplar9900@yahoo.com>
2018-09-14 01:22:48 -0500

    ExtVendor

--- Skins/ExtVendor.lua
+++ Skins/ExtVendor.lua
@@ -5,10 +5,15 @@ if not AS:CheckAddOn('ExtVendor') then return end
 function AS:ExtVendor()
 	AS:StripTextures(MerchantFrameFilterButton, true)
 	AS:SkinButton(MerchantFrameFilterButton)
+
 	AS:SkinButton(MerchantFrameSellJunkButton)
 	AS:SkinTexture(MerchantFrameSellJunkButtonIcon)
+
 	MerchantFrameSellJunkButtonIcon:SetInside()
+
 	AS:SkinEditBox(MerchantFrameSearchBox)
+	AS:SkinButton(ExtVendor_SellJunkPopupYesButton)
+	AS:SkinButton(ExtVendor_SellJunkPopupNoButton)
 
 	for i = 13, 20 do
 		local Slot = _G["MerchantItem"..i]
@@ -17,32 +22,22 @@ function AS:ExtVendor()
 		AS:StyleButton(Slot.ItemButton)
 		AS:SkinTexture(Slot.ItemButton.icon)
 		Slot.ItemButton.icon:SetInside()
+		Slot.ItemButton:SetPoint("TOPLEFT", Slot, "TOPLEFT", 4, -4)
 		Slot.ItemButton.IconBorder:SetAlpha(0)
 
-		Slot.ItemButton:SetPoint("TOPLEFT", Slot, "TOPLEFT", 4, -4)
+		hooksecurefunc(Slot.ItemButton.IconBorder, 'SetVertexColor', function(self, r, g, b) Slot.ItemButton:SetBackdropBorderColor(r, g, b) end)
+		hooksecurefunc(Slot.ItemButton.IconBorder, 'Hide', function(self) Slot.ItemButton:SetBackdropBorderColor(unpack(AS.BorderColor)) end)
 
 		_G["MerchantItem"..i.."MoneyFrame"]:ClearAllPoints()
-		_G["MerchantItem"..i.."MoneyFrame"]:SetPoint("BOTTOMLEFT", Slot.ItemButton, "BOTTOMRIGHT", 3, 0)
+		_G["MerchantItem"..i.."MoneyFrame"]:Point("BOTTOMLEFT", Slot.ItemButton, "BOTTOMRIGHT", 3, 0)
 
 		for j = 1, 3 do
-			AS:CreateBackdrop(_G["MerchantItem"..i.."AltCurrencyFrameItem"..j])
-			_G["MerchantItem"..i.."AltCurrencyFrameItem"..j].Backdrop:SetOutside(_G["MerchantItem"..i.."AltCurrencyFrameItem"..j.."Texture"])
+			AS:CreateBackdrop(_G["MerchantItem"..i.."AltCurrencyFrameItem"..j.."Texture"])
 			AS:SkinTexture(_G["MerchantItem"..i.."AltCurrencyFrameItem"..j.."Texture"])
 		end
 	end
 
-	MerchantFrame:HookScript('OnUpdate', function()
-		for i = 13, 20 do
-			if _G['MerchantItem'..i..'AltCurrencyFrame'] then
-				_G['MerchantItem'..i..'AltCurrencyFrame']:SetPoint('BOTTOMLEFT', _G['MerchantItem'..i..'NameFrame'], 'BOTTOMLEFT', 3, 34)
-			end
-		end
-	end)
-
 	MerchantFrame:HookScript('OnShow', function(self) self:SetWidth(690) end)
-
-	AS:SkinButton(ExtVendor_SellJunkPopupYesButton)
-	AS:SkinButton(ExtVendor_SellJunkPopupNoButton)
 end
 
 AS:RegisterSkin('ExtVendor', AS.ExtVendor)
