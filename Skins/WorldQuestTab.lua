b153d38c7f4af4bb853f1edff4efff4926797042
Azilroka <darktemplar9900@yahoo.com>
2018-09-19 21:19:22 -0500

    WQT

--- Skins/WorldQuestTab.lua
+++ Skins/WorldQuestTab.lua
@@ -5,18 +5,21 @@ if not AS:CheckAddOn('WorldQuestTab') then return end
 function AS:WorldQuestTab()
 	AS:SkinButton(WQT_TabNormal)
 	AS:SkinButton(WQT_TabWorld)
+
 	AS:SkinButton(WQT_WorldQuestFrameSortButton)
+
 	AS:StripTextures(WQT_WorldQuestFrameFilterButton, true)
+
 	AS:SkinButton(WQT_WorldQuestFrameFilterButton)
+
 	AS:SkinNextPrevButton(WQT_WorldQuestFrameSortButtonButton)
+
 	WQT_TabWorld.TabBg:Hide()
+	WQT_TabWorld.Highlight:SetTexture("")
+
+	WQT_TabNormal.Highlight:SetTexture("")
 	WQT_TabNormal.TabBg:Hide()
 
-	--Reposition elements
-	WQT_TabNormal:ClearAllPoints()
-	WQT_TabNormal:Point('RIGHT', WorldMapFrameCloseButton, 'LEFT', -231, -36)
-	WQT_TabWorld:ClearAllPoints()
-	WQT_TabWorld:Point('LEFT', WQT_TabNormal, 'RIGHT', 1, 0)
 	WQT_WorldQuestFrameSortButton:ClearAllPoints()
 	WQT_WorldQuestFrameSortButton:Point('LEFT', WQT_TabWorld, 'RIGHT', 18, -2)
 	WQT_WorldQuestFrameSortButtonButton:ClearAllPoints()
@@ -26,12 +29,22 @@ function AS:WorldQuestTab()
 	WQT_WorldQuestFrameFilterButton:ClearAllPoints()
 	WQT_WorldQuestFrameFilterButton:Point('LEFT', WQT_WorldQuestFrameSortButton, 'RIGHT', 18, 0)
 
-	--Resize Buttons
 	WQT_WorldQuestFrameSortButton:Size(92, 32)
 	WQT_WorldQuestFrameSortButtonButton:Size(27, 28)
 	WQT_WorldQuestFrameFilterButton:Size(92, 32)
 
 	AS:SkinScrollBar(WQT_QuestScrollFrameScrollBar)
+
+	for _, Button in ipairs(WQT_QuestScrollFrame.buttons) do
+		Button.Reward:SetSize(26, 26)
+		AS:SkinTexture(Button.Reward.Icon, true)
+		Button.Reward.IconBorder:SetAlpha(0)
+		hooksecurefunc(Button.Reward.IconBorder, 'SetVertexColor', function(self, r, g, b) Button.Reward.Icon.Backdrop:SetBackdropBorderColor(r, g, b) end)
+	end
+
+	for _, Tooltip in pairs({ WQT_CompareTooltip1, WQT_CompareTooltip2, WQT_Tooltip}) do
+		AS:SkinTooltip(Tooltip)
+	end
 end
 
 AS:RegisterSkin('WorldQuestTab', AS.WorldQuestTab)
