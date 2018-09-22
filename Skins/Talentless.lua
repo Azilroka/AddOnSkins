0bac92befef2a34d25dbfb44e4afa9865631eac0
Azilroka <darktemplar9900@yahoo.com>
2018-09-21 21:40:39 -0500

    Talentless

--- Skins/Talentless.lua
+++ Skins/Talentless.lua
@@ -1,14 +1,39 @@
- local AS = unpack(AddOnSkins)
+local AS = unpack(AddOnSkins)
 
- if not AS:CheckAddOn('Talentless') then return end
+if not AS:CheckAddOn('Talentless') then return end
 
- function AS:Talentless(event, addon)
-	 AS:SkinButton(TalentlessSpecButton1)
-	 AS:SkinButton(TalentlessSpecButton2)
-	 AS:SkinButton(TalentlessSpecButton3)
-	 AS:SkinButton(TalentlessSpecButton4)
-	 AS:SkinButton(TalentlessItemButton1)
-	 AS:SkinButton(TalentlessItemButton2)
- end
+function AS:Talentless()
+	hooksecurefunc(Talentless, "CreateSpecButtons", function(self)
+		for _, Button in pairs(self.Specs) do
+			AS:SkinButton(Button)
+			Button:SetBackdropColor(0, 0, 0, 0)
+			AS:StyleButton(Button)
+			Button:GetCheckedTexture():SetAlpha(0)
+			if Button:GetChecked() then
+				Button:SetBackdropBorderColor(.2, .5, .1)
+			end
+			hooksecurefunc(Button, 'SetChecked', function(self, value)
+				if value == true then
+					self:SetBackdropBorderColor(.2, .5, .1)
+				else
+					self:SetBackdropBorderColor(unpack(AS.BorderColor))
+				end
+			end)
+			Button:HookScript("OnLeave", function(self)
+				if self:GetChecked() then
+					self:SetBackdropBorderColor(.2, .5, .1)
+				end
+			end)
+		end
+	end)
 
- AS:RegisterSkin('Talentless', AS.Talentless)
+	hooksecurefunc(Talentless, "CreateSpecButtons", function(self)
+		for _, Button in pairs(self.Items) do
+			AS:SkinButton(Button)
+			Button:SetBackdropColor(0, 0, 0, 0)
+			AS:StyleButton(Button)
+		end
+	end)
+end
+
+AS:RegisterSkin('Talentless', AS.Talentless)
