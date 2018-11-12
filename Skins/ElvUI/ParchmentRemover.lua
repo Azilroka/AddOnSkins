8a9dfe62ff31f7dd7dc95fb28792f77d2cc13408
Azilroka <darktemplar9900@yahoo.com>
2018-11-11 18:17:42 -0600

    Bring ParchmentRemover up to date

--- Skins/ElvUI/ParchmentRemover.lua
+++ Skins/ElvUI/ParchmentRemover.lua
@@ -2,117 +2,117 @@ local AS = unpack(AddOnSkins)
 
 if not AS:CheckAddOn('ElvUI') then return end
 
+local function RemoveParchment(frame)
+	if frame then
+		if frame.spellTex then
+			frame.spellTex:SetAlpha(0)
+		end
+		if frame.spellTex2 then
+			frame.spellTex2:SetAlpha(0)
+		end
+	end
+end
+
 function AS:ParchmentRemover(event, addon)
 	if ElvUI[1].private.skins.blizzard.enable ~= true then return end
+
 	if addon == 'Blizzard_ArchaeologyUI' and ElvUI[1].private.skins.blizzard.archaeology == true then
 		AS:StripTextures(ArchaeologyFrame, true)
-		for i = 1, ARCHAEOLOGY_MAX_COMPLETED_SHOWN do
-			local artifact = _G["ArchaeologyFrameCompletedPageArtifact"..i]
-
-			if artifact then
-				_G["ArchaeologyFrameCompletedPageArtifact"..i.."ArtifactName"]:SetTextColor(1, .8, .1)
-				_G["ArchaeologyFrameCompletedPageArtifact"..i.."ArtifactSubText"]:SetTextColor(0.6, 0.6, 0.6)
-			end
-		end
 
 		for i = 1, ARCHAEOLOGY_MAX_RACES do
-			local frame = _G['ArchaeologyFrameSummaryPageRace'..i]
+			local frame = ArchaeologyFrame.summaryPage['race'..i]
+			local artifact = ArchaeologyFrame.completedPage['artifact'..i]
+			frame.raceName:SetTextColor(1, 1, 1)
 
-			if frame then
-				frame.raceName:SetTextColor(1, 1, 1)
+			artifact.artifactName:SetTextColor(1, .8, .1)
+			artifact.artifactSubText:SetTextColor(0.6, 0.6, 0.6)
+		end
+
+		for _, Frame in pairs({ ArchaeologyFrame.completedPage, ArchaeologyFrame.summaryPage }) do
+			for i = 1, Frame:GetNumRegions() do
+				local Region = select(i, Frame:GetRegions())
+				if Region:IsObjectType("FontString") then
+					Region:SetTextColor(1, .8, .1)
+				end
 			end
 		end
 
-		for i = 1, ArchaeologyFrameCompletedPage:GetNumRegions() do
-			local region = select(i, ArchaeologyFrameCompletedPage:GetRegions())
-			if region:GetObjectType() == 'FontString' then
-				region:SetTextColor(1, .8, .1)
-			end
-		end
+		ArchaeologyFrame.completedPage.infoText:SetTextColor(1, 1, 1)
 
-		for i = 1, ArchaeologyFrameSummaryPage:GetNumRegions() do
-			local region = select(i, ArchaeologyFrameSummaryPage:GetRegions())
-			if region:GetObjectType() == 'FontString' then
-				region:SetTextColor(1, .8, .1)
-			end
-		end
+		ArchaeologyFrame.artifactPage.historyTitle:SetTextColor(1, .8, .1)
 
-		ArchaeologyFrameCompletedPage.infoText:SetTextColor(1, 1, 1)
-		ArchaeologyFrameHelpPageTitle:SetTextColor(1, .8, .1)
+		ArchaeologyFrameArtifactPageHistoryScrollChildText:SetTextColor(1, 1, 1)
+
+		ArchaeologyFrame.helpPage.titleText:SetTextColor(1, .8, .1)
+
 		ArchaeologyFrameHelpPageDigTitle:SetTextColor(1, .8, .1)
 		ArchaeologyFrameHelpPageHelpScrollHelpText:SetTextColor(1, 1, 1)
-		ArchaeologyFrameArtifactPageHistoryTitle:SetTextColor(1, .8, .1)
-		ArchaeologyFrameArtifactPageHistoryScrollChildText:SetTextColor(1, 1, 1)
 	end
+
 	if addon == 'Blizzard_Calendar' and ElvUI[1].private.skins.blizzard.calendar == true then
 		for i = 1, 42 do
-			_G['CalendarDayButton'..i]:SetFrameLevel(_G['CalendarDayButton'..i]:GetFrameLevel() + 1)
-			AS:StripTextures(_G['CalendarDayButton'..i])
-			AS:SetTemplate(_G['CalendarDayButton'..i], 'Default')
-			_G['CalendarDayButton'..i..'OverlayFrame']:SetAlpha(1)
-			AS:StripTextures(_G['CalendarDayButton'..i..'DarkFrame'])
-			_G['CalendarDayButton'..i..'EventTexture']:SetAlpha(1)
+			local Button = _G["CalendarDayButton"..i]
+			Button:SetFrameLevel(Button:GetFrameLevel() + 1)
+			AS:SkinFrame(Button)
+			Button:SetBackdropColor(0, 0, 0, 0)
 			for j = 1, 4 do
-				local b = _G['CalendarDayButton'..i..'EventButton'..j]
-				AS:StripTextures(b)
-				AS:StyleButton(b)
+				local EventButton = _G["CalendarDayButton"..i.."EventButton"..j]
+				AS:StripTextures(EventButton)
+				AS:StyleButton(EventButton)
 			end
 		end
 	end
+
 	if event == 'PLAYER_ENTERING_WORLD' then
 		if ElvUI[1].private.skins.blizzard.gossip == true then
-			GossipGreetingScrollFrame.spellTex:SetTexture('')
+			RemoveParchment(GossipGreetingScrollFrame)
+
 			for i = 1, NUMGOSSIPBUTTONS do
-				obj = select(3, _G['GossipTitleButton'..i]:GetRegions())
-				obj:SetTextColor(1,1,1)
+				_G["GossipTitleButton"..i]:GetFontString():SetTextColor(1, 1, 1)
 			end
 
-			GossipGreetingText:SetTextColor(1,1,1)
+			GossipGreetingText:SetTextColor(1, 1, 1)
 
-			hooksecurefunc('GossipFrameUpdate', function()
+			hooksecurefunc("GossipFrameUpdate", function()
 				for i = 1, NUMGOSSIPBUTTONS do
-					local button = _G['GossipTitleButton'..i]
+					local button = _G["GossipTitleButton"..i]
 					if button:GetFontString() then
-						if button:GetFontString():GetText() and button:GetFontString():GetText():find('|cff000000') then
-							button:GetFontString():SetText(string.gsub(button:GetFontString():GetText(), '|cff000000', '|cffffe519'))
+						local Text = button:GetFontString():GetText()
+						if Text and strfind(Text, '|cff000000') then
+							button:GetFontString():SetText(string.gsub(Text, '|cff000000', '|cffffe519'))
 						end
 					end
 				end
 			end)
 		end
+
 		if ElvUI[1].private.skins.blizzard.quest == true then
-			QuestScrollFrame:HookScript('OnUpdate', function(self)
-				if self.spellTex and self.spellTex2 then
-					self.spellTex:SetTexture("")
-					self.spellTex:SetTexture("")
-				end
-			end)
-			QuestDetailScrollFrame:HookScript('OnUpdate', function(self)
-				self.spellTex:SetTexture("")
-			end)
-			QuestRewardScrollFrame:HookScript('OnUpdate', function(self)
-				self.spellTex:SetTexture("")
-			end)
-			QuestLogPopupDetailFrameScrollFrame:HookScript('OnUpdate', function(self)
-				self.spellTex:SetTexture("")
-			end)
+			AS:StripTextures(QuestFrameInset, true)
 
 			AS:StripTextures(QuestMapFrame.DetailsFrame, true)
 
-			if QuestProgressScrollFrame.spellTex then
-				QuestProgressScrollFrame.spellTex:SetTexture("")
-			end
+			RemoveParchment(QuestDetailScrollFrame)
+			RemoveParchment(QuestProgressScrollFrame)
+			RemoveParchment(QuestGreetingScrollFrame)
 
-			if QuestGreetingScrollFrame.spellTex then
-				QuestGreetingScrollFrame.spellTex:SetTexture("")
-			end
+			QuestScrollFrame:HookScript('OnUpdate', function(self)
+				RemoveParchment(self)
+			end)
+			QuestRewardScrollFrame:HookScript('OnUpdate', function(self)
+				RemoveParchment(self)
+			end)
+			QuestLogPopupDetailFrameScrollFrame:HookScript('OnUpdate', function(self)
+				RemoveParchment(self)
+			end)
+
+			AS:StripTextures(QuestMapFrame.DetailsFrame, true)
 
 			hooksecurefunc('QuestInfoItem_OnClick', function(self)
 				QuestInfoItemHighlight:SetOutside(self.Icon)
 
 				self.Name:SetTextColor(1, .8, .1)
 				local parent = self:GetParent()
-				for i=1, #parent.RewardButtons do
+				for i = 1, #parent.RewardButtons do
 					local questItem = QuestInfoRewardsFrame.RewardButtons[i]
 					if(questItem ~= self) then
 						questItem.Name:SetTextColor(1, 1, 1)
@@ -212,38 +212,31 @@ function AS:ParchmentRemover(event, addon)
 					end
 				end
 			end)
-
-			AS:StripTextures(QuestFrameInset)
 		end
+
 		if ElvUI[1].private.skins.blizzard.spellbook == true then
-			SpellBookPage1:Kill()
-			SpellBookPage2:Kill()
+			AS:StripTextures(SpellBookPage1, true)
+			AS:StripTextures(SpellBookPage2, true)
 			SpellBookPageText:SetTextColor(0.6, 0.6, 0.6)
 
-			--First Aid is no more
-			local professionheaders = {
-				"PrimaryProfession1",
-				"PrimaryProfession2",
-				"SecondaryProfession1",
-				"SecondaryProfession2",
-				"SecondaryProfession3"
-			}
 
-			for _, header in pairs(professionheaders) do
-				_G[header.."Missing"]:SetTextColor(1, 0.8, 0)
-				_G[header.."Missing"]:SetShadowColor(0, 0, 0)
-				_G[header.."Missing"]:SetShadowOffset(1, -1)
-				_G[header].missingText:SetTextColor(0.6, 0.6, 0.6)
+			for _, Button in pairs({ PrimaryProfession1, PrimaryProfession2, SecondaryProfession1, SecondaryProfession2, SecondaryProfession3 }) do
+				Button.missingHeader:SetTextColor(1, .82, 0)
+				Button.missingText:SetTextColor(1, 1, 1)
 			end
 
-			hooksecurefunc("SpellButton_UpdateButton", function(self, first)
+			hooksecurefunc("SpellButton_UpdateButton", function()
 				for i = 1, SPELLS_PER_PAGE do
-					local r, g, b = _G["SpellButton"..i.."SpellName"]:GetTextColor()
+					local Button = _G["SpellButton"..i]
+					Button:SetHighlightTexture('')
+					local r = Button.SpellName:GetTextColor()
 					if r < 0.8 then
-						_G["SpellButton"..i.."SpellName"]:SetTextColor(0.6, 0.6, 0.6)
+						Button.SpellName:SetTextColor(0.6, 0.6, 0.6)
+					else
+						Button.SpellName:SetTextColor(1, 1, 1)
 					end
-					_G["SpellButton"..i.."SubSpellName"]:SetTextColor(0.6, 0.6, 0.6)
-					_G["SpellButton"..i.."RequiredLevelString"]:SetTextColor(0.6, 0.6, 0.6)
+					Button.SpellSubName:SetTextColor(0.6, 0.6, 0.6)
+					Button.RequiredLevelString:SetTextColor(0.6, 0.6, 0.6)
 				end
 			end)
 		end
