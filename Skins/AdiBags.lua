22dad410438c5c3e19b861f0da419abcad30d9f9
Azilroka <darktemplar9900@yahoo.com>
2019-02-27 22:16:03 -0600

    AdiBags

--- Skins/AdiBags.lua
+++ Skins/AdiBags.lua
@@ -2,43 +2,45 @@ local AS = unpack(AddOnSkins)
 
 if not AS:CheckAddOn('AdiBags') then return end
 
-local function SkinFrame(frame)
-	local region = frame.HeaderRightRegion
-	AS:SkinFrame(frame)
-	AS:SkinFrame(_G[frame:GetName()..'Bags'], 'Default', true)
-	AS:SkinCloseButton(frame.CloseButton)
-	for i = 1, 5 do
-		local widget = region.widgets[i] and region.widgets[i].widget
-		if widget then
-			if widget:IsObjectType('Button') then
-				AS:SkinButton(widget, true)
-			elseif widget:IsObjectType('EditBox') then
-				AS:SkinEditBox(widget)
-			end
-		end
-	end
-	AS:SkinTexture(frame.BagSlotButton:GetNormalTexture())
-end
-
 function AS:AdiBags(event)
 	local AdiBags = LibStub('AceAddon-3.0'):GetAddon('AdiBags')
 
-	if event == 'PLAYER_ENTERING_WORLD' then
-		AS:Delay(1, function()
-			if not AdiBagsContainer1 then ToggleBackpack() ToggleBackpack() end
-			if AdiBagsContainer1 then
-				SkinFrame(AdiBagsContainer1)
-				AdiBagsContainer1SearchBox:SetPoint('TOPRIGHT', AdiBagsSimpleLayeredRegion2, 'TOPRIGHT', -75, -1)
+	AdiBags:HookBagFrameCreation(AdiBags, function(bag)
+		local frame = bag:GetFrame()
+		AS:SkinFrame(frame)
+		AS:SkinCloseButton(frame.CloseButton)
+		for i = 1, 5 do
+			local widget = frame.HeaderRightRegion.widgets[i] and frame.HeaderRightRegion.widgets[i].widget
+			if widget then
+				if widget:IsObjectType('Button') then
+					AS:SkinButton(widget, true)
+				elseif widget:IsObjectType('EditBox') then
+					AS:SkinEditBox(widget)
+				end
 			end
-		end)
-	elseif event == 'BANKFRAME_OPENED' then
-		AS:Delay(0.1, function()
-			if AdiBagsContainer2 then
-				SkinFrame(AdiBagsContainer2)
-				AS:UnregisterSkinEvent('AdiBags', event)
+		end
+		AS:SkinTexture(frame.BagSlotButton:GetNormalTexture())
+	end)
+
+	AS:RawHook(AdiBags, "CreateBagSlotPanel", function(this, ...)
+		local bPanel = AS.hooks[this].CreateBagSlotPanel(this, ...)
+			AS:SetTemplate(bPanel)
+			for _, v in pairs(bPanel.buttons) do
+				AS:SetTemplate(v)
 			end
-		end)
-	end
+			return bPanel
+		end,
+	true)
+
+	AdiBags:RegisterMessage("AdiBags_UpdateButton", function(event, btn)
+		AS:SkinIconButton(btn)
+		if btn.IconQuestTexture:GetBlendMode() == "ADD" then
+			btn:SetBackdropBorderColor(btn.IconQuestTexture:GetVertexColor())
+			btn.IconQuestTexture:Hide()
+		else
+			btn.IconQuestTexture:Show()
+		end
+	end)
 end
 
-AS:RegisterSkin('AdiBags', AS.AdiBags, 'BANKFRAME_OPENED')
+AS:RegisterSkin('AdiBags', AS.AdiBags)
