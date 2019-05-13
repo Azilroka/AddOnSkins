cb96cc44dceea0665ccbaa23fe6bcf4da9b8385c
Merathilis <neo0608@gmail.com>
2019-05-13 15:47:03 +0200

    Steal Tabs and Upvalue

--- Skins/RaiderIO.lua
+++ Skins/RaiderIO.lua
@@ -1,10 +1,15 @@
 local AS = unpack(AddOnSkins)
-
 if not AS:CheckAddOn('RaiderIO') then return end
 
+-- Cache global variables
+--Lua functions
+local _G = _G
+--WoW API / Variables
+-- GLOBALS:
+
 function AS:RaiderIO()
-	RaiderIO_ProfileTooltip:SetScript("OnShow", function()
-				AS:SkinTooltip(RaiderIO_ProfileTooltip)
+	_G.RaiderIO_ProfileTooltip:SetScript("OnShow", function()
+		AS:SkinTooltip(_G.RaiderIO_ProfileTooltip)
 	end)
 end
 
