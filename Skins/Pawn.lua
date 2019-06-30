3509d483aa699c530d83fb8acc8e84315113702f
Azilroka <darktemplar9900@yahoo.com>
2019-06-30 15:49:34 -0500

    Pawn

--- Skins/Pawn.lua
+++ Skins/Pawn.lua
@@ -2,75 +2,94 @@ local AS = unpack(AddOnSkins)
 
 if not AS:CheckAddOn('Pawn') then return end
 
-function AS:Pawn()
-	if addon == 'Blizzard_InspectUI' or IsAddOnLoaded('Blizzard_InspectUI') then
-		InspectFrame:HookScript('OnShow', function() AS:SkinButton(PawnUI_InspectPawnButton, true) PawnUI_InspectPawnButton:SetFormattedText(" Pawn ") PawnUI_InspectPawnButton:ClearAllPoints() PawnUI_InspectPawnButton:Point('RIGHT', InspectFrameCloseButton, 'LEFT', -8, -4) PawnUI_InspectPawnButton:Size(66, 20)end)
-		AS:UnregisterSkinEvent('Pawn', event)
+function AS:Pawn(event, addon)
+	local Texture = "Interface\\AddOns\\Pawn\\Textures\\PawnLogo"
+
+	if addon == 'Blizzard_InspectUI' or event == 'PLAYER_ENTERING_WORLD' and IsAddOnLoaded('Blizzard_InspectUI') then
+		AS:Delay(0.1, function()
+			AS:SkinButton(PawnUI_InspectPawnButton, true)
+			PawnUI_InspectPawnButton:SetNormalTexture(Texture)
+			PawnUI_InspectPawnButton:GetNormalTexture():SetTexCoord(0, 1, 0, 1)
+			PawnUI_InspectPawnButton:ClearAllPoints()
+			PawnUI_InspectPawnButton:Point('RIGHT', InspectFrameCloseButton, 'LEFT', -8, -4)
+			PawnUI_InspectPawnButton:Size(40, 20)
+		end)
 	end
 
-	-- General
-	AS:SkinFrame(PawnUIFrame)
-	AS:SkinCloseButton(PawnUIFrame_TinyCloseButton)
-	AS:SkinScrollBar(PawnUIScaleSelectorScrollFrameScrollBar)
-	AS:SkinFrame(PawnUIStringDialog)
-	AS:SkinEditBox(PawnUIStringDialog_TextBox)
-	AS:SkinButton(PawnUIStringDialog_OKButton)
-	AS:SkinButton(PawnUIStringDialog_CancelButton)
+	if addon == 'Blizzard_ItemSocketingUI' or event == 'PLAYER_ENTERING_WORLD' and IsAddOnLoaded('Blizzard_ItemSocketingUI') then
 
-	AS:SkinButton(PawnUI_InventoryPawnButton)
-	PawnUI_InventoryPawnButton:SetFormattedText(" Pawn ")
-	-- PawnUI_InspectPawnButton:SetFormattedText(" Pawn ")
-
-	for i = 1, PawnUIFrame.numTabs do
-		AS:SkinTab(_G["PawnUIFrameTab"..i])
+		AS:Delay(0.1, function()
+			AS:SkinButton(PawnUI_SocketingPawnButton)
+			PawnUI_SocketingPawnButton:Size(40, 20)
+			PawnUI_SocketingPawnButton:SetNormalTexture(Texture)
+			PawnUI_SocketingPawnButton:GetNormalTexture():SetTexCoord(0, 1, 0, 1)
+			AS:SkinTooltip(PawnSocketingTooltip)
+		end)
 	end
 
-	-- Scale
-	AS:SkinButton(PawnUIFrame_RenameScaleButton)
-	AS:SkinButton(PawnUIFrame_DeleteScaleButton)
-	AS:SkinCheckBox(PawnUIFrame_ShowScaleCheck)
-	AS:SkinButton(PawnUIFrame_ImportScaleButton)
-	AS:SkinButton(PawnUIFrame_ExportScaleButton)
-	AS:SkinButton(PawnUIFrame_CopyScaleButton)
-	AS:SkinButton(PawnUIFrame_NewScaleFromDefaultsButton)
-	AS:SkinButton(PawnUIFrame_NewScaleButton)
-	AS:SkinButton(PawnUIFrame_AutoSelectScalesOnButton) -- huge button, with highlight
-	AS:SkinButton(PawnUIFrame_AutoSelectScalesOffButton) -- huge button, with highlight
+	if event == 'PLAYER_ENTERING_WORLD' then
+		-- General
+		AS:SkinFrame(PawnUIFrame)
+		AS:SkinCloseButton(PawnUIFrame_TinyCloseButton)
+		AS:SkinScrollBar(PawnUIScaleSelectorScrollFrameScrollBar)
+		AS:SkinFrame(PawnUIStringDialog)
+		AS:SkinEditBox(PawnUIStringDialog_TextBox)
+		AS:SkinButton(PawnUIStringDialog_OKButton)
+		AS:SkinButton(PawnUIStringDialog_CancelButton)
 
-	-- Weights
-	AS:SkinScrollBar(PawnUIFrame_StatsListScrollBar)
-	AS:SkinCheckBox(PawnUIFrame_NormalizeValuesCheck)
-	AS:SkinEditBox(PawnUIFrame_StatValueBox)
-	AS:SkinButton(PawnUIFrame_ClearValueButton)
+		AS:SkinButton(PawnUI_InventoryPawnButton)
+		PawnUI_InventoryPawnButton:SetNormalTexture(Texture)
+		PawnUI_InventoryPawnButton:Size(40, 20)
+		PawnUI_InventoryPawnButton:GetNormalTexture():SetTexCoord(0, 1, 0, 1)
 
-	-- Compare
-	AS:SkinBackdropFrame(PawnUICompareItemIcon1, nil, true, false, true)
-	AS:SkinBackdropFrame(PawnUICompareItemIcon2, nil, true, false, true)
-	-- AS:SkinButton(PawnUIFrame_ClearItemsButton) -- This button needs some attention
-	AS:SkinButton(PawnUIFrame_CompareSwapButton)
-	AS:SkinScrollBar(PawnUICompareScrollFrameScrollBar)
+		for i = 1, PawnUIFrame.numTabs do
+			AS:SkinTab(_G["PawnUIFrameTab"..i])
+		end
 
-	-- Gems
-	AS:SkinEditBox(PawnUIFrame_GemQualityLevelBox, 40, 20)
-	AS:SkinScrollBar(PawnUIGemScrollFrameScrollBar)
+		-- Scale
+		AS:SkinButton(PawnUIFrame_RenameScaleButton)
+		AS:SkinButton(PawnUIFrame_DeleteScaleButton)
+		AS:SkinCheckBox(PawnUIFrame_ShowScaleCheck)
+		AS:SkinButton(PawnUIFrame_ImportScaleButton)
+		AS:SkinButton(PawnUIFrame_ExportScaleButton)
+		AS:SkinButton(PawnUIFrame_CopyScaleButton)
+		AS:SkinButton(PawnUIFrame_NewScaleFromDefaultsButton)
+		AS:SkinButton(PawnUIFrame_NewScaleButton)
+		AS:SkinButton(PawnUIFrame_AutoSelectScalesOnButton) -- huge button, with highlight
+		AS:SkinButton(PawnUIFrame_AutoSelectScalesOffButton) -- huge button, with highlight
 
-	-- Options
-	AS:SkinButton(PawnUIFrame_ResetUpgradesButton)
-	AS:SkinCheckBox(PawnUIFrame_EnchantedValuesCheck)
-	AS:SkinCheckBox(PawnUIFrame_ShowIconsCheck)
-	AS:SkinCheckBox(PawnUIFrame_ShowSpecIconsCheck)
-	AS:SkinCheckBox(PawnUIFrame_AlignRightCheck)
-	AS:SkinCheckBox(PawnUIFrame_ColorTooltipBorderCheck)
-	AS:SkinCheckBox(PawnUIFrame_ShowBagUpgradeAdvisorCheck)
-	AS:SkinCheckBox(PawnUIFrame_ShowLootUpgradeAdvisorCheck)
-	AS:SkinCheckBox(PawnUIFrame_ShowQuestUpgradeAdvisorCheck)
-	AS:SkinCheckBox(PawnUIFrame_ShowSocketingAdvisorCheck)
-	AS:SkinCheckBox(PawnUIFrame_IgnoreGemsWhileLevelingCheck)
-	AS:SkinCheckBox(PawnUIFrame_DebugCheck)
-	AS:SkinCheckBox(PawnUIFrame_ShowItemIDsCheck)
+		-- Weights
+		AS:SkinScrollBar(PawnUIFrame_StatsListScrollBar)
+		AS:SkinCheckBox(PawnUIFrame_NormalizeValuesCheck)
+		AS:SkinEditBox(PawnUIFrame_StatValueBox)
+		AS:SkinButton(PawnUIFrame_ClearValueButton)
 
-	--Resize Buttons
-	PawnUI_InventoryPawnButton:Size(112, 20)
+		-- Compare
+		AS:SkinBackdropFrame(PawnUICompareItemIcon1, nil, true, false, true)
+		AS:SkinBackdropFrame(PawnUICompareItemIcon2, nil, true, false, true)
+		-- AS:SkinButton(PawnUIFrame_ClearItemsButton) -- This button needs some attention
+		AS:SkinButton(PawnUIFrame_CompareSwapButton)
+		AS:SkinScrollBar(PawnUICompareScrollFrameScrollBar)
+
+		-- Gems
+		AS:SkinEditBox(PawnUIFrame_GemQualityLevelBox, 40, 20)
+		AS:SkinScrollBar(PawnUIGemScrollFrameScrollBar)
+
+		-- Options
+		AS:SkinButton(PawnUIFrame_ResetUpgradesButton)
+		AS:SkinCheckBox(PawnUIFrame_EnchantedValuesCheck)
+		AS:SkinCheckBox(PawnUIFrame_ShowIconsCheck)
+		AS:SkinCheckBox(PawnUIFrame_ShowSpecIconsCheck)
+		AS:SkinCheckBox(PawnUIFrame_AlignRightCheck)
+		AS:SkinCheckBox(PawnUIFrame_ColorTooltipBorderCheck)
+		AS:SkinCheckBox(PawnUIFrame_ShowBagUpgradeAdvisorCheck)
+		AS:SkinCheckBox(PawnUIFrame_ShowLootUpgradeAdvisorCheck)
+		AS:SkinCheckBox(PawnUIFrame_ShowQuestUpgradeAdvisorCheck)
+		AS:SkinCheckBox(PawnUIFrame_ShowSocketingAdvisorCheck)
+		AS:SkinCheckBox(PawnUIFrame_IgnoreGemsWhileLevelingCheck)
+		AS:SkinCheckBox(PawnUIFrame_DebugCheck)
+		AS:SkinCheckBox(PawnUIFrame_ShowItemIDsCheck)
+	end
 end
 
-AS:RegisterSkin('Pawn', AS.Pawn)
+AS:RegisterSkin('Pawn', AS.Pawn, 'ADDON_LOADED')
