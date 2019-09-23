014a301ad52d0af89e37aa7a500dc138bb2856a5
Azilroka <darktemplar9900@yahoo.com>
2019-09-22 21:09:08 -0500

    ExtendedCharacterStats

--- Skins/Classic/ExtendedCharacterStats.lua
+++ Skins/Classic/ExtendedCharacterStats.lua
@@ -6,7 +6,7 @@ if not AS:CheckAddOn('ExtendedCharacterStats') then return end
 function AS:ExtendedCharacterStats()
 	AS:SkinFrame(ECS_StatsFrame)
 	AS:SkinCloseButton(ECS_StatsFrame.CloseButton)
-	AS:SkinScrollBar(ECSMainWindow.ScrollFrame.ScrollBar)
+	AS:SkinScrollBar(ECS_StatsFrame.ScrollFrame.ScrollBar)
 end
 
 AS:RegisterSkin('ExtendedCharacterStats', AS.ExtendedCharacterStats)
