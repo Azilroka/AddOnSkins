aef1d8f3747edf22bbfe23ba342b319481c58a6e
Azilroka <darktemplar9900@yahoo.com>
2019-11-16 18:35:42 -0600

    DBM / Core Update

--- Skins/AddOns/DBM.lua
+++ Skins/AddOns/DBM.lua
@@ -25,11 +25,11 @@ function AS:DBM(event, addon)
 
 						AS:SkinTexture(icon1, true)
 						icon1:ClearAllPoints()
-						icon1:SetPoint('BOTTOMRIGHT', frame, 'BOTTOMLEFT', AS:AdjustForTheme(-2), AS:AdjustForTheme(1))
+						icon1:SetPoint('BOTTOMRIGHT', frame, 'BOTTOMLEFT', AS:AdjustForTheme(-2), 1)
 
 						AS:SkinTexture(icon2, true)
 						icon2:ClearAllPoints()
-						icon2:SetPoint('BOTTOMLEFT', frame, 'BOTTOMRIGHT', AS:AdjustForTheme(2), AS:AdjustForTheme(1))
+						icon2:SetPoint('BOTTOMLEFT', frame, 'BOTTOMRIGHT', AS:AdjustForTheme(2), 1)
 
 						AS:SetInside(tbar, frame)
 
