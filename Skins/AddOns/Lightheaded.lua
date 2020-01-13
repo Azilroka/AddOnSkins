bcb0df85622a6b39b490b3aec39c7ca119deba00
NihilisticPandemonium <whiro@tukui.org>
2020-01-13 12:13:57 -0800

    Comment this out it errors.

--- Skins/AddOns/Lightheaded.lua
+++ Skins/AddOns/Lightheaded.lua
@@ -1,10 +1,12 @@
 local AS = unpack(AddOnSkins)
 
-if not AS:CheckAddOn('Lightheaded') then return end
+if not AS:CheckAddOn("Lightheaded") then
+	return
+end
 
 function AS:LightHeaded()
 	AS:SkinFrame(LightHeadedFrame)
-	QuestNPCModel:SetScale(UIParent:GetScale())
+	--QuestNPCModel:SetScale(UIParent:GetScale())
 	LightHeadedFrame:SetScale(UIParent:GetScale())
 	AS:SkinFrame(LightHeadedSearchBox)
 	AS:SkinTooltip(LightHeadedTooltip)
@@ -15,10 +17,10 @@ function AS:LightHeaded()
 	AS:SkinArrowButton(LightHeadedFrameSub.next)
 	LightHeadedFrameSub.prev:SetSize(16, 16)
 	LightHeadedFrameSub.next:SetSize(16, 16)
-	LightHeadedFrameSub.prev:SetPoint('RIGHT', LightHeadedFrameSub.page, 'LEFT', -25, 0)
-	LightHeadedFrameSub.next:SetPoint('LEFT', LightHeadedFrameSub.page, 'RIGHT', 25, 0)
+	LightHeadedFrameSub.prev:SetPoint("RIGHT", LightHeadedFrameSub.page, "LEFT", -25, 0)
+	LightHeadedFrameSub.next:SetPoint("LEFT", LightHeadedFrameSub.page, "RIGHT", 25, 0)
 	AS:SkinScrollBar(LightHeadedScrollFrameScrollBar)
-	LightHeadedFrameSub.title:SetTextColor(23/255, 132/255, 209/255)	
+	LightHeadedFrameSub.title:SetTextColor(23 / 255, 132 / 255, 209 / 255)
 end
 
-AS:RegisterSkin('Lightheaded', AS.LightHeaded)
+AS:RegisterSkin("Lightheaded", AS.LightHeaded)
