58690ac510514f7914378a70b4511e991c79a84c
NihilisticPandemonium <whiro@tukui.org>
2020-01-13 12:14:06 -0800

    Fix the Scrap skin.

--- Skins/AddOns/Scrap.lua
+++ Skins/AddOns/Scrap.lua
@@ -1,21 +1,25 @@
 local AS = unpack(AddOnSkins)
 
-if not AS:CheckAddOn('Scrap') then return end
+if not AS:CheckAddOn("Scrap") then
+	return
+end
 
 local AddOnCount = 0
 function AS:Scrap(event, addon)
-	if addon == 'Scrap_Merchant' or IsAddOnLoaded('Scrap_Merchant') then
-		if Scrap and Scrap:IsObjectType('Button') and not Scrap.IsSkinned then
-			AS:StyleButton(Scrap)
-			Scrap:SetTemplate('Default', true)
-			ScrapBorder:SetTexture()
-			ScrapIcon:SetTexture('Interface\\Addons\\Scrap\\Art\\Enabled Icon')
-			ScrapIcon:SetAllPoints()
+	if addon == "Scrap_Merchant" or IsAddOnLoaded("Scrap_Merchant") then
+		local Button = Scrap.Merchant
+		if Button and not Button.IsSkinned then
+			Button:OnEnable()
+			AS:StyleButton(Button)
+			Button:SetTemplate("Default", true)
+			Button.border:SetTexture()
+			Button.icon:SetTexture("Interface\\Addons\\Scrap\\Art\\Enabled Icon")
+			Button.icon:SetAllPoints()
 			local numTabs = MerchantFrame.numTabs
 			for i = numTabs, 1, -1 do
-				local tab = _G[('MerchantFrameTab%d'):format(i)]
+				local tab = _G[("MerchantFrameTab%d"):format(i)]
 				if tab then
-					if tab:GetText() == 'Scrap' then
+					if tab:GetText() == "Scrap" then
 						AS:SkinTab(tab)
 						break
 					end
@@ -23,13 +27,10 @@ function AS:Scrap(event, addon)
 					break
 				end
 			end
-			Scrap.IsSkinned = true
-			AddOnCount = AddOnCount + 1
+			Button.IsSkinned = true
 		end
-	end
 
-	if addon == 'Scrap_Visualizer' or IsAddOnLoaded('Scrap_Visualizer')then
-		AS:SkinFrame(ScrapVisualizer, 'Default')
+		AS:SkinFrame(ScrapVisualizer, "Default")
 		AS:StripTextures(ScrapVisualizerInset)
 		AS:StripTextures(ScrapVisualizerScroll)
 		AS:SkinScrollBar(ScrapVisualizerScrollBar)
@@ -37,13 +38,10 @@ function AS:Scrap(event, addon)
 		AS:SkinTab(ScrapVisualizerTab1)
 		AS:SkinTab(ScrapVisualizerTab2)
 		AS:SkinButton(ScrapVisualizerButton, true)
-		ScrapVisualizer:SetWidth(MerchantFrame:GetWidth()-6)
-		AddOnCount = AddOnCount + 1
-	end
+		ScrapVisualizer:SetWidth(MerchantFrame:GetWidth() - 6)
 
-	if AddOnCount == 2 then
-		AS:UnregisterSkinEvent('Scrap', 'ADDON_LOADED')
+		AS:UnregisterSkinEvent("Scrap", "ADDON_LOADED")
 	end
 end
 
-AS:RegisterSkin('Scrap', AS.Scrap, 'ADDON_LOADED')
+AS:RegisterSkin("Scrap", AS.Scrap, "ADDON_LOADED")
