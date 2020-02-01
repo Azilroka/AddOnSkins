7970a0532268a6da6688db0f64b35122e2283e8a
Azilroka <darktemplar9900@yahoo.com>
2020-02-01 16:49:58 -0600

    Lightheaded

--- Skins/AddOns/Lightheaded.lua
+++ Skins/AddOns/Lightheaded.lua
@@ -1,8 +1,6 @@
 local AS = unpack(AddOnSkins)
 
-if not AS:CheckAddOn("Lightheaded") then
-	return
-end
+if not AS:CheckAddOn('Lightheaded') then return end
 
 function AS:LightHeaded()
 	AS:SkinFrame(LightHeadedFrame)
@@ -17,10 +15,10 @@ function AS:LightHeaded()
 	AS:SkinArrowButton(LightHeadedFrameSub.next)
 	LightHeadedFrameSub.prev:SetSize(16, 16)
 	LightHeadedFrameSub.next:SetSize(16, 16)
-	LightHeadedFrameSub.prev:SetPoint("RIGHT", LightHeadedFrameSub.page, "LEFT", -25, 0)
-	LightHeadedFrameSub.next:SetPoint("LEFT", LightHeadedFrameSub.page, "RIGHT", 25, 0)
+	LightHeadedFrameSub.prev:SetPoint('RIGHT', LightHeadedFrameSub.page, 'LEFT', -25, 0)
+	LightHeadedFrameSub.next:SetPoint('LEFT', LightHeadedFrameSub.page, 'RIGHT', 25, 0)
 	AS:SkinScrollBar(LightHeadedScrollFrameScrollBar)
 	LightHeadedFrameSub.title:SetTextColor(23 / 255, 132 / 255, 209 / 255)
 end
 
-AS:RegisterSkin("Lightheaded", AS.LightHeaded)
+AS:RegisterSkin('Lightheaded', AS.LightHeaded)
