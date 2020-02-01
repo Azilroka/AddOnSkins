6cf77845fcd47f98f404480d58f23c392ef83372
Azilroka <darktemplar9900@yahoo.com>
2020-02-01 16:50:05 -0600

    Raider IO

--- Skins/AddOns/RaiderIO.lua
+++ Skins/AddOns/RaiderIO.lua
@@ -8,10 +8,14 @@ local _G = _G
 -- GLOBALS:
 
 function AS:RaiderIO()
-	AS:SkinTooltip(RaiderIO_CustomDropDownListMenuBackdrop)
-	_G.RaiderIO_ProfileTooltip:SetScript("OnShow", function()
-		AS:SkinTooltip(_G.RaiderIO_ProfileTooltip)
-	end)
+	if RaiderIO_CustomDropDownListMenuBackdrop then
+		AS:SkinTooltip(RaiderIO_CustomDropDownListMenuBackdrop)
+	end
+	if _G.RaiderIO_ProfileTooltip then
+		_G.RaiderIO_ProfileTooltip:SetScript("OnShow", function()
+			AS:SkinTooltip(_G.RaiderIO_ProfileTooltip)
+		end)
+	end
 end
 
 AS:RegisterSkin('RaiderIO', AS.RaiderIO)
