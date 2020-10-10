00be9d2a56c4619a3c651647ed2fcbefe0f54d84
Azilroka <darktemplar9900@yahoo.com>
2020-10-10 16:40:32 -0500

    SimC

--- Skins/AddOns/Simulationcraft.lua
+++ Skins/AddOns/Simulationcraft.lua
@@ -4,9 +4,9 @@ if not AS:CheckAddOn('Simulationcraft') then return end
 
 function AS:Simulationcraft()
 
-	AS:SkinFrame(SimcCopyFrame)
-	AS:SkinButton(SimcCopyFrameButton)
-	AS:SkinScrollBar(SimcCopyFrameScrollScrollBar)
+	AS:SkinFrame(SimcFrame)
+	AS:SkinButton(SimcFrameButton)
+	AS:SkinScrollBar(SimcFrameScrollScrollBar)
 end
 
 AS:RegisterSkin('Simulationcraft', AS.Simulationcraft)
