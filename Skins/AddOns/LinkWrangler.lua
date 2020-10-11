88b78cfd170ed6c5ebfd84d2efc987d62dcc5859
Azilroka <darktemplar9900@yahoo.com>
2020-10-10 19:45:25 -0500

    LinkWrangler

--- Skins/AddOns/LinkWrangler.lua
+++ Skins/AddOns/LinkWrangler.lua
@@ -3,35 +3,29 @@ local AS = unpack(AddOnSkins)
 if not AS:CheckAddOn('LinkWrangler') then return end
 
 function AS:LinkWrangler()
-	local Path = "Interface\\AddOns\\AddOnSkins\\Skins\\Buttons\\"
+	local Path = [[Interface\AddOns\AddOnSkins\Media\Textures\Buttons\]]
 
 	local function LWSkin_SkinButton(button, tex, distex)
-		if (button == nil) then
-			return
-		end
+		if not button then return end
+
 		button:SetNormalTexture(tex)
 		button:SetDisabledTexture(distex)
 		button:SetPushedTexture(tex)
 		button:SetHighlightTexture(tex)
-		return button
 	end
 
 	local function LWSkin(frame, link)
-		local r, g, b = 1, 1, 0
-		if GetItemInfo(link) then
-			local quality = select(3, GetItemInfo(link))
-			r, g, b = GetItemQualityColor(tonumber(quality))
-		end
-		AS:SetTemplate(frame, 'Default')
-		frame:SetBackdropBorderColor(r,g,b)
+		if frame.isSkinned then return end
+
 		local framename = frame:GetName()
+
 		LWSkin_SkinButton(_G[framename.."CloseButton"], Path.."close")
 		LWSkin_SkinButton(_G[framename.."MinButton"], Path.."min")
 		LWSkin_SkinButton(_G[framename.."CompButton"], Path.."compare", Path.."compareDisable")
 		LWSkin_SkinButton(_G[framename.."WhisperButton"], Path.."whisper")
 		LWSkin_SkinButton(_G[framename.."RelinkButton"], Path.."relink")
 		LWSkin_SkinButton(_G[framename.."DressupButton"], Path.."dress")
-		LWSkin_SkinButton(_G[framename.."CaptureButton"], Path.."compare")
+		frame.isSkinned = true
 	end
 
 	LinkWrangler.RegisterCallback("LinkWrangler_Skin", LWSkin, "show", "showcomp")
