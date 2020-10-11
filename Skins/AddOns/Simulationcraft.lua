5bc2202659712d3ae137fd69fa96c10924c9f799
Azilroka <darktemplar9900@yahoo.com>
2020-10-10 19:12:07 -0500

    SimC

--- Skins/AddOns/Simulationcraft.lua
+++ Skins/AddOns/Simulationcraft.lua
@@ -3,9 +3,14 @@ local AS = unpack(AddOnSkins)
 if not AS:CheckAddOn('Simulationcraft') then return end
 
 function AS:Simulationcraft()
-	AS:SkinFrame(SimcFrame)
-	AS:SkinButton(SimcFrameButton)
-	AS:SkinScrollBar(SimcScrollFrameScrollBar)
+	local Simulationcraft = LibStub("AceAddon-3.0"):GetAddon("Simulationcraft")
+	hooksecurefunc(Simulationcraft, 'GetMainFrame', function()
+		if not SimcFrame.isSkinned then
+			AS:SkinFrame(SimcFrame)
+			AS:SkinButton(SimcFrameButton)
+			AS:SkinScrollBar(SimcScrollFrameScrollBar)
+		end
+	end)
 end
 
 AS:RegisterSkin('Simulationcraft', AS.Simulationcraft)
