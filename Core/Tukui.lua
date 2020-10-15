33215a327af720ae79fb7a1b6af80434f774cb35
Azilroka <darktemplar9900@yahoo.com>
2020-10-14 19:40:00 -0500

    Tukui 20 Update

--- Core/Tukui.lua
+++ Core/Tukui.lua
@@ -8,25 +8,25 @@ function AS:UpdateMedia()
 		T, C = Tukui:unpack()
 	end
 
-	AS.HideShadows = C['General']['HideShadows']
+	AS.HideShadows = C.General.HideShadows
 
-	AS.DataTextFontSize = 12 -- T['DataTexts']['Size']
-	AS.DataTexts = T['DataTexts']
-	AS.InfoLeft = T['Panels']['DataTextLeft']
-	AS.InfoRight = T['Panels']['DataTextRight']
-	AS.ChatBackgroundRight = T['Panels']['RightChatBG']
-	AS.ChatBackgroundLeft = T['Panels']['LeftChatBG']
-	AS.TabsRightBackground = T['Panels']['TabsBGRight']
-	AS.TabsLeftBackground = T['Panels']['TabsBGLeft']
-	AS.Minimap = T['Maps']['Minimap']
-	AS.ActionBar1 = T['Panels']['ActionBar1']
-	AS.ActionBar2 = T['Panels']['ActionBar2']
-	AS.ActionBar3 = T['Panels']['ActionBar3']
-	AS.ActionBar4 = T['Panels']['ActionBar4']
+	AS.DataTextFontSize = 12
+	AS.DataTexts = T.DataTexts
+	AS.InfoLeft = T.DataTexts.Panels.Left
+	AS.InfoRight = T.DataTexts.Panels.Right
+	AS.ChatBackgroundRight = T.Chat.Panels.RightChat
+	AS.ChatBackgroundLeft = T.Chat.Panels.LeftChat
+	AS.TabsRightBackground = T.Chat.Panels.RightChatTabs
+	AS.TabsLeftBackground = T.Chat.Panels.LeftChatTabs
+	AS.Minimap = T.Maps.Minimap
+	AS.ActionBar1 = T.ActionBars.Bars.Bar1
+	AS.ActionBar2 = T.ActionBars.Bars.Bar2
+	AS.ActionBar3 = T.ActionBars.Bars.Bar3
+	AS.ActionBar4 = T.ActionBars.Bars.Bar4
 
-	AS.Blank = C['Medias']['Blank']
-	AS.NormTex = C['Medias']['Normal']
-	AS.Font = C['Medias']['Font']
-	AS.BackdropColor = C['General']['BackdropColor']
-	AS.BorderColor = C['General']['BorderColor']
+	AS.Blank = C.Medias.Blank
+	AS.NormTex = C.Medias.Normal
+	AS.Font = C.Medias.Font
+	AS.BackdropColor = C.General.BackdropColor
+	AS.BorderColor = C.General.BorderColor
 end
