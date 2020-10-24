cd5a7b29afe01b567b30c4b7e2edfaee1639b29d
Azilroka <darktemplar9900@yahoo.com>
2020-10-23 19:27:18 -0500

    Cleanup BackdropTemplate

--- Core/API.lua
+++ Core/API.lua
@@ -382,7 +382,8 @@ function AS:SetTemplate(Frame, Template, Texture)
 
 	if AS:CheckOption('Theme') == 'ThickBorder' or AS:CheckOption('Theme') == 'TwoPixel' then
 		for _, Inset in pairs({ 'InsideBorder', 'OutsideBorder' }) do
-			Frame[Inset] = CreateFrame('Frame', nil, Frame, 'BackdropTemplate')
+			Frame[Inset] = CreateFrame('Frame', nil, Frame)
+			if not Frame[Inset].SetBackdrop then _G.Mixin(Frame[Inset], _G.BackdropTemplateMixin) end
 			Frame[Inset]:SetBackdrop({ edgeFile = AS.Blank, edgeSize = 1 })
 			Frame[Inset]:SetBackdropBorderColor(0, 0, 0, 1)
 		end
