893d22baf6a921948527ed6b02da16ab93bedb14
Torch <ben@behub.com>
2020-11-03 19:26:38 +0000

    Update Classic Quest Log

--- Skins/AddOns/ClassicQuestLog.lua
+++ Skins/AddOns/ClassicQuestLog.lua
@@ -8,12 +8,12 @@ function AS:ClassicQuestLog()
 	AS:SkinScrollBar(ClassicQuestLogDetailScrollFrameScrollBar)
 	AS:StripTextures(ClassicQuestLogInset)
 	AS:StripTextures(ClassicQuestLogScrollFrame.expandAll)
-	AS:StripTextures(ClassicQuestLog.count)
+	AS:StripTextures(ClassicQuestLog.chrome.countFrame)
 	AS:StripTextures(ClassicQuestLogScrollFrame)
 	AS:StripTextures(ClassicQuestLogDetailScrollFrame)
 
-	for i = 1, ClassicQuestLog:GetNumChildren() do
-		local object = select(i, ClassicQuestLog:GetChildren())
+	for i = 1, ClassicQuestLog.chrome:GetNumChildren() do
+		local object = select(i, ClassicQuestLog.chrome:GetChildren())
 		if object:IsObjectType('Button') then
 			if object:GetText() ~= nil then
 				AS:SkinButton(object, true)
@@ -27,21 +27,21 @@ function AS:ClassicQuestLog()
 	ClassicQuestLogScrollFrame.expandAll:ClearAllPoints()
 	ClassicQuestLogScrollFrame.expandAll:SetPoint('BOTTOMLEFT', ClassicQuestLog, 'TOPLEFT', 10, -53)
 
-	ClassicQuestLog.count:SetSize(100, 20)
-	ClassicQuestLog.count:ClearAllPoints()
-	ClassicQuestLog.count:SetPoint('LEFT', ClassicQuestLogScrollFrame.expandAll, 'RIGHT', -4, -4)
+	ClassicQuestLog.chrome.countFrame:SetSize(100, 20)
+	ClassicQuestLog.chrome.countFrame:ClearAllPoints()
+	ClassicQuestLog.chrome.countFrame:SetPoint('LEFT', ClassicQuestLogScrollFrame.expandAll, 'RIGHT', -4, -4)
 
 	--Reposition Show Map Button
-	ClassicQuestLog.mapButton:ClearAllPoints()
-	ClassicQuestLog.mapButton:SetPoint('BOTTOMRIGHT', ClassicQuestLog, 'TOPRIGHT', 0, -59)
-	ClassicQuestLog.mapButton.text:SetFormattedText("Click") -- Install addon and fix this button.
+	ClassicQuestLog.chrome.mapButton:ClearAllPoints()
+	ClassicQuestLog.chrome.mapButton:SetPoint('BOTTOMRIGHT', ClassicQuestLog, 'TOPRIGHT', 0, -59)
+	ClassicQuestLog.chrome.mapButton.text:SetFormattedText("Click") -- Install addon and fix this button.
 
 	--Resize Expand/Collapse Button
 	ClassicQuestLogScrollFrame.expandAll:SetSize(120, 30)
 	ClassicQuestLogScrollFrame.expandAll:SetFormattedText("Expand/Collapse")
 
 	--Resize Show Map Button
-	ClassicQuestLog.mapButton:SetSize(56, 40)
+	ClassicQuestLog.chrome.mapButton:SetSize(56, 40)
 end
 
 AS:RegisterSkin('Classic Quest Log', AS.ClassicQuestLog)
