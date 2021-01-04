e5e495b0a6afa1dafda2a600a4848f4836e17e7c
Merathilis <neo0608@gmail.com>
2021-01-04 18:32:42 +0100

    Raider.IO tooltip changes.

--- Skins/AddOns/RaiderIO.lua
+++ Skins/AddOns/RaiderIO.lua
@@ -8,14 +8,28 @@ local _G = _G
 -- GLOBALS:
 
 function AS:RaiderIO()
-	if RaiderIO_CustomDropDownListMenuBackdrop then
-		AS:SkinTooltip(RaiderIO_CustomDropDownListMenuBackdrop)
+	if _G.RaiderIO_CustomDropDownListMenuBackdrop then
+		AS:SkinTooltip(_G.RaiderIO_CustomDropDownListMenuBackdrop)
 	end
+
+	_G.PVEFrame:HookScript("OnShow", function(self)
+		if not _G.RaiderIOProfileTooltip.IsSkinned then
+			_G.RaiderIOProfileTooltip:SetBackdrop(nil)
+			_G.RaiderIOProfileTooltip.SetBackdrop = AS.Noop
+
+			AS:SkinTooltip(_G.RaiderIOProfileTooltip)
+
+			_G.RaiderIOProfileTooltip.IsSkinned = true
+		end
+	end)
+
+	--[[ Azil pls check
 	if _G.RaiderIO_ProfileTooltip then
 		_G.RaiderIO_ProfileTooltip:SetScript("OnShow", function()
 			AS:SkinTooltip(_G.RaiderIO_ProfileTooltip)
 		end)
 	end
+	]]
 end
 
 AS:RegisterSkin('RaiderIO', AS.RaiderIO)
