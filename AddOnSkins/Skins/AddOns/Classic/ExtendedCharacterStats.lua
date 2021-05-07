2e5783717646703a5375d1aec4339ee60524b2d7
Luckyone <Fussel.13@web.de>
2021-05-07 17:46:07 +0200

    Skin ECS ToggleButton aswell

--- AddOnSkins/Skins/AddOns/Classic/ExtendedCharacterStats.lua
+++ AddOnSkins/Skins/AddOns/Classic/ExtendedCharacterStats.lua
@@ -5,6 +5,7 @@ if not AS:CheckAddOn('ExtendedCharacterStats') then return end
 function AS:ExtendedCharacterStats()
 	AS:SkinFrame(ECS_StatsFrame)
 	AS:SkinButton(ECS_StatsFrame.configButton)
+	AS:SkinButton(ECS_ToggleButton)
 	AS:SkinCloseButton(ECS_StatsFrame.CloseButton)
 	AS:SkinScrollBar(ECS_StatsFrame.ScrollFrame.ScrollBar)
 	ECS_StatsFrame.ScrollFrame.ScrollBar:Point('Right', ECS_StatsFrame, -1, -1)
