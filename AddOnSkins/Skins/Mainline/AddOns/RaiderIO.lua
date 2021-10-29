d844022cd786d9c624cffe9c2d347a7a19ba1bfb
Luckyone <Luckyone.Development@pm.me>
2021-10-29 21:57:08 +0200

    Update Raider.IO

--- AddOnSkins/Skins/Mainline/AddOns/RaiderIO.lua
+++ AddOnSkins/Skins/Mainline/AddOns/RaiderIO.lua
@@ -1,11 +1,7 @@
 local AS = unpack(AddOnSkins)
 if not AS:CheckAddOn('RaiderIO') then return end
 
--- Cache global variables
---Lua functions
 local _G = _G
---WoW API / Variables
--- GLOBALS:
 
 function AS:RaiderIO()
 	if _G.RaiderIO_CustomDropDownListMenuBackdrop then
@@ -13,10 +9,10 @@ function AS:RaiderIO()
 	end
 
 	_G.PVEFrame:HookScript("OnShow", function(self)
-		if not _G.RaiderIOProfileTooltip.IsSkinned then
-			AS:SkinFrame(_G.RaiderIOProfileTooltip)
+		if not _G.RaiderIO_ProfileTooltip.IsSkinned then
+			AS:SkinFrame(_G.RaiderIO_ProfileTooltip)
 
-			_G.RaiderIOProfileTooltip.IsSkinned = true
+			_G.RaiderIO_ProfileTooltip.IsSkinned = true
 		end
 	end)
 end
