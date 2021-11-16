018184e131430761fd1d7f15445ddf09df3af3a8
Luckyone <Luckyone.Development@pm.me>
2021-11-16 03:59:50 +0100

    Update ECS

--- AddOnSkins/Skins/AddOns/ExtendedCharacterStats.lua
+++ AddOnSkins/Skins/AddOns/ExtendedCharacterStats.lua
@@ -3,6 +3,9 @@ local AS = unpack(AddOnSkins)
 if not AS:CheckAddOn('ExtendedCharacterStats') then return end
 
 function AS:ExtendedCharacterStats()
+	ECS_StatsFrame:ClearAllPoints()
+	ECS_StatsFrame:Point('Right', _G.CharacterFrame, 169, 32)
+	ECS_StatsFrame:SetHeight(424)
 	AS:SkinFrame(ECS_StatsFrame)
 	AS:SkinButton(ECS_StatsFrame.configButton)
 	AS:SkinButton(ECS_ToggleButton)
