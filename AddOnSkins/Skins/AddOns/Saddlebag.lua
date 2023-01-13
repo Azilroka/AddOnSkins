20292ca8b94af4a5832f605ad5a0664aa95bdaaa
Jessica C <bnet.glin@mistflux.net>
2023-01-13 14:35:07 -0500

    Add Saddlebag Exchange
    Added new skin file for Saddlebag Exchange's auction addon

---
+++ AddOnSkins/Skins/AddOns/Saddlebag.lua
@@ -0,0 +1,16 @@
+local AS = unpack(AddOnSkins)
+
+if not AS:CheckAddOn('Saddlebag') then return end
+
+function AS:Saddlebag()
+	local Saddlebag = LibStub("AceAddon-3.0"):GetAddon("Saddlebag")
+	hooksecurefunc(Saddlebag, 'auctionButton', function()
+		if not SaddlebagFrame.isSkinned then
+			AS:SkinFrame(SaddlebagFrame)
+			AS:SkinButton(SaddlebagFrameButton)
+			AS:SkinScrollBar(SaddlebagScrollFrameScrollBar)
+		end
+	end)
+end
+
+AS:RegisterSkin('Saddlebag', AS.Saddlebag)
