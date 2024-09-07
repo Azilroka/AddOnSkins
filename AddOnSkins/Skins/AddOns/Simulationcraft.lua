2c4a9cb09db16594eaf5068dcc55186618a1d117
Repooc <tjcooper1982@gmail.com>
2024-09-07 04:56:59 -0400

    after the initial opening, the frame wouldnt be skinned, so remove the call to the function, skin looks fine this way imo

--- AddOnSkins/Skins/AddOns/Simulationcraft.lua
+++ AddOnSkins/Skins/AddOns/Simulationcraft.lua
@@ -4,7 +4,6 @@ function R:Simulationcraft()
 	local Simulationcraft = LibStub("AceAddon-3.0"):GetAddon("Simulationcraft")
 	hooksecurefunc(Simulationcraft, 'GetMainFrame', function()
 		if not SimcFrame.isSkinned then
-			S:HandleFrame(SimcFrame)
 			S:HandleButton(SimcFrameButton)
 			S:HandleScrollBar(SimcScrollFrameScrollBar)
 		end
